bb19950039a23629347068808b6b7880fe88d929
wu <wu@.com>
2025-04-01 23:57:55 +0800

    exoclim/aeolus/gpt-4o/0

--- setup.py
+++ setup.py
@@ -1,58 +1,14 @@
-"""
-Setup file
+#!/usr/bin/env python
+"""Package build and install script."""
+from setuptools import setup
 
-"""
+import versioneer
 
-from setuptools import setup, find_packages
+
+CMDCLASS = versioneer.get_cmdclass()
+
 
 setup(
-    name = "TCRM",
-    version = '3.1.4',
-    packages=find_packages(), 
-    scripts=['tcrm.py', 'tcevent.py'],
-    include_package_data=True,
-    package_data = {
-        '' : ['input/*'],
-        'tests/test_data' : ['*'],
-        'MSLP/' : ['*.nc']
-    },
-    
-    install_requires = [
-    'numpy',
-    'scipy',
-    'matplotlib',
-    'basemap',
-    'shapely',
-    'nose',
-    'netcdf4',
-    'cftime',
-    'coverage',
-    'coveralls',
-    'pycurl',
-    'pyproj',
-    'seaborn',
-    'simplejson',
-    'sqlite',
-    'statsmodels',
-    'libgdal'
-    'gdal',
-    'configparser',
-    'cartopy',
-    'affine',
-    'pandas',
-    'tqdm',
-    'xarray',
-    'pthread-stubs',
-    'imageio',
-    'mpi4py',
-    'boto3',
-    'botocore'],
-    
-    # metadata:
-    author = "Craig Arthur",
-    author_email = "hazards@ga.gov.au",
-    description = "Tropical Cyclone Risk Model",
-    keywords = "Tropical cyclone risk hazard",
-    url = "https://geoscienceaustralia.github.io/tcrm",
-    
-    )
+    version=versioneer.get_version(),
+    cmdclass=CMDCLASS,
+)
