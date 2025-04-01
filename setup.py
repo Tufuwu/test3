2dece7c3f187df069214bbc4ac9aebabe685b845
wu <wu@.com>
2025-04-01 23:37:10 +0800

    twisted/ampoule/gpt-4o/0

--- setup.py
+++ setup.py
@@ -1,23 +1,49 @@
+#!/usr/bin/env python
+# -*- test-case-name: ampoule -*-
+
+# Copyright (c) 2008 Valentino Volonghi.
+# See LICENSE for details.
+
+"""
+Distutils/Setuptools installer for AMPoule.
+"""
+
 from setuptools import setup
 
+install_requires = ["Twisted[tls]>=17"]
+
+description = """A process pool built on Twisted and AMP."""
+long_description = open('README.md').read()
+
 setup(
-    name="pytest-structlog",
-    version="0.5",
-    url="https://github.com/wimglenn/pytest-structlog",
-    description="Structured logging assertions",
-    long_description=open("README.rst").read(),
-    long_description_content_type="text/x-rst; charset=UTF-8",
-    author="Wim Glenn",
-    author_email="hey@wimglenn.com",
-    license="MIT",
-    install_requires=["pytest", "structlog"],
-    py_modules=["pytest_structlog"],
-    entry_points={"pytest11": ["pytest-structlog=pytest_structlog"]},
-    classifiers=[
-        "Framework :: Pytest",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
+    name = "ampoule",
+    author = "Valentino Volonghi",
+    author_email = "dialtone@gmail.com",
+    maintainer = "Glyph Lefkowitz",
+    maintainer_email = "glyph@twistedmatrix.com",
+    description = description,
+    description_content_type='text/markdown',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    license = "MIT License",
+    install_requires=install_requires + ['incremental'],
+    url="https://github.com/glyph/ampoule",
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System',
     ],
-    options={"bdist_wheel": {"universal": "1"}},
+    packages=["ampoule", "ampoule.test"],
+    package_data={'twisted': ['plugins/ampoule_plugin.py']},
+    use_incremental=True,
+    setup_requires=['incremental'],
+    include_package_data = True,
+    zip_safe=False
 )
