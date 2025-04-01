94a5b70690a8dd387a5a6b1067bc5955b0b180ed
wu <wu@.com>
2025-04-01 23:38:25 +0800

    intgr/topy/gpt-4o/0

--- setup.py
+++ setup.py
@@ -1,28 +1,47 @@
-import setuptools
+#!/usr/bin/env python
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    author='Alex Hill',
-    author_email='alex@hill.net.au',
-    name='django-relativity',
-    version='0.2.6',
-    description='A flexible relationship field for the Django ORM.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/alexhill/django-relativity',
-    packages=setuptools.find_packages(exclude=['tests']),
-    install_requires=['django>=1.11'],
+setup(
+    name='topy',
+    version='1.1.0',
+
+    # PyPI metadata
+    author='Marti Raudsepp',
+    author_email='marti@juffo.org',
+    url='https://github.com/intgr/topy',
+    download_url='https://pypi.python.org/pypi/topy/',
+    license='MIT, CC-BY-SA',
+    description='Fixes typos in text using regular expressions, based on RegExTypoFix from Wikipedia',
+    long_description=open('README.rst').read(),
+    platforms='any',
+    keywords='typo spelling grammar text',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        # Until we have a test suite we're conservative about Python version compatibility claims
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Text Processing :: Filters',
     ],
+
+    # Installation settings
+    packages=['topy'],
+    entry_points={'console_scripts': ['topy = topy.topy:main']},
+    package_data={
+        '': ['*.txt']
+    },
+    install_requires=[
+        'regex>=2016.07.14',
+        'beautifulsoup4',
+    ],
+    test_suite='tests',
 )
