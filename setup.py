c4cb9b0087e5a3f9a34672501d9c5b697ce4eac7
wu <wu@.com>
2025-05-06 15:17:34 +0800

    dakrauth/django-swingtime

--- setup.py
+++ setup.py
@@ -1,63 +1,47 @@
-"""
-Installation setup for mtgjson5
-"""
-import configparser
-import pathlib
+#!/usr/bin/env python
+import os, sys
+from setuptools import setup, find_packages
 
-import setuptools
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit(0)
 
-# Establish project directory
-project_root: pathlib.Path = pathlib.Path(__file__).resolve().parent
+with open('README.rst', 'r') as f:
+    long_description = f.read()
 
-# Read config details to determine version-ing
-config_file = project_root.joinpath("mtgjson5/resources/mtgjson.properties")
-config = configparser.ConfigParser()
-if config_file.is_file():
-    config.read(str(config_file))
+# Dynamically calculate the version based on swingtime.VERSION.
+version=__import__('swingtime').get_version()
 
-setuptools.setup(
-    name="mtgjson5",
-    version=config.get("MTGJSON", "version", fallback="5.0.0+fallback"),
-    author="Zach Halpern",
-    author_email="zach@mtgjson.com",
-    url="https://mtgjson.com/",
-    description="Magic: the Gathering compiled database generator",
-    long_description=project_root.joinpath("README.md").open(encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    license="MIT",
+setup(
+    name='django-swingtime',
+    url='https://github.com/dakrauth/django-swingtime',
+    author='David A Krauth',
+    author_email='dakrauth@gmail.com',
+    description='A Django calendaring application.',
+    version=version,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    platforms=['any'],
+    license='MIT License',
+    python_requires='>=3.6, <4',
+    install_requires=['Django>=2.2,<4.0', 'python-dateutil==2.8.0'],
     classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows :: Windows 10",
-        "Operating System :: Unix",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python",
-        "Topic :: Database",
-        "Topic :: Software Development :: Version Control :: Git",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Office/Business :: Scheduling',
     ],
-    keywords=[
-        "Big Data",
-        "Card Games",
-        "Collectible",
-        "Database",
-        "JSON",
-        "MTG",
-        "MTGJSON",
-        "Trading Cards",
-        "Magic: The Gathering",
-    ],
-    include_package_data=True,
-    packages=setuptools.find_packages(),
-    install_requires=project_root.joinpath("requirements.txt")
-    .open(encoding="utf-8")
-    .readlines()
-    if project_root.joinpath("requirements.txt").is_file()
-    else [],  # Use the requirements file, if able
+    packages=find_packages(),
+    package_data={'swingtime': ['locale/*/*/*.*',]},
+    zip_safe=False,
 )
