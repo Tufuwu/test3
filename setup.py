8fa6d9212e911e1441ad7f95d0edafa5973d4efc
wu <wu@.com>
2025-05-15 10:57:16 +0800

    arthurdejong/python-pskc

--- setup.py
+++ setup.py
@@ -1,91 +1,84 @@
-from setuptools import setup, find_packages
-from codecs import open
-from os import path, environ
+#!/usr/bin/env python
+
+# setup.py - python-pskc installation script
+#
+# Copyright (C) 2014-2019 Arthur de Jong
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+"""python-pskc installation script."""
+
+import os
 import sys
+from setuptools import setup, find_packages
 
-here = path.abspath(path.dirname(__file__))
+import pskc
 
-# Get the long description from the relevant file
-with open(path.join(here, 'docs/description.rst'), encoding='utf-8') as f:
-        long_description = f.read()
+# fix permissions for sdist
+if 'sdist' in sys.argv:
+    os.system('chmod -R a+rX .')
+    os.umask(int('022', 8))
 
-with open(path.join(here, 'VERSION'), mode='r', encoding='utf-8') as version_file:
-        version = version_file.read().strip()
+base_dir = os.path.dirname(__file__)
+
+with open(os.path.join(base_dir, 'README'), 'r') as fp:
+    long_description = fp.read()
 
 setup(
-    name='pyeapi',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version=version,
-
-    description='Python Client for eAPI',
+    name='python-pskc',
+    version=pskc.__version__,
+    description='Python module for handling PSKC files',
     long_description=long_description,
-
-    # The project's main homepage.
-    url='https://github.com/arista-eosplus/pyeapi',
-
-    # Author details
-    author='Arista EOS+ CS',
-    author_email='eosplus-dev@arista.com',
-
-    # Choose your license
-    license='BSD-3',
-
-    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    author='Arthur de Jong',
+    author_email='arthur@arthurdejong.org',
+    keywords=['PSKC', 'RFC 6030', 'key container'],
+    url='https://arthurdejong.org/python-pskc/',
+    license='LGPL',
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
-        'Topic :: System :: Networking',
-
-        'License :: OSI Approved :: BSD License',
-
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        'Topic :: Text Processing :: Markup :: XML',
     ],
-
-    keywords='networking arista eos eapi',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
-    # List run-time dependencies here.  These will be installed by pip when your
-    # project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['netaddr'],
-
-    # List additional dependencies here (e.g. development dependencies).
-    # You can install these using the following syntax, for example:
-    # $ pip install -e .[dev,test]
+    packages=find_packages(),
+    install_requires=['cryptography', 'python-dateutil'],
     extras_require={
-        'dev': ['check-manifest', 'pep8', 'pyflakes', 'twine'],
-        'test': ['coverage', 'mock'],
+        'lxml': ['lxml'],
+        'defuse': ['defusedxml'],
+        'signature': ['signxml'],
+    },
+    entry_points={
+        'console_scripts': [
+            'csv2pskc = pskc.scripts.csv2pskc:main',
+            'pskc2csv = pskc.scripts.pskc2csv:main',
+            'pskc2pskc = pskc.scripts.pskc2pskc:main',
+        ],
     },
 )
-
-def install():
-    if "install" in sys.argv:
-        return True
-    else:
-        return False
-
-# Use the following to dynamically build pyeapi module documentation
-if install() and environ.get('READTHEDOCS'):
-    print('This method is only called by READTHEDOCS.')
-    from subprocess import Popen
-    proc = Popen(['make', 'modules'], cwd='docs/')
-    (_, err) = proc.communicate()
-    return_code = proc.wait()
-
-    if return_code or err:
-        raise ('Failed to make modules.(%s:%s)' % (return_code, err))
