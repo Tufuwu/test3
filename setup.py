1996677670c5362a4b7e367e2993ef00db1a5cc3
wu <wu@.com>
2025-05-20 15:07:32 +0800

    d:\vscode\repos\burnysc2\python-sc2\data d:\vscode\repos\burnysc2\python-sc2\docs d:\vscode\repos\burnysc2\python-sc2\docs_generate d:\vscode\repos\burnysc2\python-sc2\examples d:\vscode\repos\burnysc2\python-sc2\sc2 d:\vscode\repos\burnysc2\python-sc2\test d:\vscode\repos\burnysc2\python-sc2\.gitignore d:\vscode\repos\burnysc2\python-sc2\.nojekyll d:\vscode\repos\burnysc2\python-sc2\generate_dicts_from_data_json.py d:\vscode\repos\burnysc2\python-sc2\generate_id_constants_from_stableid.py d:\vscode\repos\burnysc2\python-sc2\index.html d:\vscode\repos\burnysc2\python-sc2\LICENSE d:\vscode\repos\burnysc2\python-sc2\Pipfile d:\vscode\repos\burnysc2\python-sc2\Pipfile.lock d:\vscode\repos\burnysc2\python-sc2\README.md d:\vscode\repos\burnysc2\python-sc2\setup.py d:\vscode\repos\burnysc2\python-sc2\.github d:\vscode\repos\burnysc2\python-sc2\bat_files

--- setup.py
+++ setup.py
@@ -1,48 +1,36 @@
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile["packages"], r=False)
+test_requirements = convert_deps_to_pip(pfile["dev-packages"], r=False)
 
 setup(
-    name="bundlewrap",
-    version="4.3.0",
-    description="Config management with Python",
-    long_description=(
-        "By allowing for easy and low-overhead config management, BundleWrap fills the gap between complex deployments using Chef or Puppet and old school system administration over SSH.\n"
-        "While most other config management systems rely on a client-server architecture, BundleWrap works off a repository cloned to your local machine. It then automates the process of SSHing into your servers and making sure everything is configured the way it's supposed to be. You won't have to install anything on managed servers."
-    ),
-    author="Torsten Rehn",
-    author_email="torsten@rehn.email",
-    license="GPLv3",
-    url="http://bundlewrap.org",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            "bw=bundlewrap.cmdline:main",
-        ],
-    },
-    keywords=["configuration", "config", "management"],
+    name="burnysc2",
+    packages=find_packages(exclude=["examples*", "examples"]),
+    version="4.11.2",
+    description="A StarCraft II API Client for Python 3",
+    license="MIT",
+    author="BurnySc2",
+    author_email="gamingburny@gmail.com",
+    url="https://github.com/Burnysc2/python-sc2",
+    keywords=["StarCraft", "StarCraft 2", "StarCraft II", "AI", "Bot"],
+    setup_requires=["pipenv"],
+    install_requires=requirements,
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Games/Entertainment",
+        "Topic :: Games/Entertainment :: Real Time Strategy",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: System :: Installation/Setup",
-        "Topic :: System :: Systems Administration",
     ],
-    install_requires=[
-        "cryptography",
-        "Jinja2",
-        "Mako",
-        "passlib",
-        "pyyaml",
-        "requests >= 1.0.0",
-        "tomlkit",
-    ],
-    zip_safe=False,
 )
