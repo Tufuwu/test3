809ea426f311ab1c2a9761eba16bf6678a94dfae
wu <wu@.com>
2025-05-21 16:38:00 +0800

    cherab/core

--- setup.py
+++ setup.py
@@ -1,59 +1,113 @@
-#!/usr/bin/env python3
-
+from setuptools import setup, find_packages, Extension
 import sys
+import numpy
 import os
+import os.path as path
+import multiprocessing
 
-from setuptools import find_packages, setup
-from setuptools_rust import RustExtension
+use_cython = True
+force = False
+profile = False
+line_profile = False
+install_rates = False
 
-PACKAGE_NAME = "cryptg"
-PACKAGE_VERSION = "0.3"
-ENVVAR_VERSION_SUFFIX = "PYPI_SETUP_VERSION_SUFFIX"
+if "--skip-cython" in sys.argv:
+    use_cython = False
+    del sys.argv[sys.argv.index("--skip-cython")]
 
+if "--force" in sys.argv:
+    force = True
+    del sys.argv[sys.argv.index("--force")]
 
-def main(args):
-    with open("README.rst", encoding='utf-8') as f:
-        long_description = f.read()
+if "--profile" in sys.argv:
+    profile = True
+    del sys.argv[sys.argv.index("--profile")]
 
-    url = "https://github.com/cher-nov/" + PACKAGE_NAME
+if "--line-profile" in sys.argv:
+    line_profile = True
+    del sys.argv[sys.argv.index("--line-profile")]
 
-    setup(
-        name=PACKAGE_NAME,
-        version=PACKAGE_VERSION+os.environ.get(ENVVAR_VERSION_SUFFIX, ""),
-        description="Cryptographic utilities for Telegram.",
-        long_description=long_description,
-        long_description_content_type="text/x-rst",
+if "--install-rates" in sys.argv:
+    install_rates = True
+    del sys.argv[sys.argv.index("--install-rates")]
 
-        url=url,
-        download_url=url+"/releases",
+source_paths = ['cherab', 'demos']
+compilation_includes = [".", numpy.get_include()]
+compilation_args = []
+cython_directives = {
+    'language_level': 3
+}
+setup_path = path.dirname(path.abspath(__file__))
 
-        author="Dmitry D. Chernov; Lonami E",
-        author_email="blackdoomer@yandex.ru",
+if line_profile:
+    compilation_args.append("-DCYTHON_TRACE=1")
+    compilation_args.append("-DCYTHON_TRACE_NOGIL=1")
+    cython_directives["linetrace"] = True
 
-        license="CC0",
+if use_cython:
 
-        # https://pypi.python.org/pypi?:action=list_classifiers
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Developers",
-            "Topic :: Security :: Cryptography",
-            "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Rust",
-        ],
-        keywords="telegram crypto cryptography mtproto aes",
+    from Cython.Build import cythonize
 
-        packages=find_packages(),
-        python_requires=">=3.3",
-        rust_extensions=[RustExtension("cryptg.cryptg")],
-        zip_safe=False,
-    )
+    # build .pyx extension list
+    extensions = []
+    for package in source_paths:
+        for root, dirs, files in os.walk(path.join(setup_path, package)):
+            for file in files:
+                if path.splitext(file)[1] == ".pyx":
+                    pyx_file = path.relpath(path.join(root, file), setup_path)
+                    module = path.splitext(pyx_file)[0].replace("/", ".")
+                    extensions.append(Extension(module, [pyx_file], include_dirs=compilation_includes, extra_compile_args=compilation_args),)
 
+    if profile:
+        cython_directives["profile"] = True
 
-if __name__ == '__main__':
-    main(sys.argv)
+    # generate .c files from .pyx
+    extensions = cythonize(extensions, nthreads=multiprocessing.cpu_count(), force=force, compiler_directives=cython_directives)
+
+else:
+
+    # build .c extension list
+    extensions = []
+    for package in source_paths:
+        for root, dirs, files in os.walk(path.join(setup_path, package)):
+            for file in files:
+                if path.splitext(file)[1] == ".c":
+                    c_file = path.relpath(path.join(root, file), setup_path)
+                    module = path.splitext(c_file)[0].replace("/", ".")
+                    extensions.append(Extension(module, [c_file], include_dirs=compilation_includes, extra_compile_args=compilation_args),)
+
+# parse the package version number
+with open(path.join(path.dirname(__file__), 'cherab/core/VERSION')) as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name="cherab",
+    version=version,
+    license="EUPL 1.1",
+    namespace_packages=['cherab'],
+    description='Cherab spectroscopy framework',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Physics"
+    ],
+    install_requires=['numpy>=1.14', 'scipy', 'matplotlib', 'raysect>=0.7.1', 'cython>=0.28'],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    ext_modules=extensions
+)
+
+# setup a rate repository with common rates
+if install_rates:
+    try:
+        from cherab.openadas import repository
+        repository.populate()
+    except ImportError:
+        pass
