0d8c3dd762c8aa82afb4903068e015074aec5cb5
wu <wu@.com>
2025-05-21 15:23:29 +0800

    changaco/python-libarchive-c

--- setup.py
+++ setup.py
@@ -1,45 +1,22 @@
 import os
+from os.path import join, dirname
 
 from setuptools import setup, find_packages
 
-base_dir = os.path.dirname(__file__)
+from version import get_version
 
-about = {}
-with open(os.path.join(base_dir, 'stacks', '__about__.py')) as f:
-    exec(f.read(), about)
+os.umask(0o022)
 
-install_requires = [
-    'configargparse>=0.9.3',
-    'PyYAML>=4.2b1',
-    'Jinja2>=2.7.3',
-    'boto>=2.40.0',
-    'tabulate>=0.7.5',
-    'setuptools',
-    'pytz',
-    'tzlocal',
-]
-
-tests_require = [
-    'moto',
-]
-
-config = {
-    'name': 'cfstacks',
-    'description': 'Manage CloudFormation sanely with templates written in YAML',
-    'url': about['__url__'],
-    'download_url': about['__url__'],
-    'version': about['__version__'],
-    'maintainer': about['__maintainer__'],
-    'maintainer_email': about['__maintainer_email__'],
-    'packages': find_packages(),
-    'install_requires': install_requires,
-    'tests_require': tests_require,
-    'entry_points': {
-        'console_scripts': [
-            'stacks = stacks.__main__:main',
-        ],
-    },
-    'python_requires': '>=3',
-}
-
-setup(**config)
+setup(
+    name='libarchive-c',
+    version=get_version(),
+    description='Python interface to libarchive',
+    author='Changaco',
+    author_email='changaco@changaco.oy.lc',
+    url='https://github.com/Changaco/python-libarchive-c',
+    license='CC0',
+    packages=find_packages(exclude=['tests']),
+    long_description=open(join(dirname(__file__), 'README.rst')).read(),
+    long_description_content_type='text/x-rst',
+    keywords='archive libarchive 7z tar bz2 zip gz',
+)
