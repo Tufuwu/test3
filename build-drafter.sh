8637fc7e7c13a5cd24eca65ee9d99153c5b9fef6
wu <wu@.com>
2025-05-21 16:57:03 +0800

    chris48s/django-apiblueprint-view

--- build-drafter.sh
+++ build-drafter.sh
@@ -4,7 +4,7 @@ set -e -x
 wget https://github.com/apiaryio/drafter/releases/download/v3.2.7/drafter-v3.2.7.tar.gz
 tar xvzf drafter-v3.2.7.tar.gz
 cd drafter-v3.2.7
-python2 configure --shared
+python3 configure --shared
 make libdrafter
 cd ..
 mkdir -p apiblueprint_view/lib
