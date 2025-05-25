2c3af8674bb8d8b893bf68e5c703d61fa7022558
wu <wu@.com>
2025-05-25 15:59:52 +0800

    parsely/streamparse

--- setup.py
+++ setup.py
@@ -1,39 +1,83 @@
 #!/usr/bin/env python
-"""Installation script."""
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+"""
+Copyright 2014-2020 Parsely, Inc.
 
-import ast
-import codecs
-import os
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import re
 
+from setuptools import setup, find_packages
 
-CURRENT_DIR = os.path.dirname(__file__)
-
-
-def get_long_description():
-    readme_path = os.path.join(CURRENT_DIR, "README.md")
-    with codecs.open(readme_path, encoding="utf8") as ld_file:
-        return ld_file.read()
-
-
+# Get version without importing, which avoids dependency issues
 def get_version():
-    pydot_py = os.path.join(CURRENT_DIR, "src", "pydot", "__init__.py")
-    _version_re = re.compile(r"__version__\s+=\s+(?P<version>.*)")
-    with codecs.open(pydot_py, "r", encoding="utf8") as f:
-        match = _version_re.search(f.read())
-        version = match.group("version") if match is not None else '"unknown"'
-    return str(ast.literal_eval(version))
+    with open("streamparse/version.py") as version_file:
+        return re.search(
+            r"""__version__\s+=\s+(['"])(?P<version>.+?)\1""", version_file.read()
+        ).group("version")
 
+
+def readme():
+    """ Returns README.rst contents as str """
+    with open("README.rst") as f:
+        return f.read()
+
+
+install_requires = [
+    l.split("#")[0].strip()
+    for l in open("requirements.txt").readlines()
+    if not l.startswith(("#", "-"))
+]
+
+tests_require = ["graphviz", "pytest"]
 
 setup(
-    name="pydot",
+    name="streamparse",
     version=get_version(),
-    package_dir={"": "src"},
-    packages=["pydot"],
-    long_description=get_long_description(),
-    long_description_content_type="text/markdown",
+    author="Parsely, Inc.",
+    author_email="hello@parsely.com",
+    url="https://github.com/Parsely/streamparse",
+    description=(
+        "streamparse lets you run Python code against real-time "
+        "streams of data. Integrates with Apache Storm."
+    ),
+    long_description=readme(),
+    license="Apache License 2.0",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "sparse = streamparse.cli.sparse:main",
+            "streamparse = streamparse.cli.sparse:main",
+            "streamparse_run = streamparse.run:main",
+        ]
+    },
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        "test": tests_require,
+        "all": install_requires + tests_require,
+        "docs": ["sphinx"] + tests_require,
+    },
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
 )
