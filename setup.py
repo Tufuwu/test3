7644eb06d4c06cbd7f3108ed604528604cf00044
wu <wu@.com>
2025-05-29 13:04:35 +0800

    colcon/colcon-core

--- setup.py
+++ setup.py
@@ -1,27 +1,61 @@
+# Copyright 2016-2018 Dirk Thomas
+# Licensed under the Apache License, Version 2.0
+
+import os
+import sys
+
+from pkg_resources import parse_version
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+minimum_version = '3.5'
+if (
+    parse_version('%d.%d' % (sys.version_info.major, sys.version_info.minor)) <
+    parse_version(minimum_version)
+):
+    sys.exit('This package requires at least Python ' + minimum_version)
 
-requires = []
-with open('requirements.txt') as f:
-    for line in f.readlines():
-        line = line.strip()  # Remove spaces
-        line = line.split('#')[0]  # Remove comments
-        if line:  # Remove empty lines
-            requires.append(line)
+cmdclass = {}
+try:
+    from stdeb.command.sdist_dsc import sdist_dsc
+except ImportError:
+    pass
+else:
+    class CustomSdistDebCommand(sdist_dsc):
+        """Weird approach to apply the Debian patches during packaging."""
 
-setup(
-    name='django-clickhouse',
-    version='1.0.3',
-    packages=['django_clickhouse'],
-    package_dir={'': 'src'},
-    url='https://github.com/carrotquest/django-clickhouse',
-    license='BSD 3-clause "New" or "Revised" License',
-    author='Carrot quest',
-    author_email='m1ha@carrotquest.io',
-    description='Django extension to integrate with ClickHouse database',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    install_requires=requires
-)
+        def run(self):  # noqa: D102
+            from stdeb.command import sdist_dsc
+            build_dsc = sdist_dsc.build_dsc
+
+            def custom_build_dsc(*args, **kwargs):
+                nonlocal build_dsc
+                debinfo = self.get_debinfo()
+                repackaged_dirname = \
+                    debinfo.source + '-' + debinfo.upstream_version
+                dst_directory = os.path.join(
+                    self.dist_dir, repackaged_dirname, 'debian', 'patches')
+                os.makedirs(dst_directory, exist_ok=True)
+                # read patch
+                with open('debian/patches/setup.cfg.patch', 'r') as h:
+                    lines = h.read().splitlines()
+                print(
+                    "writing customized patch '%s'" %
+                    os.path.join(dst_directory, 'setup.cfg.patch'))
+                # write patch with modified path
+                with open(
+                    os.path.join(dst_directory, 'setup.cfg.patch'), 'w'
+                ) as h:
+                    for line in lines:
+                        if line.startswith('--- ') or line.startswith('+++ '):
+                            line = \
+                                line[0:4] + repackaged_dirname + '/' + line[4:]
+                        h.write(line + '\n')
+                with open(os.path.join(dst_directory, 'series'), 'w') as h:
+                    h.write('setup.cfg.patch\n')
+                return build_dsc(*args, **kwargs)
+
+            sdist_dsc.build_dsc = custom_build_dsc
+            super().run()
+    cmdclass['sdist_dsc'] = CustomSdistDebCommand
+
+setup(cmdclass=cmdclass)
