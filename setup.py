5c20aceddd2e3cfe445341b746ce68688753dc62
wu <wu@.com>
2025-06-02 11:25:46 +0800

    collective/collective.solr

--- setup.py
+++ setup.py
@@ -1,30 +1,88 @@
 from setuptools import setup
+from setuptools import find_packages
 
+version = "8.4.2.dev0"
+
+long_description = "\n\n".join(
+    [open("README.rst").read(), open("CHANGES.rst").read()])
 
 setup(
-    name='wtf-peewee',
-    version='3.0.1',
-    url='https://github.com/coleifer/wtf-peewee/',
-    license='MIT',
-    author='Charles Leifer',
-    author_email='coleifer@gmail.com',
-    description='WTForms integration for peewee models',
-    packages=['wtfpeewee'],
-    zip_safe=False,
-    platforms='any',
-    install_requires=[
-        'peewee>=3.0.0', 'wtforms==2.3.3',
-    ],
+    name="collective.solr",
+    version=version,
+    description="Solr integration for external indexing and searching.",
+    long_description=long_description,
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    test_suite='runtests.runtests'
+    keywords="plone cmf zope indexing searching solr lucene",
+    author="Plone Community",
+    author_email="plone-developers@lists.sourceforge.net",
+    maintainer="Timo Stollenwerk",
+    maintainer_email="tisto@plone.org",
+    url="https://github.com/collective/collective.solr",
+    license="GPL version 2",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    namespace_packages=["collective"],
+    include_package_data=True,
+    platforms="Any",
+    zip_safe=False,
+    install_requires=[
+        "Acquisition",
+        "DateTime",
+        "Products.CMFCore",
+        "Products.CMFPlone >= 4.3.7",
+        "Products.GenericSetup",
+        "Products.ZCatalog",
+        # 'collective.indexing >= 2.1',
+        "collective.js.showmore",
+        "lxml",
+        "plone.app.content",
+        "plone.app.layout",
+        "plone.app.registry",
+        "plone.app.vocabularies",
+        "plone.api",
+        "plone.browserlayer",
+        "plone.indexer",
+        "plone.restapi",
+        "setuptools",
+        "transaction",
+        "zope.component",
+        "zope.i18nmessageid",
+        "zope.interface",
+        "zope.publisher",
+        "zope.schema",
+    ],
+    extras_require={
+        "test": ["plone.app.testing[robot]", "plone.app.robotframework[debug]"],
+        "test4": [
+            "Products.LinguaPlone >=3.1a1",
+            "plone.app.robotframework[debug]",
+            "plone.app.testing[robot]",
+        ],
+    },
+    entry_points="""
+      [z3c.autoinclude.plugin]
+      target=plone
+      [zopectl.command]
+      solr_activate=collective.solr.commands:solr_activate
+      solr_deactivate=collective.solr.commands:solr_deactivate
+      solr_clear_index=collective.solr.commands:solr_clear_index
+      solr_reindex=collective.solr.commands:solr_reindex
+    """,
 )
