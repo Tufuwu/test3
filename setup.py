acca4a521a976962ca11e6f95cdd1846cdc25167
wu <wu@.com>
2025-06-05 14:35:41 +0800

    danirus/django-comments-xtd

--- setup.py
+++ setup.py
@@ -1,24 +1,58 @@
-""" Setup file. """
+import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
+from setuptools.command.test import test
 
-with open("README.rst", "r") as readme_file:
-    README = readme_file.read()
 
-exec(open("src/matching/version.py", "r").read())
+def run_tests(*args):
+    from django_comments_xtd.tests import run_tests
+    errors = run_tests()
+    if errors:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+test.run_tests = run_tests
+
 
 setup(
-    name="matching",
-    version=__version__,
-    description="A package for solving matching games.",
-    long_description=README,
-    url="https://github.com/daffidwilde/matching",
-    author="Henry Wilde",
-    author_email="henrydavidwilde@gmail.com",
+    name="django-comments-xtd",
+    version="2.8.2",
+    packages=find_packages(),
+    include_package_data=True,
     license="MIT",
-    keywords=["game-theory gale-shapley matching-games"],
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    python_requires=">=3.5",
-    tests_require=["pytest", "hypothesis", "numpy"],
+    description=("Django Comments Framework extension app with thread "
+                 "support, follow up notifications and email "
+                 "confirmations."),
+    long_description=("A reusable Django app that extends django-contrib-"
+                      "comments Framework with thread support, following up "
+                      "notifications and comments that only hits the "
+                      "database after users confirm them by email."),
+    author="Daniel Rus Morales",
+    author_email="mbox@danir.us",
+    maintainer="Daniel Rus Morales",
+    maintainer_email="mbox@danir.us",
+    url="http://pypi.python.org/pypi/django-comments-xtd",
+    install_requires=[
+        'Django>=2.2',
+        'django-contrib-comments>=1.9',
+        'djangorestframework>=3.9',
+        'docutils',
+        'six',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+    ],
+    test_suite="dummy",
+    zip_safe=True
 )
