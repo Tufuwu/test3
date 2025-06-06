d029265a78adb9ddf2d11bfd825347ea3cc16108
wu <wu@.com>
2025-06-06 16:10:18 +0800

    dask/partd

--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
 
 from os.path import exists
-
-import versioneer
 from setuptools import setup
+import versioneer
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
-if exists("README.rst"):
-    with open("README.rst") as f:
-        long_description = f.read()
-else:
-    long_description = ""
-
-setup(
-    name="dask-jobqueue",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="Deploy Dask on job queuing systems like PBS, Slurm, SGE or LSF",
-    url="https://jobqueue.dask.org",
-    python_requires=">=3.5",
-    license="BSD 3-Clause",
-    packages=["dask_jobqueue"],
-    include_package_data=True,
-    install_requires=install_requires,
-    tests_require=["pytest >= 2.7.1"],
-    long_description=long_description,
-    zip_safe=False,
-)
+setup(name='partd',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Appendable key-value storage',
+      url='http://github.com/dask/partd/',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='',
+      packages=['partd'],
+      install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      python_requires=">=3.5",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+      ],
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      extras_require={'complete': [
+          'numpy >= 1.9.0',
+          'pandas >=0.19.0',
+          'pyzmq',
+          'blosc',
+      ]},
+      zip_safe=False)
