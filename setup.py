13ce065cca223e3c2ca0b4f1cb5309e48d9180c9
wu <wu@.com>
2025-06-07 11:02:56 +0800

    dbcli/pgcli

--- setup.py
+++ setup.py
@@ -1,56 +1,64 @@
-"""
-Publish a new version:
+import platform
+from setuptools import setup, find_packages
 
-$ git tag X.Y.Z -m "Release X.Y.Z"
-$ git push --tags
+from pgcli import __version__
 
-$ pip install --upgrade twine wheel
-$ python setup.py sdist bdist_wheel --universal
-$ twine upload dist/*
-"""
-import codecs
-from setuptools import setup
+description = "CLI for Postgres Database. With auto-completion and syntax highlighting."
+
+install_requirements = [
+    "pgspecial>=1.11.8",
+    "click >= 4.1",
+    "Pygments >= 2.0",  # Pygments has to be Capitalcased. WTF?
+    # We still need to use pt-2 unless pt-3 released on Fedora32
+    # see: https://github.com/dbcli/pgcli/pull/1197
+    "prompt_toolkit>=2.0.6,<4.0.0",
+    "psycopg2 >= 2.8",
+    "sqlparse >=0.3.0,<0.5",
+    "configobj >= 5.0.6",
+    "pendulum>=2.1.0",
+    "cli_helpers[styles] >= 2.0.0",
+]
 
 
-SCHEDULE_VERSION = '0.6.0'
-SCHEDULE_DOWNLOAD_URL = (
-    'https://github.com/dbader/schedule/tarball/' + SCHEDULE_VERSION
-)
-
-
-def read_file(filename):
-    """
-    Read a utf8 encoded text file and return its contents.
-    """
-    with codecs.open(filename, 'r', 'utf8') as f:
-        return f.read()
-
+# setproctitle is used to mask the password when running `ps` in command line.
+# But this is not necessary in Windows since the password is never shown in the
+# task manager. Also setproctitle is a hard dependency to install in Windows,
+# so we'll only install it if we're not in Windows.
+if platform.system() != "Windows" and not platform.system().startswith("CYGWIN"):
+    install_requirements.append("setproctitle >= 1.1.9")
 
 setup(
-    name='schedule',
-    packages=['schedule'],
-    version=SCHEDULE_VERSION,
-    description='Job scheduling for humans.',
-    long_description=read_file('README.rst'),
-    license='MIT',
-    author='Daniel Bader',
-    author_email='mail@dbader.org',
-    url='https://github.com/dbader/schedule',
-    download_url=SCHEDULE_DOWNLOAD_URL,
-    keywords=[
-        'schedule', 'periodic', 'jobs', 'scheduling', 'clockwork',
-        'cron', 'scheduler', 'job scheduling'
-    ],
+    name="pgcli",
+    author="Pgcli Core Team",
+    author_email="pgcli-dev@googlegroups.com",
+    version=__version__,
+    license="BSD",
+    url="http://pgcli.com",
+    packages=find_packages(),
+    package_data={"pgcli": ["pgclirc", "packages/pgliterals/pgliterals.json"]},
+    description=description,
+    long_description=open("README.rst").read(),
+    install_requires=install_requirements,
+    extras_require={"keyring": ["keyring >= 12.2.0"]},
+    python_requires=">=3.6",
+    entry_points="""
+        [console_scripts]
+        pgcli=pgcli.main:cli
+    """,
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Natural Language :: English',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: SQL",
+        "Topic :: Database",
+        "Topic :: Database :: Front-Ends",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
 )
