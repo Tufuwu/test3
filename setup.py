beadc00e5588fd39cded0d747df5b87220e22d71
wu <wu@.com>
2025-06-09 14:19:13 +0800

    districtdatalabs/yellowbrick

--- setup.py
+++ setup.py
@@ -1,60 +1,177 @@
 #!/usr/bin/env python
+# setup
+# Setup script for installing yellowbrick
+#
+# Author:   Benjamin Bengfort
+# Created:  Wed May 18 14:33:26 2016 -0400
+#
+# Copyright (C) 2016 The scikit-yb developers
+# For license information, see LICENSE.txt and NOTICE.md
+#
+# ID: setup.py [c4f3ba7] benjamin@bengfort.com $
 
-import ast
-import os.path
-from setuptools import setup, find_packages
+"""
+Setup script for installing yellowbrick.
+See http://bbengfort.github.io/programmer/2016/01/20/packaging-with-pypi.html
+"""
 
-class GetVersion(ast.NodeVisitor):
-    def __init__(self, path):
-        with open(path) as f:
-            self.visit(ast.parse(f.read(), path))
+##########################################################################
+## Imports
+##########################################################################
 
-    def visit_Assign(self, node):
-        if any(target.id == 'VERSION' for target in node.targets):
-            assert not hasattr(self, 'VERSION')
-            self.VERSION = node.value.s
+import os
+import codecs
 
-this_directory = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(this_directory, 'README.rst')) as f:
-    long_description = f.read()
+from setuptools import setup
+from setuptools import find_packages
 
-setup(
-    name='django-bitfield',
-    version=GetVersion(os.path.join(os.path.dirname(__file__), 'bitfield', '__init__.py')).VERSION,
-    author='Disqus',
-    author_email='opensource@disqus.com',
-    url='https://github.com/disqus/django-bitfield',
-    description='BitField in Django',
-    long_description=long_description,
-    long_description_content_type='text/x-rst',
-    packages=find_packages(),
-    zip_safe=False,
-    install_requires=[
-        'Django>=1.10.8',
-        'six',
-    ],
-    extras_require={
-        'tests': [
-            'flake8',
-            'mysqlclient',
-            'psycopg2>=2.3',
-            'pytest-django',
-        ],
-    },
-    include_package_data=True,
-    classifiers=[
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development',
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
+##########################################################################
+## Package Information
+##########################################################################
+
+## Basic information
+## Basic information
+NAME         = "yellowbrick"
+DESCRIPTION  = "A suite of visual analysis and diagnostic tools for machine learning."
+AUTHOR       = "The scikit-yb developers"
+EMAIL        = "yellowbrick@googlegroups.com"
+MAINTAINER   = "The scikit-yb developers"
+LICENSE      = "Apache 2"
+REPOSITORY   = "https://github.com/DistrictDataLabs/yellowbrick"
+PACKAGE      = "yellowbrick"
+URL          = "http://scikit-yb.org/"
+
+## Define the keywords
+KEYWORDS = (
+    "visualization",
+    "machine learning",
+    "scikit-learn",
+    "matplotlib",
+    "data science",
 )
+
+## Define the classifiers
+## See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+CLASSIFIERS  = (
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: Scientific/Engineering :: Visualization",
+)
+
+## Important Paths
+PROJECT = os.path.abspath(os.path.dirname(__file__))
+REQUIRE_PATH = "requirements.txt"
+VERSION_PATH = os.path.join(PACKAGE, "version.py")
+PKG_DESCRIBE = "DESCRIPTION.md"
+
+## Directories to ignore in find_packages
+EXCLUDES = (
+    "tests", "tests.*",
+    "bin",
+    "docs", "docs.*",
+    "fixtures",
+    "register",
+    "notebooks", "notebooks.*",
+    "examples", "examples.*",
+    "binder", "binder.*",
+    "paper",
+)
+
+##########################################################################
+## Helper Functions
+##########################################################################
+
+
+def read(*parts):
+    """
+    Assume UTF-8 encoding and return the contents of the file located at the
+    absolute path from the REPOSITORY joined with *parts.
+    """
+    with codecs.open(os.path.join(PROJECT, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+def get_version(path=VERSION_PATH):
+    """
+    Reads the python file defined in the VERSION_PATH to find the get_version
+    function, and executes it to ensure that it is loaded correctly. Separating
+    the version in this way ensures no additional code is executed.
+    """
+    namespace = {}
+    exec(read(path), namespace)
+    return namespace["get_version"](short=True)
+
+
+def get_requires(path=REQUIRE_PATH):
+    """
+    Yields a generator of requirements as defined by the REQUIRE_PATH which
+    should point to a requirements.txt output by `pip freeze`.
+    """
+    for line in read(path).splitlines():
+        line = line.strip()
+        if line and not line.startswith("#"):
+            yield line
+
+
+def get_description_type(path=PKG_DESCRIBE):
+    """
+    Returns the long_description_content_type based on the extension of the
+    package describe path (e.g. .txt, .rst, or .md).
+    """
+    _, ext = os.path.splitext(path)
+    return {".rst": "text/x-rst", ".txt": "text/plain", ".md": "text/markdown"}[ext]
+
+
+##########################################################################
+## Define the configuration
+##########################################################################
+
+config = {
+    "name": NAME,
+    "version": get_version(),
+    "description": DESCRIPTION,
+    "long_description": read(PKG_DESCRIBE),
+    "long_description_content_type": get_description_type(PKG_DESCRIBE),
+    "classifiers": CLASSIFIERS,
+    "keywords": KEYWORDS,
+    "license": LICENSE,
+    "author": AUTHOR,
+    "author_email": EMAIL,
+    "url": URL,
+    "maintainer": MAINTAINER,
+    "maintainer_email": EMAIL,
+    "project_urls": {
+        "Documentation": URL,
+        "Download": "{}/tarball/v{}".format(REPOSITORY, get_version()),
+        "Source": REPOSITORY,
+        "Tracker": "{}/issues".format(REPOSITORY),
+    },
+    "download_url": "{}/tarball/v{}".format(REPOSITORY, get_version()),
+    "packages": find_packages(where=PROJECT, exclude=EXCLUDES),
+    "package_data": {"yellowbrick": ["datasets/manifest.json"]},
+    "zip_safe": False,
+    "entry_points": {"console_scripts": []},
+    "install_requires": list(get_requires()),
+    "python_requires": ">=3.4, <4",
+    "setup_requires": ["pytest-runner"],
+    "tests_require": ["pytest"],
+}
+
+
+##########################################################################
+## Run setup script
+##########################################################################
+
+if __name__ == "__main__":
+    setup(**config)
