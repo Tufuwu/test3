ffb2fa807552c2c29aed83da2c84fe68f12240b1
wu <wu@.com>
2025-06-10 21:56:43 +0800

    dlenski/aztec_code_generator

--- setup.py
+++ setup.py
@@ -1,33 +1,29 @@
-from setuptools import setup
+#!/usr/bin/env python3
+import sys
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
 
-setup(
-    name='rnc2rng',
-    version='2.6.1',
-    url='https://github.com/djc/rnc2rng',
-    author='David Mertz',
-    description='RELAX NG Compact to regular syntax conversion library',
-    maintainer='Dirkjan Ochtman',
-    maintainer_email='dirkjan@ochtman.nl',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'License :: Public Domain',
-        'Topic :: Text Processing :: Markup :: XML',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    packages=['rnc2rng'],
-    entry_points={
-        'console_scripts': [
-            'rnc2rng = rnc2rng.__main__:main',
-        ],
-    },
-    use_2to3=True,
-    install_requires=['rply'],
-)
+if sys.version_info < (3,4):
+    sys.exit("Python 3.4+ is required; you are using %s" % sys.version)
+
+setup(name="aztec_code_generator",
+      version="0.6",
+      description='Aztec Code generator in Python',
+      long_description=open('description.rst').read(),
+      author='Dmitry Alimov',
+      author_email="dvalimov@gmail.com",
+      install_requires=open('requirements.txt').readlines(),
+      extras_require={
+          "Image": [
+              "pillow>=3.0,<6.0; python_version < '3.5'",
+              "pillow>=3.0,<8.0; python_version >= '3.5' and python_version < '3.6'",
+              "pillow>=8.0; python_version >= '3.6'",
+          ]
+      },
+      tests_require=open('requirements-test.txt').readlines(),
+      license='MIT',
+      url="https://github.com/dlenski/aztec_code_generator",
+      py_modules=["aztec_code_generator"],
+      )
