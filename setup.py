8fd895f8393c863232fe9f7157d6353b3338cf5f
wu <wu@.com>
2025-06-10 21:27:55 +0800

    django-tastypie/django-tastypie

--- setup.py
+++ setup.py
@@ -1,53 +1,57 @@
-import os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
-from setuptools import setup
+
+from tastypie import __version__
+
 
 setup(
-    name="django-recurrence",
-    use_scm_version=True,
-    license="BSD",
-    description="Django utility wrapping dateutil.rrule",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    author="Tamas Kemenczy",
-    author_email="tamas.kemenczy@gmail.com",
-    url="https://github.com/django-recurrence/django-recurrence",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Environment :: Plugins",
-        "Framework :: Django",
-        "Framework :: Django",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.2",
-        "Framework :: Django :: 4.0",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: Implementation",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
+    name='django-tastypie',
+    version=__version__,
+    description='A flexible & capable API layer for Django.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='https://github.com/django-tastypie/django-tastypie',
+    long_description=open('README.rst', 'r').read(),
+    packages=[
+        'tastypie',
+        'tastypie.utils',
+        'tastypie.management',
+        'tastypie.management.commands',
+        'tastypie.migrations',
+        'tastypie.contrib',
+        'tastypie.contrib.gis',
+        'tastypie.contrib.contenttypes',
     ],
-    python_requires=">=3.7",
-    install_requires=["django>=2.2", "python-dateutil"],
-    setup_requires=["setuptools_scm"],
-    packages=["recurrence", "recurrence.migrations"],
-    package_dir={"recurrence": "recurrence"},
     package_data={
-        "recurrence": [
-            os.path.join("static", "*.css"),
-            os.path.join("static", "*.png"),
-            os.path.join("static", "*.js"),
-            os.path.join("locale", "*.po"),
-            os.path.join("locale", "*.mo"),
-        ]
+        'tastypie': ['templates/tastypie/*'],
     },
     zip_safe=False,
-    include_package_data=True,
+    requires=[
+        'python_mimeparse(>=0.1.4, !=1.5)',
+        'dateutil(>=1.5, !=2.0)',
+    ],
+    install_requires=[
+        'python-mimeparse >= 0.1.4, != 1.5',
+        'python-dateutil >= 1.5, != 2.0',
+    ],
+    tests_require=['mock', 'PyYAML', 'lxml', 'defusedxml'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
 )
