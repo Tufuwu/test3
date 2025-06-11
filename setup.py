ac64f292470589e30294d6acd6fac63da279c4ba
wu <wu@.com>
2025-06-11 15:48:48 +0800

    eliben/pss

--- setup.py
+++ setup.py
@@ -1,84 +1,63 @@
-# -*- coding: utf-8 -*-
-#  Licensed to Elasticsearch B.V. under one or more contributor
-#  license agreements. See the NOTICE file distributed with
-#  this work for additional information regarding copyright
-#  ownership. Elasticsearch B.V. licenses this file to you under
-#  the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
+#-------------------------------------------------------------------------------
+# pss: setup.py
 #
-# 	http://www.apache.org/licenses/LICENSE-2.0
+# Setup/installation script.
 #
-#  Unless required by applicable law or agreed to in writing,
-#  software distributed under the License is distributed on an
-#  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-#  KIND, either express or implied.  See the License for the
-#  specific language governing permissions and limitations
-#  under the License.
+# Eli Bendersky (eliben@gmail.com)
+# This code is in the public domain
+#-------------------------------------------------------------------------------
+import os, sys
+try:
+    from setuptools import setup
+    use_setuptools = True
+except ImportError:
+    from distutils.core import setup
+    use_setuptools = False
 
-from os.path import dirname, join
+if use_setuptools:
+    # Setuptools provides an "entry points" facility to automatically generate
+    # scripts that work in the same way as the supplied "pss" script. This
+    # feature is more portable to other platforms than providing a manually
+    # created script, so we use that feature if it is available.
+    # By using entry points, we get a "pss" shell script on Unix, and a
+    # "pss.exe" command on Windows, without any extra effort.
+    extra_args = {
+        'entry_points': {
+            'console_scripts': 'pss = psslib.pss:main'
+        },
+    }
+else:
+    # Setuptools is not available, so fall back to custom built scripts.
+    extra_args = {
+        'scripts': ['scripts/pss.py', 'scripts/pss'],
+    }
 
-from setuptools import find_packages, setup
 
-VERSION = (7, 2, 0)
-__version__ = VERSION
-__versionstr__ = ".".join(map(str, VERSION))
+try:
+    with open('README.rst', 'rt') as readme:
+        description = '\n' + readme.read()
+except IOError:
+    # maybe running setup.py from some other dir
+    description = ''
 
-f = open(join(dirname(__file__), "README"))
-long_description = f.read().strip()
-f.close()
-
-install_requires = [
-    "six",
-    "python-dateutil",
-    "elasticsearch>=7.0.0,<8.0.0",
-    # ipaddress is included in stdlib since python 3.3
-    'ipaddress; python_version<"3.3"',
-]
-
-develop_requires = [
-    "mock",
-    "pytest>=3.0.0",
-    "pytest-cov",
-    "pytest-mock<3.0.0",
-    "pytz",
-    "coverage<5.0.0",
-    "sphinx",
-    "sphinx_rtd_theme",
-]
 
 setup(
-    name="elasticsearch-dsl",
-    description="Python client for Elasticsearch",
-    license="Apache-2.0",
-    url="https://github.com/elasticsearch/elasticsearch-dsl-py",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-    version=__versionstr__,
-    author="Honza Král",
-    author_email="honza.kral@gmail.com",
-    maintainer="Seth Michael Larson",
-    maintainer_email="seth.larson@elastic.co",
-    packages=find_packages(where=".", exclude=("tests*",)),
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Apache Software License",
-        "Intended Audience :: Developers",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-    ],
-    install_requires=install_requires,
-    extras_require={"develop": develop_requires},
+    # metadata
+    name='pss',
+    description='Tool for grepping through source code',
+    long_description=description,
+    license='Public domain',
+    version='1.42',
+    author='Eli Bendersky',
+    maintainer='Eli Bendersky',
+    author_email='eliben@gmail.com',
+    url='https://github.com/eliben/pss',
+    platforms='Cross Platform',
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',],
+
+    packages=['psslib', 'psslib.colorama'],
+
+    **extra_args
 )
