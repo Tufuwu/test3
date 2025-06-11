a9a1b9fd8d2fa0d07ecfb5608265e254e0549355
wu <wu@.com>
2025-06-11 14:12:08 +0800

    dylan-profiler/visions

--- setup.py
+++ setup.py
@@ -1,23 +1,70 @@
-from distutils.core import setup
-import sys
+from pathlib import Path
 
-import fitparse
+from setuptools import find_packages, setup
 
+# Read the contents of README file
+source_root = Path(".")
+with (source_root / "README.rst").open(encoding="utf-8") as f:
+    long_description = f.read()
 
-requires = None
-if sys.version_info < (2, 7) or (3, 0) <= sys.version_info < (3, 3):
-    sys.exit("Python 2.7 or Python 3.3+ are required.")
+# Read the requirements
+with (source_root / "requirements.txt").open(encoding="utf8") as f:
+    requirements = f.readlines()
+
+with (source_root / "requirements_dev.txt").open(encoding="utf8") as f:
+    dev_requirements = f.readlines()
+
+with (source_root / "requirements_test.txt").open(encoding="utf8") as f:
+    test_requirements = f.readlines()
+
+type_geometry_requires = ["shapely"]
+type_image_path_requires = ["imagehash", "Pillow"]
+
+extras_requires = {
+    "type_geometry": type_geometry_requires,
+    "type_image_path": type_image_path_requires,
+    "plotting": ["pydot", "pygraphviz", "matplotlib"],
+    "dev": dev_requirements,
+    "test": test_requirements,
+}
+
+extras_requires["all"] = requirements + [
+    dependency
+    for name, dependencies in extras_requires.items()
+    if name.startswith("type_") or name == "plotting"
+    for dependency in dependencies
+]
+
+__version__ = None
+with (source_root / "src/visions/version.py").open(encoding="utf8") as f:
+    exec(f.read())
 
 
 setup(
-    name='fitparse',
-    version=fitparse.__version__,
-    description='Python library to parse ANT/Garmin .FIT files',
-    author='David Cooper',
-    author_email='dave@kupesoft.com',
-    url='https://www.github.com/dtcooper/python-fitparse',
-    license=open('LICENSE').read(),
-    packages=['fitparse'],
-    scripts=['scripts/fitdump'],  # Don't include generate_profile.py
-    install_requires=requires,
+    name="visions",
+    version=__version__,
+    url="https://github.com/dylan-profiler/visions",
+    description="Visions",
+    license="BSD License",
+    author="Dylan Profiler",
+    author_email="visions@ictopzee.nl",
+    package_data={"vision": ["py.typed"]},
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    include_package_data=True,
+    extras_require=extras_requires,
+    tests_require=test_requirements,
+    python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
