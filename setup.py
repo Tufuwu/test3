62ab706ce7a3ce45051b88e58bcac34c151ca51c
wu <wu@.com>
2025-06-11 15:37:55 +0800

    ei-corebioinformatics/mikado

--- setup.py
+++ setup.py
@@ -1,58 +1,111 @@
-# -*- coding: utf-8 -*-
-#
-# This software may be modified and distributed under the terms
-# of the MIT license.  See the LICENSE file for details.
+# coding: utf-8
 
+"""Setup file for PyPI"""
+
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+from distutils.extension import Extension
+from Cython.Build import cythonize
+from codecs import open
 from os import path
-from setuptools import setup
-from shutil import rmtree
+import glob
+import re
 import sys
-
-NAME = 'python-logstash-async'
-VERSION = '2.3.0'
-
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'README.rst'), 'rb') as f:
-    LONG_DESCRIPTION = f.read().decode('utf-8')
+import numpy as np
+from scipy._build_utils import numpy_nodepr_api
 
 
-if 'bdist_wheel' in sys.argv:
-    # Remove previous build dir when creating a wheel build, since if files have been removed
-    # from the project, they'll still be cached in the build dir and end up as part of the
-    # build, which is really neat!
-    for directory in ('build', 'dist', 'python_logstash_async.egg-info'):
-        rmtree(directory, ignore_errors=True)
+here = path.abspath(path.dirname("__file__"))
 
+with open(path.join(here, "DESCRIPTION.md"), encoding="utf-8") as description:
+    long_description = description.read()
+
+version = {}
+with open(path.join(here, "Mikado", "version.py")) as fp:
+    exec(fp.read(), version)
+version = version["__version__"]
+
+if version is None:
+    print("No version found, exiting", file=sys.stderr)
+    sys.exit(1)
+
+if sys.version_info.major != 3:
+    raise EnvironmentError("""Mikado is a pipeline specifically programmed for python3,
+    and is not compatible with Python2. Please upgrade your python before proceeding!""")
+
+extensions = [Extension("Mikado.utilities.overlap",
+                        sources=[path.join("Mikado", "utilities", "overlap.pyx")],
+                        **numpy_nodepr_api),
+              Extension("Mikado.utilities.f1",
+                        sources=[path.join("Mikado", "utilities", "f1.pyx")],
+                        **numpy_nodepr_api),
+              Extension("Mikado.scales.contrast",
+                        sources=[path.join("Mikado", "scales", "contrast.pyx")],
+                        **numpy_nodepr_api),
+              Extension("Mikado.utilities.intervaltree",
+                        sources=[path.join("Mikado", "utilities", "intervaltree.pyx")],
+                        **numpy_nodepr_api),
+              Extension("Mikado.serializers.blast_serializer.btop_parser",
+                        include_dirs=[np.get_include()],
+                        language="c++",
+                        sources=[path.join("Mikado", "serializers", "blast_serializer", "btop_parser.pyx")],
+                        **numpy_nodepr_api),
+              Extension("Mikado.serializers.blast_serializer.aln_string_parser",
+                        include_dirs=[np.get_include()],
+                        language="c++",
+                        sources=[path.join("Mikado", "serializers", "blast_serializer", "aln_string_parser.pyx")],
+                        **numpy_nodepr_api)
+              ]
 
 setup(
-    name=NAME,
-    packages=['logstash_async'],
-    version=VERSION,
-    description='Asynchronous Python logging handler for Logstash.',
-    long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/x-rst',
-    license='MIT',
-    author='Enrico Tröger',
-    author_email='enrico.troeger@uvena.de',
-    url='https://github.com/eht16/python-logstash-async',
-    project_urls={
-        'Travis CI': 'https://travis-ci.org/eht16/python-logstash-async/',
-        'Source code': 'https://github.com/eht16/python-logstash-async/',
-        'Documentation': 'https://python-logstash-async.readthedocs.io/en/stable/',
-    },
-    keywords='logging logstash asynchronous',
-    install_requires=['limits', 'pylogbeat', 'requests'],
-    python_requires='>3.5',
-    include_package_data=True,
+    name="Mikado",
+    version=version,
+    description="A Python3 annotation program to select the best gene model in each locus",
+    long_description=long_description,
+    url="https://github.com/EI-CoreBioinformatics/mikado",
+    author="Luca Venturini",
+    author_email="lucventurini@gmail.com",
+    license="LGPL3",
+    tests_require=["pytest"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Logging',
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Framework :: Pytest",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: 3.7'
+    ],
+    ext_modules=cythonize(extensions, compiler_directives = {"language_level": "3"}),
+    zip_safe=False,
+    keywords="rna-seq annotation genomics transcriptomics",
+    packages=find_packages(),
+    scripts=glob.glob("util/*.py"),
+    entry_points={"console_scripts": ["mikado = Mikado.__main__:main",
+                                      "daijin = Mikado.daijin:main",
+                                      ]},
+    install_requires=[line.rstrip() for line in open("requirements.txt", "rt")],
+    extras_require={
+        "postgresql": ["psycopg2"],
+        "mysql": ["mysqlclient>=1.3.6"],
+        "bam": ["pysam>=0.8"]
+    },
+    # test_suite="nose2.collector.collector",
+    package_data={
+        "Mikado.configuration":
+            glob.glob("Mikado/configuration/*json") + glob.glob("Mikado/configuration/*yaml"),
+        "Mikado.configuration.scoring_files":
+            glob.glob("Mikado/configuration/scoring_files/*"),
+        "Mikado":
+            glob.glob(path.join("Mikado", "daijin", "*smk")) +
+            glob.glob(path.join("Mikado", "daijin", "*yaml")) + glob.glob("Mikado/daijin/*json"),
+        "Mikado.utilities.overlap": [path.join("Mikado", "utilities", "overlap.pxd")],
+        "Mikado.utilities.intervaltree": [path.join("Mikado", "utilities", "intervaltree.pxd")],
+        "Mikado.serializers.blast_serializers": glob.glob(path.join("Mikado", "serializers", "blast_serializers",
+                                                                    "*pxd")),
+        "Mikado.tests.blast_data": glob.glob(path.join("Mikado", "tests", "blast_data", "*"))
+        },
+    include_package_data=True
 )
