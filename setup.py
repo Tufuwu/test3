62bbb758e61cd3b377b477bbc9fd4f7d5121bf7e
wu <wu@.com>
2025-06-12 14:55:48 +0800

    fusionbox\django-authtools

--- setup.py
+++ setup.py
@@ -1,71 +1,47 @@
+#!/usr/bin/env python
+import io
 import os
 
 from setuptools import setup, find_packages
 
-
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+__doc__ = "Custom user model app for Django featuring email as username."
 
 
 def read(fname):
-    with open(fname) as fp:
-        return fp.read()
+    return io.open(os.path.join(os.path.dirname(__file__), fname), encoding="utf-8").read()
 
 
-try:
-    import pypandoc
-
-    long_description = pypandoc.convert("README.md", "rst")
-except (IOError, ImportError, OSError):
-    long_description = read("README.md")
-
-
-REQUIREMENTS_FILE = "requirements.txt"
-REQUIREMENTS = open(os.path.join(PROJECT_DIR, REQUIREMENTS_FILE)).readlines()
-
-REQUIREMENTS_TESTS_FILE = "requirements-test.txt"
-REQUIREMENTS_TESTS = open(
-    os.path.join(PROJECT_DIR, REQUIREMENTS_TESTS_FILE)
-).readlines()
-
-REQUIREMENTS_TOX_FILE = "requirements-tox.txt"
-REQUIREMENTS_TOX = open(os.path.join(PROJECT_DIR, REQUIREMENTS_TOX_FILE)).readlines()
-
-EXTRAS_REQUIRE = {
-    "enum": ["marshmallow-enum"],
-    "union": ["marshmallow-union"],
-}
-
+install_requires = [
+    'Django>=2.2',
+]
 
 setup(
-    name="marshmallow-jsonschema",
-    version="0.11.0",
-    description="JSON Schema Draft v4 (http://json-schema.org/)"
-    " formatting with marshmallow",
-    long_description=long_description,
-    author="Stephen Fuhry",
-    author_email="fuhrysteve@gmail.com",
-    url="https://github.com/fuhrysteve/marshmallow-jsonschema",
-    packages=find_packages(exclude=("test*",)),
-    package_dir={"marshmallow-jsonschema": "marshmallow-jsonschema"},
-    include_package_data=True,
-    install_requires=REQUIREMENTS,
-    tests_require=REQUIREMENTS_TESTS + REQUIREMENTS_TOX,
-    extras_require=EXTRAS_REQUIRE,
-    license=read("LICENSE"),
+    name='django-authtools',
+    version='2.0',
+    author='Fusionbox, Inc.',
+    author_email='programmers@fusionbox.com',
+    description=__doc__,
+    long_description='\n\n'.join([read('README.rst'), read('CHANGES.rst')]),
+    url='https://django-authtools.readthedocs.org/',
+    license='BSD',
+    packages=[package for package in find_packages() if package.startswith('authtools')],
+    install_requires=install_requires,
     zip_safe=False,
-    keywords=(
-        "marshmallow-jsonschema marshmallow schema serialization "
-        "jsonschema validation"
-    ),
+    include_package_data=True,
     classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
-    test_suite="tests",
 )
