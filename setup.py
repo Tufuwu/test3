d37b2b817928177945026bc7f31c215d0ea09a9e
wu <wu@.com>
2025-06-12 12:40:10 +0800

    fbpic/fbpic

--- setup.py
+++ setup.py
@@ -1,27 +1,15 @@
 #!/bin/env python
 
-# Copyright 2016, FBPIC contributors
-# Authors: Remi Lehe, Manuel Kirchen, Kevin Peters, Soeren Jalas
-# License: 3-Clause-BSD-LBNL
-import sys
+import fbpic
 from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import fbpic # In order to extract the version number
 
-# Obtain the long description from README.md
+# 读取说明文档
 with open('./README.md') as f:
     long_description = f.read()
-# Get the package requirements from the requirements.txt file
+
+# 读取依赖项
 with open('requirements.txt') as f:
-    install_requires = [ line.strip('\n') for line in f.readlines() ]
-
-# Define a custom class to run the py.test with `python setup.py test`
-class PyTest(TestCommand):
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(['--ignore=tests/unautomated', '--durations=10'])
-        sys.exit(errcode)
+    install_requires = [line.strip() for line in f.readlines()]
 
 setup(
     name='fbpic',
@@ -33,11 +21,9 @@ setup(
     maintainer_email='remi.lehe@normalesup.org',
     license='BSD-3-Clause-LBNL',
     packages=find_packages('.'),
-    tests_require=['more-itertools<6.0.0', 'pytest', 'openpmd_viewer'],
-    cmdclass={'test': PyTest},
     install_requires=install_requires,
-    extras_require = {
-        'picmi':  ["picmistandard", "numexpr", "periodictable"],
+    extras_require={
+        'test': ["pytest", "more-itertools<6.0.0", "openpmd_viewer"]
     },
     include_package_data=True,
     platforms='any',
@@ -45,12 +31,12 @@ setup(
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 3 - Alpha',
-        'Natural Language :: English',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: Physics',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'],
-    )
+        'Programming Language :: Python :: 3.5',
+    ],
+)
