4fcfd521bb04bb3b8ccb50ca8d055d744e0969a0
wu <wu@.com>
2025-06-14 16:14:18 +0800

    hatchet/hatchet

--- setup.py
+++ setup.py
@@ -1,82 +1,56 @@
-"""
-Mizani
+# Copyright 2017-2021 Lawrence Livermore National Security, LLC and other
+# Hatchet Project Developers. See the top-level LICENSE file for details.
+#
+# SPDX-License-Identifier: MIT
 
-Mizani is a scales package for graphics. It is based on Hadley
-Wickham's *Scales* package.
-"""
+from setuptools import setup
+from setuptools import Extension
+from codecs import open
+from os import path
 
-from setuptools import setup, find_packages
+here = path.abspath(path.dirname(__file__))
 
-import versioneer
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
-
-__author__ = 'Hassan Kibirige'
-__email__ = 'has2k1@gmail.com'
-__description__ = "Scales for Python"
-__license__ = 'BSD (3-clause)'
-__url__ = 'https://github.com/has2k1/mizani'
-__classifiers__ = [
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3',
-    'Topic :: Scientific/Engineering :: Visualization',
-]
+# Get the version in a safe way which does not refrence hatchet `__init__` file
+# per python docs: https://packaging.python.org/guides/single-sourcing-package-version/
+version = {}
+with open("./hatchet/version.py") as fp:
+    exec(fp.read(), version)
 
 
-def check_dependencies():
-    """
-    Check for system level dependencies
-    """
-    pass
-
-
-def get_required_packages():
-    """
-    Return required packages
-
-    Plus any version tests and warnings
-    """
-    install_requires = ['numpy',
-                        'pandas >= 1.1.0',
-                        'matplotlib >= 3.1.1',
-                        'palettable']
-    return install_requires
-
-
-def get_package_data():
-    """
-    Return package data
-
-    For example:
-
-        {'': ['*.txt', '*.rst'],
-         'hello': ['*.msg']}
-
-    means:
-        - If any package contains *.txt or *.rst files,
-          include them
-        - And include any *.msg files found in
-          the 'hello' package, too:
-    """
-    return {}
-
-
-if __name__ == '__main__':
-
-    check_dependencies()
-
-    setup(name='mizani',
-          maintainer=__author__,
-          maintainer_email=__email__,
-          description=__description__,
-          long_description=__doc__,
-          license=__license__,
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass(),
-          url=__url__,
-          python_requires='>=3.6',
-          install_requires=get_required_packages(),
-          packages=find_packages(),
-          package_data=get_package_data(),
-          classifiers=__classifiers__
-          )
+setup(
+    name="hatchet",
+    version=version["__version__"],
+    description="A Python library for analyzing hierarchical performance data",
+    url="https://github.com/hatchet/hatchet",
+    author="Abhinav Bhatele",
+    author_email="bhatele@cs.umd.edu",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="",
+    packages=[
+        "hatchet",
+        "hatchet.readers",
+        "hatchet.util",
+        "hatchet.external",
+        "hatchet.tests",
+        "hatchet.cython_modules.libs",
+    ],
+    install_requires=["pydot", "PyYAML", "matplotlib", "numpy", "pandas"],
+    ext_modules=[
+        Extension(
+            "hatchet.cython_modules.libs.reader_modules",
+            ["hatchet/cython_modules/reader_modules.c"],
+        ),
+        Extension(
+            "hatchet.cython_modules.libs.graphframe_modules",
+            ["hatchet/cython_modules/graphframe_modules.c"],
+        ),
+    ],
+)
