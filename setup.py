ab0a67f346d45992a29ff963d75a88ab118be780
wu <wu@.com>
2025-06-14 16:38:53 +0800

    hbehrens/puncover

--- setup.py
+++ setup.py
@@ -1,56 +1,72 @@
-# Copyright 2017-2021 Lawrence Livermore National Security, LLC and other
-# Hatchet Project Developers. See the top-level LICENSE file for details.
-#
-# SPDX-License-Identifier: MIT
+#!/usr/bin/env python
+import os
+from pathlib import Path
 
-from setuptools import setup
-from setuptools import Extension
-from codecs import open
-from os import path
+from setuptools import setup, find_packages, Command
 
-here = path.abspath(path.dirname(__file__))
+ROOTDIR = Path(__file__).parent
 
-# Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+__version__ = None  # Overwritten by executing version.py.
+with open(ROOTDIR / "puncover/version.py") as f:
+    exec(f.read())
 
-# Get the version in a safe way which does not refrence hatchet `__init__` file
-# per python docs: https://packaging.python.org/guides/single-sourcing-package-version/
-version = {}
-with open("./hatchet/version.py") as fp:
-    exec(fp.read(), version)
+
+with open(ROOTDIR / "requirements-test.txt") as f:
+    tests_require = list(filter(lambda x: not x.strip().startswith('-r'), f.readlines()))
+
+with open(ROOTDIR / "requirements.txt") as f:
+    requires = f.readlines()
+
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+
+    # http://stackoverflow.com/a/3780822/196350
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info")
 
 
 setup(
-    name="hatchet",
-    version=version["__version__"],
-    description="A Python library for analyzing hierarchical performance data",
-    url="https://github.com/hatchet/hatchet",
-    author="Abhinav Bhatele",
-    author_email="bhatele@cs.umd.edu",
+    name="puncover",
+    version=__version__,
+    description="Analyses C/C++ build output for code size, static variables, and stack usage.",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    url="https://github.com/hbehrens/puncover",
+    download_url="https://github.com/hbehrens/puncover/tarball/%s" % __version__,
+    author="Heiko Behrens",
     license="MIT",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    keywords="",
-    packages=[
-        "hatchet",
-        "hatchet.readers",
-        "hatchet.util",
-        "hatchet.external",
-        "hatchet.tests",
-        "hatchet.cython_modules.libs",
-    ],
-    install_requires=["pydot", "PyYAML", "matplotlib", "numpy", "pandas"],
-    ext_modules=[
-        Extension(
-            "hatchet.cython_modules.libs.reader_modules",
-            ["hatchet/cython_modules/reader_modules.c"],
-        ),
-        Extension(
-            "hatchet.cython_modules.libs.graphframe_modules",
-            ["hatchet/cython_modules/graphframe_modules.c"],
-        ),
-    ],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={"console_scripts": ["puncover = puncover.puncover:main"]},
+    install_requires=requires,
+    tests_require=tests_require,
+    cmdclass={
+        "clean": CleanCommand,
+    },
+    # TODO: https://github.com/HBehrens/puncover/issues/36
+    #  Fix Python 3.5
+    python_requires=">=3.6",
 )
