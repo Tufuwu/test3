a58f989499c53bd787d4dc524a4c226026b41e9c
wu <wu@.com>
2025-06-17 13:07:22 +0800

    keras-team/keras-tuner

--- setup.py
+++ setup.py
@@ -1,63 +1,68 @@
-import os
+# Copyright 2019 The Keras Tuner Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+"""Setup script."""
+
+from __future__ import absolute_import
+
+from setuptools import find_packages
 from setuptools import setup
-from e3fp import version
 
-ON_RTD = os.environ.get("READTHEDOCS") == "True"
-
-
-requirements = [
-    "scipy>=0.18.0",
-    "numpy>=1.11.3",
-    "mmh3>=2.3.1",
-    "sdaxen_python_utilities>=0.1.4",
-]
-if ON_RTD:  # ReadTheDocs can't handle C libraries
-    requirements = requirements[-1:] + ["mock"]
-
-test_requirements = ["pytest", "mock"]
-
-classifiers = [
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-    "Operating System :: OS Independent",
-    "Development Status :: 4 - Beta",
-    "Intended Audience :: Science/Research",
-    "Intended Audience :: Developers",
-    "Topic :: Scientific/Engineering :: Chemistry",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-]
-
-
-def get_readme():
-    with open("README.rst") as f:
-        return f.read()
-
+version = '1.0.3'
 
 setup(
-    name="e3fp",
-    packages=[
-        "e3fp",
-        "e3fp.config",
-        "e3fp.conformer",
-        "e3fp.fingerprint",
-        "e3fp.test",
-    ],
+    name='keras-tuner',
     version=version,
-    description="Molecular 3D fingerprinting",
-    long_description=get_readme(),
-    keywords="e3fp 3d molecule fingerprint conformer",
-    author="Seth Axen",
-    author_email="seth.axen@gmail.com",
-    license="LGPLv3",
-    url="https://github.com/keiserlab/e3fp",
-    classifiers=classifiers,
-    download_url="https://github.com/keiserlab/e3fp/tarball/" + version,
-    install_requires=requirements,
-    include_package_data=True,
-    tests_require=test_requirements,
+    description='Hypertuner for Keras',
+    url='https://github.com/keras-team/keras-tuner',
+    author='The Keras Tuner authors',
+    author_email='kerastuner@google.com',
+    license='Apache License 2.0',
+    # tensorflow isn't a dependency because it would force the
+    # download of the gpu version or the cpu version.
+    # users should install it manually.
+    install_requires=[
+        'packaging',
+        'future',
+        'numpy',
+        'tabulate',
+        'terminaltables',
+        'colorama',
+        'tqdm',
+        'requests',
+        'scipy',
+        'scikit-learn',
+        'tensorboard',
+        'ipython'
+    ],
+    extras_require={
+        'tests': ['pytest',
+                  'flake8',
+                  'mock',
+                  'portpicker',
+                  'pytest-xdist',
+                  'pytest-cov'],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development'
+    ],
+    packages=find_packages(exclude=('tests',))
 )
