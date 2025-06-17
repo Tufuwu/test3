edad4e987a3d3123fc0a32849c8141cb96c14972
wu <wu@.com>
2025-06-17 21:48:24 +0800

    lgoodridge/django-uniauth

--- setup.py
+++ setup.py
@@ -1,53 +1,43 @@
-#!/usr/bin/env python
+import setuptools
 
-import sys
-from setuptools import setup, find_packages
-from io import open
+with open("README.md", "r") as readme:
+    long_description = readme.read()
 
-
-needs_wheel = {'bdist_wheel'}.intersection(sys.argv)
-wheel = ['wheel'] if needs_wheel else []
-
-with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()
-
-setup(
-    name="ufoProcessor",
-    use_scm_version={"write_to": "Lib/ufoProcessor/_version.py"},
-    description="Read, write and generate UFOs with designspace data.",
+setuptools.setup(
+    name="django-uniauth",
+    version="1.4.1",
+    author="Lance Goodridge",
+    author_email="ldgoodridge95@gmail.com",
+    keywords=["django", "auth", "authentication", "cas", "sso", "single sign-on"],
+    description="A Django app for managing CAS and custom user authentication.",
+    include_package_data=True,
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author="Erik van Blokland",
-    author_email="erik@letterror.com",
-    url="https://github.com/LettError/ufoProcessor",
-    keywords='font development tools',
-    license="MIT",
-    packages=find_packages("Lib"),
-    package_dir={"": "Lib"},
-    python_requires='>=2.7',
-    setup_requires=wheel + ["setuptools_scm"],
+    long_description_content_type="text/markdown",
+    url="https://github.com/lgoodridge/django-uniauth",
+    license='LGPLv3',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[
-        "defcon[lxml]>=0.6.0",
-        "fontMath>=0.4.9",
-        "fontParts>=0.8.2",
-        "fontTools[ufo,lxml]>=3.32.0",
-        "mutatorMath>=2.1.2",
+        "Django>=1.11",
+        "python-cas>=1.4.0",
+        "djangorestframework-simplejwt>=4.1.0",
     ],
+    extras_require = {
+        ":python_version<='3.2'": ["mock"],
+    },
+    packages=setuptools.find_packages(exclude=["demo-app",]),
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Environment :: Console",
-        "Environment :: Other Environment",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2",
+        "Framework :: Django :: 3",
+        "Framework :: Django :: 4",
         "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
-        "Topic :: Multimedia :: Graphics",
-        "Topic :: Multimedia :: Graphics :: Graphics Conversion",
-        "Topic :: Multimedia :: Graphics :: Editors :: Vector-Based",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
+    ]
 )
