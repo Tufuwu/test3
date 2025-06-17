b0d890426ac5bc9b4c6063c50cfcad9459dece9c
wu <wu@.com>
2025-06-17 14:33:02 +0800

    kolotaev/vakt

--- setup.py
+++ setup.py
@@ -1,69 +1,76 @@
-import os
-from setuptools import setup, Command
-
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
-
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+from setuptools import setup, find_packages
+from os import path
 
 
-class PyTest(Command):
-    user_options = []
+here = path.abspath(path.dirname(__file__))
+about = {}
+with open(path.join(here, 'vakt', 'version.py'), mode='r', encoding='utf-8') as f:
+    exec(f.read(), about)
 
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import subprocess
-        import sys
-
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
-setup(
-    name='django-q',
-    version='1.3.4',
-    author='Ilan Steemers',
-    author_email='koed00@gmail.com',
-    keywords='django multiprocessing worker scheduler queue',
-    packages=['django_q'],
-    include_package_data=True,
-    url='https://django-q.readthedocs.org',
-    license='MIT',
-    description='A multiprocessing distributed task queue for Django',
-    long_description=README,
-    install_requires=['django>=2.2', 'django-picklefield', 'blessed', 'arrow'],
-    test_requires=['pytest', 'pytest-django', ],
-    cmdclass={'test': PyTest},
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
-    entry_points={
-        'djangoq.errorreporters': [
-            'rollbar = django_q_rollbar:Rollbar',
-            'sentry = django_q_sentry:Sentry',
-        ]
-    },
-    extras_require={
-        'rollbar': ["django-q-rollbar>=0.1"],
-        'sentry': ["django-q-sentry>=0.1"],
-    }
-)
+if __name__ == '__main__':
+    setup(
+        name='vakt',
+        description='Attribute-based access control (ABAC) SDK for Python',
+        keywords='ACL ABAC access-control policy security authorization permission',
+        version=about['__version__'],
+        author='Egor Kolotaev',
+        author_email='ekolotaev@gmail.com',
+        license="Apache 2.0 license",
+        url='http://github.com/kolotaev/vakt',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        py_modules=['vakt'],
+        python_requires='>=3.4',
+        install_requires=[
+            'jsonpickle>=1.0',
+        ],
+        extras_require={
+            # Dev dependencies are not using pinned compatibility versions
+            # in order to support a wide array of Python versions
+            'dev': [
+                'pytest',
+                'pytest-cov',
+                'pylint',
+                'PyMySQL',
+                'psycopg2cffi~=2.8',
+            ],
+            'mongo': [
+                'pymongo~=3.5',
+            ],
+            'sql': [
+                'SQLAlchemy~=1.3',
+                # 'typing', # sqlalchemy imports typing since v1.3.24
+            ],
+            'redis': [
+                'redis~=3.3'
+            ],
+        },
+        packages=find_packages(exclude='tests'),
+        classifiers=[
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: Apache Software License',
+            'Operating System :: OS Independent',
+            'Topic :: System :: Systems Administration',
+            'Topic :: System :: Networking',
+            'Topic :: System :: Networking :: Firewalls',
+            'Topic :: Security',
+            'Topic :: Software Development',
+            'Topic :: Utilities',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: Implementation :: PyPy',
+            'Programming Language :: Python :: Implementation :: CPython',
+        ],
+    )
