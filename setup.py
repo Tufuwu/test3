994815b97815da938c2a9ec8fb520c1e2799a6a1
wu <wu@.com>
2025-06-19 16:23:01 +0800

    nexpy/nexpy

--- setup.py
+++ setup.py
@@ -1,100 +1,66 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-# SPDX-License-Identifier: Apache-2.0
+#-----------------------------------------------------------------------------
+# Copyright (c) 2013-2020, NeXpy Development Team.
 #
-# http://nexb.com and https://github.com/nexB/scancode.io
-# The ScanCode.io software is licensed under the Apache License version 2.0.
-# Data generated with ScanCode.io is provided as-is without warranties.
-# ScanCode is a trademark of nexB Inc.
+# Distributed under the terms of the Modified BSD License.
 #
-# You may not use this software except in compliance with the License.
-# You may obtain a copy of the License at: http://apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software distributed
-# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-# CONDITIONS OF ANY KIND, either express or implied. See the License for the
-# specific language governing permissions and limitations under the License.
-#
-# Data Generated with ScanCode.io is provided on an "AS IS" BASIS, WITHOUT WARRANTIES
-# OR CONDITIONS OF ANY KIND, either express or implied. No content created from
-# ScanCode.io should be considered or used as legal advice. Consult an Attorney
-# for any legal advice.
-#
-# ScanCode.io is a free software code scanning tool from nexB Inc. and others.
-# Visit https://github.com/nexB/scancode.io for support and download.
+# The full license is in the file COPYING, distributed with this software.
+#-----------------------------------------------------------------------------
 
-from setuptools import find_packages
-from setuptools import setup
+from setuptools import setup, find_packages, Extension
 
-__version__ = "1.0.6"
+import os, sys
+import versioneer
 
-requirement_files = ["etc/requirements/base.txt"]
+# pull in some definitions from the package's __init__.py file
+sys.path.insert(0, os.path.join('src', ))
+import nexpy
+import nexpy.requires
 
-all_requirements = [
-    r.strip()
-    for req_file in requirement_files
-    for r in open(req_file).readlines()
-    if r.strip() and not r.strip().startswith("#")
-]
+verbose=1
 
-setup(
-    name="scancodeio",
-    version=__version__,
-    license="Apache-2.0",
-    description="ScanCode.io",
-    long_description="ScanCode.io",
-    author="nexB Inc.",
-    author_email="info@scancode.io",
-    url="https://github.com/nexB/scancode.io",
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    python_requires=">= 3.6",
-    install_requires=all_requirements,
-    entry_points={
-        "console_scripts": [
-            "scanpipe = scancodeio:command_line",
-        ],
-    },
-    classifiers=[
-        # complete classifiers list
-        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Legal Industry",
-        "Framework :: Django",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: POSIX",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Topic :: Utilities",
-    ],
-    keywords=[
-        "open source",
-        "scan",
-        "license",
-        "package",
-        "dependency",
-        "copyright",
-        "filetype",
-        "author",
-        "extract",
-        "licensing",
-        "scancode",
-        "scanpipe",
-        "docker",
-        "rootfs",
-        "vm",
-        "virtual machine",
-        "pipeline",
-        "code analysis",
-        "container",
-    ],
-)
+setup (name =  nexpy.__package_name__,        # NeXpy
+       version=versioneer.get_version(),
+       cmdclass=versioneer.get_cmdclass(),
+       license = nexpy.__license__,
+       description = nexpy.__description__,
+       long_description = nexpy.__long_description__,
+       author=nexpy.__author_name__,
+       author_email=nexpy.__author_email__,
+       url=nexpy.__url__,
+       download_url=nexpy.__download_url__,
+       platforms='any',
+       python_requires='>=3.7',
+       install_requires = nexpy.requires.pkg_requirements,
+       extras_require = nexpy.requires.extra_requirements,
+       package_dir = {'': 'src'},
+       packages = find_packages('src'),
+       include_package_data = True,
+       package_data = {
+                       'nexpy.gui': ['resources/icon/*.svg',
+                                     'resources/icon/*.png',
+                                     'resources/*.png',
+                                    ],
+                       'nexpy.definitions': ['base_classes/*.xml'],
+                       'nexpy': [
+                           'examples/*.*',
+                           'examples/*/*.*',
+                           'examples/*/*/*.*',
+                       ],
+                   },
+       entry_points={
+            # create & install scripts in <python>/bin
+            'gui_scripts': ['nexpy = nexpy.nexpygui:main',],
+       },
+       classifiers= ['Development Status :: 4 - Beta',
+                     'Intended Audience :: Developers',
+                     'Intended Audience :: Science/Research',
+                     'License :: OSI Approved :: BSD License',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 3',
+                     'Programming Language :: Python :: 3.7',
+                     'Programming Language :: Python :: 3.8',
+                     'Programming Language :: Python :: 3.9',
+                     'Topic :: Scientific/Engineering',
+                     'Topic :: Scientific/Engineering :: Visualization'],
+      )
