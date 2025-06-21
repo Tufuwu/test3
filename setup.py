22db8bbb1fa915c5feb9d2444109032fa379c8d6
wu <wu@.com>
2025-06-21 14:37:58 +0800

    obriencj/python-javatools

--- setup.py
+++ setup.py
@@ -1,158 +1,52 @@
+#! /usr/bin/env python
+
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# <http://www.gnu.org/licenses/>.
+
+
 """
-Copyright 2017 NREL
+Python Javatools
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use
-this file except in compliance with the License. You may obtain a copy of the
-License at http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software distributed
-under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-CONDITIONS OF ANY KIND, either express or implied. See the License for the
-specific language governing permissions and limitations under the License.
+author: Christopher O'Brien  <obriencj@gmail.com>
+license: LGPL v.3
 """
 
-# This setup file was taken from https://github.com/kennethreitz/setup.py
-# accessed on April 3, 2019.
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pip install twine
-
-import io
-import os
-import sys
-from shutil import rmtree
-
-from setuptools import find_packages, setup, Command
-
-# Package meta-data.
-NAME = 'FLORIS'
-DESCRIPTION = 'A controls-oriented engineering wake model.'
-URL = 'https://github.com/NREL/FLORIS'
-EMAIL = 'rafael.mudafort@nrel.gov'
-AUTHOR = 'NREL National Wind Technology Center'
-REQUIRES_PYTHON = '>=3.3.0'
-VERSION = '1.1.4'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    # simulation
-    'matplotlib>=3',
-    'numpy==1.16.3',
-    'pytest>=4',
-    'scipy==1.1.0',
-
-    # tools
-    'h5pyd==0.3.3',
-    'pandas==0.24.2',
-    'pyproj==2.1.3',
-    'seaborn==0.9.0'
-]
-
-# What packages are optional?
-EXTRAS = {
-    'docs': {
-        'readthedocs-sphinx-ext==0.5.15',
-        'Sphinx==2.0',
-        'sphinxcontrib-napoleon==0.7'
-    }
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
+from setuptools import setup as _setup
 
 
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-        
-        sys.exit()
-
-
-# Where the magic happens:
-setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='Apache-2.0',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+PYTHON_SUPPORTED_VERSIONS = (
+    ">=2.7",
+    "!=3.0.*", "!=3.1.*", "!=3.2.*", "!=3.3.*", "!=3.4.*",
+    "<4",
 )
+
+
+def delayed_cheetah_build_py_cmd(*args, **kwds):
+    # only import cheetah_build_py_cmd immediately before
+    # instantiating it.
+    from javatools.cheetah.setuptools import cheetah_build_py_cmd
+    return cheetah_build_py_cmd(*args, **kwds)
+
+
+def setup():
+    return _setup(cmdclass={'build_py': delayed_cheetah_build_py_cmd, })
+
+
+if __name__ == '__main__':
+    setup()
+
+
+#
+# The end.
