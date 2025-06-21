ed5d2f3a301f3bac546c977daf46f4ec6a5e55bf
wu <wu@.com>
2025-06-21 18:19:27 +0800

    openshift-scale/cerberus

--- setup.py
+++ setup.py
@@ -1,45 +1,15 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+import sys
+
+from pkg_resources import VersionConflict, require
+from setuptools import setup
+
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
 
-setup(
-    name='fingerprints',
-    version='0.6.6',
-    description="A library to generate entity fingerprints.",
-    long_description="",
-    classifiers=[
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    keywords='names people companies normalisation',
-    author='Friedrich Lindenberg',
-    author_email='friedrich@pudo.org',
-    url='http://github.com/alephdata/fingerprints',
-    license='MIT',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'test']),
-    namespace_packages=[],
-    package_data={
-        '': ['fingerprints/data/types.yml']
-    },
-    include_package_data=True,
-    zip_safe=False,
-    test_suite='nose.collector',
-    install_requires=[
-        'normality>=0.4.0',
-        'pyyaml',
-        'six'
-    ],
-    tests_require=[
-        'nose',
-        'coverage',
-        'wheel'
-    ],
-    entry_points={
-    }
-)
+if __name__ == "__main__":
+    setup()
