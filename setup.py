733de51897bf69204cdef6eaf040353aad1f60cc
wu <wu@.com>
2025-06-21 17:24:49 +0800

    openforcefield/openforcefields

--- setup.py
+++ setup.py
@@ -1,71 +1,62 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""
+openforcefields
+"""
+import sys
+from setuptools import setup
+import versioneer
 
-from setuptools import setup, find_packages
+short_description = __doc__.split("\n")
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:]),
 
-requirements = [
-    'datacube',
-    'Flask',
-    'flask_log_request_id',
-    'requests',
-    'affine',
-    'click',
-    'colour',
-    'lxml',
-    'matplotlib',
-    'numpy',
-    'opencensus',
-    'Pillow',
-    'prometheus_client',
-    'psycopg2',
-    'python_dateutil',
-    'pytz',
-    'rasterio',
-    'regex',
-    'scikit-image',
-    'timezonefinderL',
-    'python-slugify',
-    'xarray'
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
-    'pytest', 'pytest-cov', 'pytest_localserver', 'owslib', 'mock', 'pep8', 'pylint==1.6.4',
-]
 
 setup(
-    name='datacube_ows',
-    version='0.2.0',
-    description="Datacube Open Web Services",
-    long_description=readme + '\n\n' + history,
-    author="Open Data Cube",
-    author_email='earth.observation@ga.gov.au',
-    url='https://github.com/opendatacube/datacube-wms',
+    # Self-descriptive entries which should always be present
+    name='openforcefields',
+    author='The Open Force Field Initiative',
+    author_email='dmobley@uci.edu',
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='CC-BY-4.0',
+
+    # Which Python importable modules should be included when your package is installed
+    packages=['openforcefields', "openforcefields.tests"],
+
+    # Optional include package data to ship with your package
+    # Comment out this line to prevent the files from being packaged with your software
+    # Extend/modify the list to include/exclude other items as need be
+    package_data={'openforcefields': ["offxml/*"]
+                  },
+
+    # Allows `setup.py test` to work correctly with pytest
+    setup_requires=[] + pytest_runner,
+
+    # Additional entries you may want simply uncomment the lines you want and fill in the data
+    url='https://github.com/openforcefield/openforcefields',  # Website
+    # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
+    platforms=['Linux',
+               'Mac OS-X',
+               'Unix'],                 # Valid platforms your code works on, adjust to your flavor
+    # python_requires=">=3.5",          # Python version restrictions
+
+    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
+    # zip_safe=False,
+
+    # Add entry point so that the forcefield directory can be discovered by the openforcefield toolkit.
     entry_points={
-        'console_scripts': [
-            'datacube-ows=datacube_ows.wsgi:main',
-            'datacube-ows-update=datacube_ows.update_ranges:main'
-        ]
-    },
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=requirements,
-    license="Apache Software License 2.0",
-    zip_safe=False,
-    keywords='datacube, wms, wcs',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements
+        'openforcefield.smirnoff_forcefield_directory' : [
+            'get_forcefield_dirs_paths = openforcefields.openforcefields:get_forcefield_dirs_paths',
+        ],
+    }
 )
