ac8d8f1508479471895274875e906701259b6a2c
wu <wu@.com>
2025-06-26 14:29:23 +0800

    py-pa/django-minio-storage

--- setup.py
+++ setup.py
@@ -1,69 +1,35 @@
-#!/usr/bin/env python
-"""Python log parser for ULog.
+# encoding: utf-8
+from setuptools import setup
 
-This module allows you to parse ULog files, which are used within the PX4
-autopilot middleware.
-
-The file format is documented on https://dev.px4.io/advanced-ulog-file-format.html
-
-"""
-
-from __future__ import print_function
-import os
-import sys
-import versioneer
-
-from setuptools import setup, find_packages
-
-DOCLINES = __doc__.split("\n")
-
-CLASSIFIERS = """\
-Development Status :: 1 - Planning
-Intended Audience :: Science/Research
-Intended Audience :: Developers
-License :: OSI Approved :: BSD License
-Programming Language :: Python
-Programming Language :: Python :: 3
-Programming Language :: Other
-Topic :: Software Development
-Topic :: Scientific/Engineering :: Artificial Intelligence
-Topic :: Scientific/Engineering :: Mathematics
-Topic :: Scientific/Engineering :: Physics
-Operating System :: Microsoft :: Windows
-Operating System :: POSIX
-Operating System :: Unix
-Operating System :: MacOS
-"""
-
-# pylint: disable=invalid-name
+with open("README.md") as f:
+    long_description = f.read()
 
 setup(
-    name='pyulog',
-    maintainer="James Goppert",
-    maintainer_email="james.goppert@gmail.com",
-    description=DOCLINES[0],
-    long_description="\n".join(DOCLINES[2:]),
-    url='https://github.com/PX4/pyulog',
-    author='Beat Kueng',
-    author_email='beat-kueng@gmx.net',
-    download_url='https://github.com/PX4/pyulog',
-    license='BSD 3-Clause',
-    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
-    platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
-    install_requires=['numpy<1.25'],
-    tests_require=['nose', 'ddt'],
-    test_suite='nose.collector',
-    entry_points = {
-        'console_scripts': [
-            'ulog_extract_gps_dump=pyulog.extract_gps_dump:main',
-            'ulog_info=pyulog.info:main',
-            'ulog_messages=pyulog.messages:main',
-            'ulog_params=pyulog.params:main',
-            'ulog2csv=pyulog.ulog2csv:main',
-            'ulog2kml=pyulog.ulog2kml:main',
-        ],
-    },
-    packages=find_packages(),
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+    name="django-minio-storage",
+    license="MIT",
+    use_scm_version=True,
+    description="Django file storage using the minio python client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tom Houlé",
+    author_email="tom@kafunsho.be",
+    url="https://github.com/py-pa/django-minio-storage",
+    packages=[
+        "minio_storage",
+        "minio_storage/management/",
+        "minio_storage/management/commands/",
+    ],
+    setup_requires=["setuptools_scm"],
+    install_requires=["django>=1.11", "minio>=4.0.21,<7"],
+    extras_require={"test": ["coverage", "requests"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Framework :: Django",
+    ],
 )
