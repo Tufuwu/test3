e0e899315d3d08ae1fb8b6f15626211ebe8d0ba1
wu <wu@.com>
2025-06-26 14:02:20 +0800

    pvlib/pvanalytics

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -1,28 +1,17 @@
 ## Description
 
-<!-- Choose one of the following prefixes for the title of your PR:
-
-     [ENH] for enhancements
-     [FIX] for bug fixes
-     [TST] for new or updated tests
-     [DOC] for new or updated documentation
-     [STY] for stylistic changes
-     [REF] for refactoring existing code
-
-     If your PR is not yet ready to be merged, also add [WIP] to label it as a "work in progress".
-
-     If this is a bug-fix or enhancement, state the issue # it closes.
-     If this is a new feature, reference what paper it implements.
--->
+*Thank you for your contribution! Please provide a brief description of the problem and the proposed solution or new feature (if not already fully described in a linked issue)*
 
 ## Checklist
 
-<!-- It's fine to submit PRs which are a work in progress!
-     But before they are merged, all PRs should provide: -->
-- [ ] [Docstrings for all functions](https://github.com/numpy/numpy/blob/master/doc/example.py)
-- [ ] Gallery example in `./examples` (new features only)
-- [ ] Unit tests
-- [ ] Clean style in [the spirit of PEP8](https://www.python.org/dev/peps/pep-0008/)
+The following items must be addressed before the code can be merged. 
+Please don't hesitate to ask for help if you are unsure of how to accomplish any of the items. 
+*You are free to remove any checklist items that do not apply or add additional items that are 
+not on this list*
 
-<!-- For detailed information on these and other aspects, see the contribution guidelines:
-     https://pulse2percept.readthedocs.io/en/latest/developers/contributing.html -->
+- [ ] Closes #xxx
+- [ ] Added new API functions to `docs/api.rst`
+- [ ] Clearly documented all new API functions with [PEP257](https://www.python.org/dev/peps/pep-0257/) and [numpydoc](https://numpydoc.readthedocs.io/en/latest/format.html) compliant docstrings
+- [ ] Non-API functions clearly documented with docstrings or comments as necessary
+- [ ] Added tests to cover all new or modified code
+- [ ] Pull request is nearly complete and ready for detailed review
--- setup.py
+++ setup.py
@@ -1,37 +1,81 @@
-import os
-import numpy as np
-from setuptools import find_packages, setup
-from setuptools.extension import Extension
-from Cython.Build import cythonize
+#!/usr/bin/env python
 
-extensions = [
-    Extension('pulse2percept.fast_retina', ['pulse2percept/fast_retina.pyx'],
-              include_dirs=[np.get_include()],
-              extra_compile_args=['-O3'])
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    raise RuntimeError('setuptools is required')
+
+DESCRIPTION = ('PVAnalytics is a python library for the analysis of ' +
+               'photovoltaic system-level data.')
+
+LONG_DESCRIPTION = """
+PVAnalytics is a collection of functions for working with data
+from photovoltaic power systems. The library includes functions for
+general data quality tests such as outlier detection, validation that
+data is physically plausible, filtering data for specific conditions,
+and labeling specific features in the data.
+
+Documentation: https://pvanalytics.readthedocs.io
+
+Source code: https://github.com/pvlib/pvanalytics
+"""
+
+DISTNAME = 'pvanalytics'
+MAINTAINER = "Will Vining"
+MAINTAINER_EMAIL = 'wfvinin@sandia.gov'
+LICENSE = 'MIT'
+URL = 'https://github.com/pvlib/pvanalytics'
+
+TESTS_REQUIRE = [
+    'pytest',
 ]
 
-# Get version and release info, which is all stored in pulse2percept/version.py
-ver_file = os.path.join('pulse2percept', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
+INSTALL_REQUIRES = [
+    'numpy >= 1.15.0',
+    'pandas >= 0.23.0',
+    'pvlib >= 0.8.0',
+    'scipy >= 1.2.0',
+    'statsmodels >= 0.9.0'
+]
 
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=find_packages(),
-            ext_modules=cythonize(extensions),
-            install_requires=REQUIRES)
+DOCS_REQUIRE = [
+    'sphinx == 2.2.0'
+]
 
+EXTRAS_REQUIRE = {
+    'optional': ['ruptures'],
+    'test': TESTS_REQUIRE,
+    'doc': DOCS_REQUIRE
+}
 
-if __name__ == '__main__':
-    setup(**opts)
+EXTRAS_REQUIRE['all'] = sorted(set(sum(EXTRAS_REQUIRE.values(), [])))
+
+SETUP_REQUIRES = ['setuptools_scm']
+
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering'
+]
+
+PACKAGES = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
+
+setup(
+    name=DISTNAME,
+    use_scm_version=True,
+    packages=PACKAGES,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    tests_require=TESTS_REQUIRE,
+    setup_requires=SETUP_REQUIRES,
+    ext_modules=[],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    license=LICENSE,
+    classifiers=CLASSIFIERS,
+    url=URL
+)
