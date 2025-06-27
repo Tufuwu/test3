07d2c63ef0309504d04351be053984cf06502b02
wu <wu@.com>
2025-06-27 15:29:55 +0800

    pysb/pysb

--- setup.py
+++ setup.py
@@ -1,84 +1,52 @@
-#! /usr/bin/env python3
-
-import re
-from pathlib import Path
-
+from ez_setup import use_setuptools
+use_setuptools()
 from setuptools import setup
-
-tests_require = [
-    "pytest>=2.3",
-    "tox",
-    "twine",
-    "pip>=7",
-    "passlib>=1.6",
-    "webtest",
-]
-
-setup_requires = ["setuptools", "setuptools-git >= 0.3", "wheel >= 0.25.0"]
+import versioneer
+import os
 
 
-def read_file(rel_path: str):
-    return Path(__file__).parent.joinpath(rel_path).read_text()
+def main():
+    this_directory = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(this_directory, 'README.rst'), 'r') as f:
+        long_description = f.read()
+
+    cmdclass = versioneer.get_cmdclass()
+
+    setup(name='pysb',
+          version=versioneer.get_version(),
+          description='Python Systems Biology modeling framework',
+          long_description=long_description,
+          long_description_content_type='text/x-rst',
+          author='Jeremy Muhlich',
+          author_email='jmuhlich@bitflood.org',
+          url='http://pysb.org/',
+          packages=['pysb', 'pysb.generator', 'pysb.importers', 'pysb.tools',
+                    'pysb.examples', 'pysb.export', 'pysb.simulator',
+                    'pysb.testing', 'pysb.tests'],
+          scripts=['scripts/pysb_export'],
+          # We should really specify some minimum versions here.
+          python_requires='>=3.6',
+          install_requires=['numpy', 'scipy>=1.1', 'sympy>=1.6', 'networkx',
+                            'futures; python_version == "2.7"'],
+          setup_requires=['nose'],
+          tests_require=['coverage', 'pygraphviz', 'matplotlib', 'pexpect',
+                         'pandas', 'h5py', 'mock', 'cython',
+                         'python-libsbml', 'libroadrunner'],
+          cmdclass=cmdclass,
+          keywords=['systems', 'biology', 'model', 'rules'],
+          classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Environment :: Console',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python :: 3',
+            'Topic :: Scientific/Engineering :: Bio-Informatics',
+            'Topic :: Scientific/Engineering :: Chemistry',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            ],
+          )
 
 
-def get_version():
-    locals_ = {}
-    version_line = re.compile(
-        r'^[\w =]*__version__ = "\d+\.\d+\.\d+\.?\w*\d*"$'
-    )
-    try:
-        for ln in filter(
-            version_line.match,
-            read_file("pypiserver/__init__.py").splitlines(),
-        ):
-            exec(ln, locals_)
-    except (ImportError, RuntimeError):
-        pass
-    return locals_["__version__"]
-
-
-setup(
-    name="pypiserver",
-    description="A minimal PyPI server for use with pip/easy_install.",
-    long_description=read_file("README.rst"),
-    version=get_version(),
-    packages=["pypiserver"],
-    package_data={"pypiserver": ["welcome.html"]},
-    python_requires=">=3.6",
-    setup_requires=setup_requires,
-    extras_require={"passlib": ["passlib>=1.6"], "cache": ["watchdog"]},
-    tests_require=tests_require,
-    url="https://github.com/pypiserver/pypiserver",
-    maintainer=(
-        "Kostis Anagnostopoulos <ankostis@gmail.com>"
-        "Matthew Planchard <mplanchard@gmail.com>"
-    ),
-    maintainer_email="ankostis@gmail.com",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: BSD License",
-        "License :: OSI Approved :: zlib/libpng License",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-        "Operating System :: POSIX",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Build Tools",
-        "Topic :: System :: Software Distribution",
-    ],
-    zip_safe=True,
-    entry_points={
-        "paste.app_factory": ["main=pypiserver:paste_app_factory"],
-        "console_scripts": ["pypi-server=pypiserver.__main__:main"],
-    },
-    options={"bdist_wheel": {"universal": True}},
-    platforms=["any"],
-)
+if __name__ == '__main__':
+    main()
