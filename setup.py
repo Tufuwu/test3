5d5c1e78bdc7f7f7b6bd932e7c8d1dad95c9b2d5
wu <wu@.com>
2025-07-01 13:03:30 +0800

    pytest-dev/nose2pytest

--- setup.py
+++ setup.py
@@ -1,53 +1,45 @@
 from setuptools import setup
 
 
-with open('README.md') as fh:
-    long_description = fh.read()
-
 setup(
-    name='python-ta',
-    version='1.8.0a1',
-    description='Code checking tool for teaching Python',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='http://github.com/pyta-uoft/pyta',
-    author='David Liu',
-    author_email='david@cs.toronto.edu',
-    license='MIT',
-    packages=['python_ta', 'python_ta.reporters', 'python_ta.checkers',
-              'python_ta.cfg', 'python_ta.contracts',
-              'python_ta.patches',
-              'python_ta.transforms', 'python_ta.typecheck', 'python_ta.util'],
-    install_requires=[
-        'astroid>=2.5,<2.6',
-        'hypothesis',
-        'pycodestyle',
-        'pylint>=2.8,<2.9',
-        'colorama',
-        'six',
-        'jinja2',
-        'pygments',
-        'wrapt>=1.12.0',
-        'typeguard>=2.7.1',
-        'requests',
-        'click>=8.0.1'
-    ],
-    extras_require={
-        'dev': [
-            'graphviz',
-            'inflection',
-            'myst-parser',
-            'pytest',
-            'pytest-cov',
-            'sphinx',
-            'sphinx-rtd-theme'
-        ]
-    },
-    entry_points={
-        'console_scripts': [
-            'python_ta = python_ta.__main__:main'
+        name='nose2pytest',
+        version='1.0.8',
+        packages=['nose2pytest'],
+        # py_modules=['assert_tools', 'nose2pytest'],
+        entry_points={
+            'console_scripts': [
+                'nose2pytest = nose2pytest.script:main',
+            ],
+            'pytest11': ['pytest_nose_assert_tools = nose2pytest.assert_tools'],
+        },
+        url='https://github.com/schollii/nose2pytest',
+        license='BSD-3',
+        author='Oliver Schoenborn',
+        author_email='oliver.schoenborn@gmail.com',
+        description='Convert nose.tools.assert_ calls found in your Nose test modules into raw asserts for pytest',
+        keywords='nose to pytest conversion',
+
+        python_requires='>=3.5',
+        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 5 - Production/Stable',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Testing',
+
+            # Pick your license as you wish (should match "license" above)
+            'License :: OSI Approved :: BSD License',
+
+            # Specify the Python versions you support here.
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
         ],
-    },
-    python_requires='~=3.8',
-    include_package_data=True,
-    zip_safe=False)
+)
