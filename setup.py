3c0708a910e29cec123416841f18cca404e313f1
wu <wu@.com>
2025-07-02 13:37:33 +0800

    quantumlib/openfermion

--- setup.py
+++ setup.py
@@ -1,112 +1,57 @@
-# Use setuptools in preference to distutils
-from setuptools import setup, find_packages
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+import io
 import os
 
-#-Write Versions File-#
+from setuptools import setup, find_packages
 
-VERSION = '0.4.8'
 
-def write_version_py(filename=None):
-    """
-    This constructs a version file for the project
-    """
-    doc = "\"\"\"\nThis is a VERSION file and should NOT be manually altered\n\"\"\""
-    doc += "\nversion = '%s'\n" % VERSION
+# This reads the __version__ variable from openfermion/_version.py
+__version__ = ''
+exec(open('src/openfermion/_version.py').read())
 
-    if not filename:
-        filename = os.path.join(os.path.dirname(__file__), 'quantecon', 'version.py')
+# Readme file as long_description:
+long_description = ('===========\n' +
+                    'OpenFermion\n' +
+                    '===========\n')
+stream = io.open('README.rst', encoding='utf-8')
+stream.readline()
+long_description += stream.read()
 
-    fl = open(filename, 'w')
-    try:
-        fl.write(doc)
-    finally:
-        fl.close()
+# Read in requirements.txt
+requirements = open('requirements.txt').readlines()
+requirements = [r.strip() for r in requirements]
 
-write_version_py()  # This is a file used to control the qe.__version__ attribute
-
-#-Meta Information-#
-#~~~~~~~~~~~~~~~~~~#
-
-DESCRIPTION = "QuantEcon is a package to support all forms of quantitative economic modelling."       #'Core package of the QuantEcon library'
-
-LONG_DESCRIPTION = """
-**QuantEcon** is an organization run by economists for economists with the aim of coordinating
-distributed development of high quality open source code for all forms of quantitative economic modelling.
-
-The project website is located at `http://quantecon.org/ <http://quantecon.org/>`_. This website provides
-more information with regards to the **quantecon** library, documentation, in addition to some resources
-in regards to how you can use and/or contribute to the package.
-
-The **quantecon** Package
--------------------------
-
-The `repository <https://github.com/QuantEcon/QuantEcon.py>`_ includes the Python package ``quantecon``
-
-Assuming you have `pip <https://pypi.python.org/pypi/pip>`_ on your computer --- as will be the case if you've `installed Anaconda <http://quant-econ.net/getting_started.html#installing-anaconda>`_ --- you can install the latest stable release of ``quantecon`` by typing
-
-    pip install quantecon
-
-at a terminal prompt
-
-Repository
-----------
-
-The main repository is hosted on Github `QuantEcon.py <https://github.com/QuantEcon/QuantEcon.py>`_
-
-**Note:** There is also a Julia version available for Julia users `QuantEcon.jl <https://github.com/QuantEcon/QuantEcon.jl>`_
-
-Current Build and Coverage Status
----------------------------------
-
-|Build Status| |Coverage Status|
-
-.. |Build Status| image:: https://travis-ci.org/QuantEcon/QuantEcon.py.svg?branch=master
-   :target: https://travis-ci.org/QuantEcon/QuantEcon.py
-.. |Coverage Status| image:: https://coveralls.io/repos/QuantEcon/QuantEcon.py/badge.png
-   :target: https://coveralls.io/r/QuantEcon/QuantEcon.py
-
-Additional Links
-----------------
-
-1. `QuantEcon Course Website <http://quant-econ.net>`_
-
-"""
-
-LICENSE = "BSD"
-
-#-Classifier Strings-#
-#-https://pypi.python.org/pypi?%3Aaction=list_classifiers-#
-CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
-    'Operating System :: OS Independent',
-    'Intended Audience :: Science/Research',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.5',
-    'Topic :: Scientific/Engineering',
-]
-
-#-Setup-#
-#~~~~~~~#
-
-setup(name='quantecon',
-      packages=find_packages(),
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
-      license=LICENSE,
-      classifiers=CLASSIFIERS,
-      author='Thomas J. Sargent and John Stachurski (Project coordinators)',
-      author_email='john.stachurski@gmail.com',
-      url='https://github.com/QuantEcon/QuantEcon.py',  # URL to the repo
-      download_url='https://github.com/QuantEcon/QuantEcon.py/tarball/' + VERSION,
-      keywords=['quantitative', 'economics'],
-      install_requires=[
-          'numba>=0.38',
-          'numpy',
-          'requests',
-          'scipy>=1.0.0',
-          'sympy',
-          ],
-      include_package_data=True
-      )
+setup(
+    name='openfermion',
+    version=__version__,
+    author='The OpenFermion Developers',
+    author_email='help@openfermion.org',
+    url='http://www.openfermion.org',
+    description=('The electronic structure package for quantum computers.'),
+    long_description=long_description,
+    install_requires=requirements,
+    license='Apache 2',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    package_data={
+        '': [os.path.join('src', 'openfermion', 'data', '*.hdf5'),
+             os.path.join('src', 'openfermion', 'data', '*.npy')]
+    },
+    data_files=[('openfermion/examples', [
+                 'examples/binary_code_transforms_demo.ipynb',
+                 'examples/bosonic_operator_tutorial.ipynb',
+                 'examples/jordan_wigner_and_bravyi_kitaev_transforms.ipynb',
+                 'examples/openfermion_tutorial.ipynb',
+                 'examples/performance_benchmarks.py'])],
+    )
