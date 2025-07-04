2d94d4381cede66d7510fc71f3b3f20dfbede43c
wu <wu@.com>
2025-07-04 16:41:29 +0800

    simplejson/simplejson

--- README.rst
+++ README.rst
@@ -1,132 +1,34 @@
-=======
-pixell
-=======
-
-.. image:: https://github.com/simonsobs/pixell/workflows/Build/badge.svg
-           :target: https://github.com/simonsobs/pixell/actions?query=workflow%3ABuild
-
-.. image:: https://readthedocs.org/projects/pixell/badge/?version=latest
-           :target: https://pixell.readthedocs.io/en/latest/?badge=latest
-		   :alt: Documentation Status
-
-.. image:: https://coveralls.io/repos/github/simonsobs/pixell/badge.svg?branch=master
-		   :target: https://coveralls.io/github/simonsobs/pixell?branch=master
-
-.. image:: https://badge.fury.io/py/pixell.svg
-		       :target: https://badge.fury.io/py/pixell
-
-``pixell`` is a library for loading, manipulating and analyzing maps stored in rectangular pixelization. It is mainly targeted for use with maps of the sky (e.g. CMB intensity and polarization maps, stacks of 21 cm intensity maps, binned galaxy positions or shear) in cylindrical projection, but its core functionality is more general. It extends numpy's ``ndarray`` to an ``ndmap`` class that associates a World Coordinate System (WCS) with a numpy array.  It includes tools for Fourier transforms  (through numpy or pyfft) and spherical harmonic transforms (through libsharp) of such maps and tools for visualization (through the Python Image Library). 
-
-
-* Free software: BSD license
-* Documentation: https://pixell.readthedocs.io.
-* Tutorials_
-
-Dependencies
-------------
-
-* Python>=3.6
-* gcc/gfortran or Intel compilers (clang might not work out of the box), if compiling from source
-* libsharp (downloaded and installed, if compiling from source)
-* automake (for libsharp compilation, if compiling from source)
-* healpy, Cython, astropy, numpy, scipy, matplotlib, pyyaml, h5py, Pillow (Python Image Library)
-
-Installing
+simplejson
 ----------
 
-Make sure your ``pip`` tool is up-to-date. To install ``pixell``, run:
+simplejson is a simple, fast, complete, correct and extensible
+JSON <http://json.org> encoder and decoder for Python 3.3+
+with legacy support for Python 2.5+.  It is pure Python code
+with no dependencies, but includes an optional C extension
+for a serious speed boost.
 
-.. code-block:: console
-		
-   $ pip install pixell --user
-   $ test-pixell
+The latest documentation for simplejson can be read online here:
+https://simplejson.readthedocs.io/
 
-This will install a pre-compiled binary suitable for your system (only Linux and Mac OS X with Python>=3.6 are supported). If you require more control over your installation, e.g. using your own installation of ``libsharp``, using Intel compilers or enabling tuning of the ``libsharp`` installation to your CPU, please see the section below on compiling from source.  The ``test-pixell`` command will run a suite of unit tests.
+simplejson is the externally maintained development version of the
+json library included with Python (since 2.6). This version is tested
+with the latest Python 3.8 and maintains backwards compatibility
+with Python 3.3+ and the legacy Python 2.5 - Python 2.7 releases.
 
-Compiling from source (advanced / development workflow)
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+The encoder can be specialized to provide serialization in any kind of
+situation, without any special support by the objects to be serialized
+(somewhat like pickle). This is best done with the ``default`` kwarg
+to dumps.
 
-For compilation instructions specific to NERSC/cori, see NERSC_.
+The decoder can handle incoming JSON strings of any specified encoding
+(UTF-8 by default). It can also be specialized to post-process JSON
+objects with the ``object_hook`` or ``object_pairs_hook`` kwargs. This
+is particularly useful for implementing protocols such as JSON-RPC
+that have a richer type system than JSON itself.
 
-For compilation instructions specific to Mac OS X, see MACOSX_ (h/t Thibaut Louis).
+For those of you that have legacy systems to maintain, there is a
+very old fork of simplejson in the `python2.2`_ branch that supports
+Python 2.2. This is based on a very old version of simplejson,
+is not maintained, and should only be used as a last resort.
 
-For all other, below are general instructions.
-
-First, download the source distribution or ``git clone`` this repository. You can work from ``master`` or checkout one of the released version tags (see the Releases section on Github). Then change into the cloned/source directory.
-
-Existing ``libsharp`` installation (optional)
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-``libsharp`` is installed automatically by the setup.py you will execute below. The installation script will
-attempt to automatically git clone the latest version of ``libsharp`` and compile it.  If
-instead you want to use an existing ``libsharp`` installation, you can do so by
-symlinking the ``libsharp`` directory into a directory called ``_deps`` in the
-root directory, such that ``pixell/_deps/libsharp/libsharp/sharp.h`` exists. If
-you are convinced that the libsharp library is successfully
-compiled,  add an empty file named
-``pixell/_deps/libsharp/libsharp/success.txt`` to ensure pixell's setup.py
-knows of your existing installation.
-
-Run ``setup.py``
-~~~~~~~~~~~~~~~~
-
-If not using Intel compilers (see below), build the package using 
-
-.. code-block:: console
-		
-   $ python setup.py build_ext -i
-
-You may now test the installation:
-
-.. code-block:: console
-		
-   $ py.test pixell/tests/
-   
-If the tests pass, either add the cloned directory to your ``$PYTHONPATH``, if you want the ability for changes made to Python source files to immediately reflect in your installation, e.g., in your ``.bashrc`` file,
-
-.. code-block:: bash
-		
-   export PYTHONPATH=$PYTHONPATH:/path/to/cloned/pixell/directory
-
-
-or alternatively, install the package  
-   
-.. code-block:: console
-
-   $ python setup.py install --user
-
-which requires you to reinstall every time changes are made to any files in your repository directory.
-   
-Intel compilers
-~~~~~~~~~~~~~~~
-
-Intel compilers require you to modify the build step above as follows
-
-.. code-block:: console
-		
-   $ python setup.py build_ext -i --fcompiler=intelem --compiler=intelem
-
-On some systems, further specification might be required (make sure to get a fresh copy of the repository before trying out a new install method), e.g.:
-
-.. code-block:: console
-
-   $ LDSHARED="icc -shared" LD=icc LINKCC=icc CC=icc python setup.py build_ext -i --fcompiler=intelem --compiler=intelem
-
-
-
-Contributions
--------------
-
-If you have write access to this repository, please:
-
-1. create a new branch
-2. push your changes to that branch
-3. merge or rebase to get in sync with master
-4. submit a pull request on github
-
-If you do not have write access, create a fork of this repository and proceed as described above. For more details, see Contributing_.
-  
-.. _Tutorials: https://github.com/simonsobs/pixell_tutorials/
-.. _Contributing: https://pixell.readthedocs.io/en/latest/contributing.html
-.. _NERSC: https://pixell.readthedocs.io/en/latest/nersc.html
-.. _MACOSX: https://github.com/simonsobs/pspy/blob/master/INSTALL_MACOS.rst
+.. _python2.2: https://github.com/simplejson/simplejson/tree/python2.2
--- setup.py
+++ setup.py
@@ -1,249 +1,136 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from __future__ import with_statement
 
-"""The setup script."""
-from __future__ import print_function
-import setuptools
-from setuptools import find_packages
-from distutils.errors import DistutilsError
-from numpy.distutils.core import setup, Extension, build_ext, build_src
-import versioneer
-import os, sys
-import subprocess as sp
-import numpy as np
-import glob
-build_ext = build_ext.build_ext
-build_src = build_src.build_src
+import os
+import sys
+try:
+    from setuptools import setup, Extension, Command
+except ImportError:
+    from distutils.core import setup, Extension, Command
+from distutils.command.build_ext import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, \
+    DistutilsPlatformError
 
+IS_PYPY = hasattr(sys, 'pypy_translation_info')
+VERSION = '3.17.3'
+DESCRIPTION = "Simple, fast, extensible JSON encoder/decoder for Python"
 
-compile_opts = {
-    'extra_compile_args': ['-std=c99','-fopenmp', '-Wno-strict-aliasing', '-g', '-Ofast', '-fPIC'],
-    'extra_f90_compile_args': ['-fopenmp', '-Wno-conversion', '-Wno-tabs', '-fPIC'],
-    'f2py_options': ['skip:', 'map_border', 'calc_weights', ':'],
-    'extra_link_args': ['-fopenmp', '-g', '-fPIC']
-    }
+with open('README.rst', 'r') as f:
+   LONG_DESCRIPTION = f.read()
 
-# Set compiler options
-# Windows
-if sys.platform == 'win32':
-    raise DistUtilsError('Windows is not supported.')
-# Mac OS X - needs gcc (usually via HomeBrew) because the default compiler LLVM (clang) does not support OpenMP
-#          - with gcc -fopenmp option implies -pthread
-elif sys.platform == 'darwin':
-    try:
-        sp.check_call('scripts/osx.sh', shell=True)
-    except sp.CalledProcessError:
-        raise DistutilsError('Failed to prepare Mac OS X properly. See earlier errors.')
-    gccpath = glob.glob('/usr/local/bin/gcc-*')
-    if gccpath:
-        # Use newest gcc found
-        sint = lambda x: int(x) if x.isdigit() else 0
-        gversion = str(max([sint(os.path.basename(x).split('-')[1]) for x in gccpath]))
-        os.environ['CC'] = 'gcc-' + gversion
-        os.environ['CXX'] = os.environ['CC'].replace("gcc","g++")
-        os.environ['FC'] = os.environ['CC'].replace("gcc","gfortran")
-        rpath = '/usr/local/opt/gcc/lib/gcc/' + gversion + '/'
-    else:
-        os.system("which gcc")
-        os.system("find / -name \'gcc\'")
-        raise Exception('Cannot find gcc in /usr/local/bin. pixell requires gcc to be installed - easily done through the Homebrew package manager (http://brew.sh). Note: gcc with OpenMP support is required.')
-    compile_opts['extra_link_args'] = ['-fopenmp', '-Wl,-rpath,' + rpath]
-# Linux
-elif sys.platform == 'linux':
-    compile_opts['extra_link_args'] = ['-fopenmp']
+PYTHON_REQUIRES = '>=2.5, !=3.0.*, !=3.1.*, !=3.2.*'
 
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'License :: OSI Approved :: Academic Free License (AFL)',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+]
 
-def pip_install(package):
-    import pip
-    if hasattr(pip, 'main'):
-        pip.main(['install', package])
-    else:
-        pip._internal.main(['install', package])
-
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements =  ['numpy>=1.16',
-                 'astropy>=2.0',
-                 'setuptools>=39',
-                 'h5py>=2.7<=2.10',
-                 'scipy>=1.0',
-                 'python_dateutil>=2.7',
-                 'cython>=0.28',
-                 'matplotlib>=2.0',
-                 'pyyaml>=5.0',
-                 'healpy>=1.13',
-                 'Pillow>=5.3.0',
-                 'pytest-cov>=2.6',
-                 'coveralls>=1.5',
-                 'pytest>=4.6']
-
-
-test_requirements = ['pip>=9.0',
-                     'bumpversion>=0.5.',
-                     'wheel>=0.30',
-                     'watchdog>=0.8',
-                     'flake8>=3.5',
-                     'coverage>=4.5',
-                     'Sphinx>=1.7',
-                     'twine>=1.10',
-                     'numpy>=1.16',
-                     'astropy>=2.0',
-                     'setuptools>=39.2',
-                     'h5py>=2.7<=2.10',
-                     'scipy>=1.0',
-                     'python_dateutil>=2.7',
-                     'cython>=0.28',
-                     'matplotlib>=2.0',
-                     'pyyaml>=5.0',
-                     'pytest-cov>=2.6',
-                     'coveralls>=1.5',
-                     'pytest>=4.6']
-    
-    
-    
-
-fcflags = os.getenv('FCFLAGS')
-if fcflags is None or fcflags.strip() == '':
-    fcflags = ['-O3','-fPIC']
+if sys.platform == 'win32' and sys.version_info < (2, 7):
+   # 2.6's distutils.msvc9compiler can raise an IOError when failing to
+   # find the compiler
+   # It can also raise ValueError https://bugs.python.org/issue7511
+   ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
+                 IOError, ValueError)
 else:
-    print('User supplied fortran flags: ', fcflags)
-    print('These will supersede other optimization flags.')
-    fcflags = fcflags.split()
-    
-compile_opts['extra_f90_compile_args'].extend(fcflags)
-compile_opts['extra_f77_compile_args'] = compile_opts['extra_f90_compile_args']
+   ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
 
-def presrc():
-    # Create f90 files for f2py.
-    if sp.call('make -C fortran', shell=True) != 0:
-        raise DistutilsError('Failure in the fortran source-prep step.')
-    
-def prebuild():
-    # Handle the special external dependencies.
-    if not os.path.exists('_deps/libsharp/success.txt'):
+class BuildFailed(Exception):
+    pass
+
+class ve_build_ext(build_ext):
+    # This class allows C extension building to fail.
+
+    def run(self):
         try:
-            sp.check_call('scripts/install_libsharp.sh', shell=True)
-        except sp.CalledProcessError:
-            raise DistutilsError('Failed to install libsharp.')
-        
-    # Handle cythonization to create sharp.c, etc.
-    no_cython = sp.call('cython --version',shell=True)
-    if no_cython:
+            build_ext.run(self)
+        except DistutilsPlatformError:
+            raise BuildFailed()
+
+    def build_extension(self, ext):
         try:
-            print("Cython not found. Attempting a conda install first.")
-            import conda.cli
-            conda.cli.main('conda', 'install',  '-y', 'cython')
-        except:
-            try:
-                print("conda install of cython failed. Attempting a pip install.")
-                pip_install("cython")
-            except:
-                raise DistutilsError('Cython not found and all attempts at installing it failed. User intervention required.')
-        
-    if sp.call('make -C cython',  shell=True) != 0:
-        raise DistutilsError('Failure in the cython pre-build step.')
+            build_ext.build_extension(self, ext)
+        except ext_errors:
+            raise BuildFailed()
 
 
-class CustomBuild(build_ext):
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
     def run(self):
-        print("Running build...")
-        prebuild()
-        # Then let setuptools do its thing.
-        return build_ext.run(self)
+        import sys, subprocess
+        raise SystemExit(
+            subprocess.call([sys.executable,
+                             # Turn on deprecation warnings
+                             '-Wd',
+                             'simplejson/tests/__init__.py']))
 
-class CustomSrc(build_src):
-    def run(self):
-        print("Running src...")
-        presrc()
-        # Then let setuptools do its thing.
-        return build_src.run(self)
+def run_setup(with_binary):
+    cmdclass = dict(test=TestCommand)
+    if with_binary:
+        kw = dict(
+            ext_modules = [
+                Extension("simplejson._speedups", ["simplejson/_speedups.c"]),
+            ],
+            cmdclass=dict(cmdclass, build_ext=ve_build_ext),
+        )
+    else:
+        kw = dict(cmdclass=cmdclass)
 
-class CustomEggInfo(setuptools.command.egg_info.egg_info):
-    def run(self):
-        print("Running EggInfo...")
-        presrc()
-        prebuild()
-        return setuptools.command.egg_info.egg_info.run(self)   
+    setup(
+        name="simplejson",
+        version=VERSION,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        classifiers=CLASSIFIERS,
+        python_requires=PYTHON_REQUIRES,
+        author="Bob Ippolito",
+        author_email="bob@redivi.com",
+        url="https://github.com/simplejson/simplejson",
+        license="MIT License",
+        packages=['simplejson', 'simplejson.tests'],
+        platforms=['any'],
+        **kw)
 
-# Cascade your overrides here.
-cmdclass = {
-    'build_ext': CustomBuild,
-    'build_src': CustomSrc,
-    'egg_info': CustomEggInfo,
-}
-cmdclass = versioneer.get_cmdclass(cmdclass)
+try:
+    run_setup(not IS_PYPY)
+except BuildFailed:
+    if os.environ.get('REQUIRE_SPEEDUPS') or os.environ.get('CIBUILDWHEEL', '0') == '1':
+        raise
+    BUILD_EXT_WARNING = ("WARNING: The C extension could not be compiled, "
+                         "speedups are not enabled.")
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print("Failure information, if any, is above.")
+    print("I'm retrying the build without the C extension now.")
+    print('*' * 75)
 
+    run_setup(False)
 
-setup(
-    author="Simons Observatory Collaboration Analysis Library Task Force",
-    author_email='mathewsyriac@gmail.com',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
-    description="pixell",
-    package_dir={"pixell": "pixell"},
-    entry_points={
-    },
-    ext_modules=[
-        Extension('pixell.sharp',
-            sources=['cython/sharp.c', 'cython/sharp_utils.c'],
-            libraries=['sharp','c_utils', 'fftpack', 'm'],
-            library_dirs=['_deps/libsharp/auto/lib'],
-            include_dirs=[np.get_include()],
-            **compile_opts),
-        Extension('pixell.distances',
-            sources=['cython/distances.c','cython/distances_core.c'],
-            include_dirs=[np.get_include()],
-            **compile_opts),
-        Extension('pixell._interpol_32',
-            sources=['fortran/interpol_32.f90'],
-            **compile_opts),
-        Extension('pixell._interpol_64',
-            sources=['fortran/interpol_64.f90'],
-            **compile_opts),
-        Extension('pixell._colorize',
-            sources=['fortran/colorize.f90'],
-            **compile_opts),
-        Extension('pixell._array_ops_32',
-            sources=['fortran/array_ops_32.f90'],
-            **compile_opts),
-        Extension('pixell._array_ops_64',
-            sources=['fortran/array_ops_64.f90'],
-            **compile_opts),
-    ],
-    include_dirs = ['_deps/libsharp/auto/include'],
-    library_dirs = ['_deps/libsharp/auto/lib'],
-    install_requires=requirements,
-    extras_require = {'fftw':['pyFFTW>=0.10'],'mpi':['mpi4py>=2.0']},
-    license="BSD license",
-    long_description=readme + '\n\n' + history,
-    package_data={'pixell': ['pixell/tests/data/*.fits','pixell/tests/data/*.dat','pixell/tests/data/*.pkl']},
-    include_package_data=True,    
-    data_files=[('pixell', ['pixell/arial.ttf'])],
-    keywords='pixell',
-    name='pixell',
-    packages=find_packages(),
-    test_suite='pixell.tests',
-    tests_require=test_requirements,
-    url='https://github.com/simonsobs/pixell',
-    version=versioneer.get_version(),
-    zip_safe=False,
-    cmdclass=cmdclass,
-    scripts=['scripts/test-pixell']
-)
-
-print('\n[setup.py request was successful.]')
-
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print("Plain-Python installation succeeded.")
+    print('*' * 75)
