7e82b0fb6ad6f243b47249727be0afceb1044600
wu <wu@.com>
2025-07-04 23:14:06 +0800

    spinlud/py-linkedin-jobs-scraper

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
-#!/usr/bin/env python
+from setuptools import setup
 
-import os
-from pathlib import Path
-import setuptools
-import versioneer
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
 
-install_requires = [
-    line.rstrip() for line in open(Path(__file__).parent / "REQUIREMENTS.txt")
-]
-
-with open(Path("starfish") / "core" / "_display.py") as f:
-    for line in f.readlines():
-        if line.startswith("NAPARI_VERSION"):
-            napari_version = line.split('"')[1]
-            break
-
-setuptools.setup(
-    packages=setuptools.find_packages(),
-    install_requires=install_requires,
-    extras_require={
-        'napari': [f"napari[all]>={napari_version}"],
-    },
-    entry_points={
-        'console_scripts': [
-            "starfish=starfish.core.starfish:starfish",
-        ]
-    },
-    include_package_data=True,
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
+setup(
+    name='linkedin-jobs-scraper',
+    version='1.4.0',
+    author='Ludovico Fabbri',
+    author_email='ludovico.fabbri@gmail.com',
+    description='Scrape public available jobs on Linkedin using headless browser',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/spinlud/py-linkedin-jobs-scraper.git',
+    packages=[
+        'linkedin_jobs_scraper',
+        'linkedin_jobs_scraper.chrome_cdp',
+        'linkedin_jobs_scraper.events',
+        'linkedin_jobs_scraper.exceptions',
+        'linkedin_jobs_scraper.filters',
+        'linkedin_jobs_scraper.query',
+        'linkedin_jobs_scraper.strategies',
+        'linkedin_jobs_scraper.utils',
+    ],
+    install_requires=[
+        'selenium',
+        'websocket-client'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
