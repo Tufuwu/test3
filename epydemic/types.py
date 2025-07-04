c71a262edd25e8eddc958a0e62f87d5ce3a1843d
wu <wu@.com>
2025-07-04 15:21:54 +0800

    simoninireland/epydemic

--- epydemic/types.py
+++ epydemic/types.py
@@ -1,4 +1,4 @@
-# Initialisation for epydemic tests package
+# Helper types
 #
 # Copyright (C) 2017--2020 Simon Dobson
 # 
@@ -17,4 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with epydemic. If not, see <http://www.gnu.org/licenses/gpl.html>.
 
-from .compartmenteddynamics import CompartmentedDynamicsTest
+from typing import Union, Tuple, Any
+
+# the parts of a network (graph)
+Node = Any                   #: A node in a network, which may be any object.
+Edge = Tuple[Node, Node]     #: An edge in a network, connecting exactly two nodes (which may be the same).
+Element = Union[Node, Edge]  #: An element in a simulation, either a node or an edge.
+
+
