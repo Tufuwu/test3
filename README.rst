ca91ab45a057a0dfdc79b340f0f5661ef8bbf851
wu <wu@.com>
2025-07-05 22:09:18 +0800

    tohojo/flent

--- README.rst
+++ README.rst
@@ -1,161 +1,89 @@
-=======
-Shapely
-=======
+Flent: The FLExible Network Tester
+==================================
 
-|github-actions| |appveyor| |coveralls|
+Flent is a Python wrapper to run multiple simultaneous netperf/iperf/ping
+instances and aggregate the results. It was previously known as
+'netperf-wrapper'. See the web site for the main documentation:
+https://flent.org.
 
-.. |github-actions| image:: https://github.com/Toblerity/Shapely/workflows/Tests%20(Linux)/badge.svg
-   :target: https://github.com/Toblerity/Shapely/actions
+Installing Flent
+----------------
+Installing Flent can be done in several ways, depending on your operating system:
 
-.. |appveyor| image:: https://ci.appveyor.com/api/projects/status/github/Toblerity/Shapely?branch=master&svg=true
-   :target: https://ci.appveyor.com/project/frsci/shapely?branch=master
 
-.. |coveralls| image:: https://coveralls.io/repos/github/Toblerity/Shapely/badge.svg?branch=master
-   :target: https://coveralls.io/github/Toblerity/Shapely?branch=master
+- **Debian and Ubuntu:**
 
-Manipulation and analysis of geometric objects in the Cartesian plane.
+    .. code-block:: bash
 
-.. image:: https://c2.staticflickr.com/6/5560/31301790086_b3472ea4e9_c.jpg
-   :width: 800
-   :height: 378
+      apt install flent
 
-Shapely is a BSD-licensed Python package for manipulation and analysis of
-planar geometric objects. It is based on the widely deployed `GEOS
-<https://trac.osgeo.org/geos/>`__ (the engine of `PostGIS
-<http://postgis.org>`__) and `JTS
-<https://locationtech.github.io/jts/>`__ (from which GEOS is ported)
-libraries. Shapely is not concerned with data formats or coordinate systems,
-but can be readily integrated with packages that are. For more details, see:
+- **Fedora:**
 
-* `Shapely GitHub repository <https://github.com/Toblerity/Shapely>`__
-* `Shapely documentation and manual <https://shapely.readthedocs.io/en/latest/>`__
+    .. code-block:: bash
 
-Usage
-=====
+      dnf install flent
 
-Here is the canonical example of building an approximately circular patch by
-buffering a point.
+- **Ubuntu pre-18.04:**
 
-.. code-block:: pycon
+  Add the `tohojo/flent PPA <https://launchpad.net/~tohojo/+archive/ubuntu/flent>`_.
 
-    >>> from shapely.geometry import Point
-    >>> patch = Point(0.0, 0.0).buffer(10.0)
-    >>> patch
-    <shapely.geometry.polygon.Polygon object at 0x...>
-    >>> patch.area
-    313.65484905459385
+- **Arch Linux:**
 
-See the manual for more examples and guidance.
+  Install Flent from `the AUR <https://aur.archlinux.org/packages/flent>`_.
 
-Requirements
-============
+- **Other Linux:**
 
-Shapely 1.7 requires
+  Install from the `Python Package Index <https://pypi.python.org/pypi/flent>`_:
+  
+    .. code-block:: bash
 
-* Python 2.7, >=3.5
-* GEOS >=3.3
+      pip install flent
 
-Installing Shapely
-==================
+- **FreeBSD:**
 
-Shapely may be installed from a source distribution or one of several kinds
-of built distribution.
+  Install the package
 
-Built distributions
--------------------
+    .. code-block:: bash
 
-Built distributions are the only option for users who do not have or do not
-know how to use their platform's compiler and Python SDK, and a good option for
-users who would rather not bother.
+      pkg install flent
 
-Linux, OS X, and Windows users can get Shapely wheels with GEOS included from the
-Python Package Index with a recent version of pip (8+):
+  Or install the port
+  
+    .. code-block:: bash
 
-.. code-block:: console
+        cd /usr/ports/net/flent && make install
 
-    $ pip install shapely
+- **macOS:**
 
-Shapely is available via system package management tools like apt, yum, and
-Homebrew, and is also provided by popular Python distributions like Canopy and
-Anaconda. If you use the Conda package manager to install Shapely, be sure to
-use the conda-forge channel.
+  `Homebrew <https://brew.sh/>`_ and Python 3 must be installed (Python 3 can be installed using Homebrew)
 
-Windows users have another good installation options: the wheels published at
-https://www.lfd.uci.edu/~gohlke/pythonlibs/#shapely. These can be installed
-using pip by specifying the entire URL.
+  Install the `patched netperf package <https://github.com/kris-anderson/homebrew-netperf>`_
 
-Source distributions
---------------------
+    .. code-block:: bash
 
-If you want to build Shapely from source for compatibility with other modules
-that depend on GEOS (such as cartopy or osgeo.ogr) or want to use a different
-version of GEOS than the one included in the project wheels you should first
-install the GEOS library, Cython, and Numpy on your system (using apt, yum,
-brew, or other means) and then direct pip to ignore the binary wheels.
+      brew tap kris-anderson/netperf
+      brew install netperf-enable-demo
 
-.. code-block:: console
+  Install other dependencies
 
-    $ pip install shapely --no-binary shapely
+    .. code-block:: bash
 
-If you've installed GEOS to a standard location, the geos-config program will
-be used to get compiler and linker options. If geos-config is not on your
-executable, it can be specified with a GEOS_CONFIG environment variable, e.g.:
+      brew install fping
+      pip3 install matplotlib --user
 
-.. code-block:: console
+  Install Flent using pip
 
-    $ GEOS_CONFIG=/path/to/geos-config pip install shapely
+    .. code-block:: bash
 
-Integration
-===========
+      pip3 install flent --user
 
-Shapely does not read or write data files, but it can serialize and deserialize
-using several well known formats and protocols. The shapely.wkb and shapely.wkt
-modules provide dumpers and loaders inspired by Python's pickle module.
+  Optional (install this if you want to use `flent-gui`)
 
-.. code-block:: pycon
+    .. code-block:: bash
 
-    >>> from shapely.wkt import dumps, loads
-    >>> dumps(loads('POINT (0 0)'))
-    'POINT (0.0000000000000000 0.0000000000000000)'
+      pip3 install pyqt5 qtpy --user
 
-Shapely can also integrate with other Python GIS packages using GeoJSON-like
-dicts.
+Quick Start
+-----------
 
-.. code-block:: pycon
-
-    >>> import json
-    >>> from shapely.geometry import mapping, shape
-    >>> s = shape(json.loads('{"type": "Point", "coordinates": [0.0, 0.0]}'))
-    >>> s
-    <shapely.geometry.point.Point object at 0x...>
-    >>> print(json.dumps(mapping(s)))
-    {"type": "Point", "coordinates": [0.0, 0.0]}
-
-Development and Testing
-=======================
-
-Dependencies for developing Shapely are listed in requirements-dev.txt. Cython
-and Numpy are not required for production installations, only for development.
-Use of a virtual environment is strongly recommended.
-
-.. code-block:: console
-
-    $ virtualenv .
-    $ source bin/activate
-    (env)$ pip install -r requirements-dev.txt
-    (env)$ pip install -e .
-
-The project uses pytest to run Shapely's suite of unittests and doctests.
-
-.. code-block:: console
-
-    (env)$ python -m pytest
-
-Support
-=======
-
-Questions about using Shapely may be asked on the `GIS StackExchange
-<https://gis.stackexchange.com/questions/tagged/shapely>`__ using the "shapely"
-tag.
-
-Bugs may be reported at https://github.com/Toblerity/Shapely/issues.
+See https://flent.org/intro.html#quick-start or doc/quickstart.rst.
--- setup.py
+++ setup.py
@@ -1,381 +1,148 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# setup.py
+#
+# Author:   Toke Høiland-Jørgensen (toke@toke.dk)
+# Date:      4 December 2012
+# Copyright (c) 2012-2016, Toke Høiland-Jørgensen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Build or install Shapely distributions
-#
-# This script has two different uses.
-#
-# 1) Installing from a source distribution, whether via
-#
-#      ``python setup.py install``
-#
-#    after downloading a source distribution, or
-#
-#      ``pip install shapely``
-#
-#    on a platform for which pip cannot find a wheel. This will most
-#    often be the case for Linux, since the project is not yet
-#    publishing Linux wheels. This will never be the case on Windows and
-#    rarely the case on OS X; both are wheels-first platforms.
-#
-# 2) Building distributions (source or wheel) from a repository. This
-#    includes using Cython to generate C source for the speedups and
-#    vectorize modules from Shapely's .pyx files.
-#
-# On import, Shapely loads a GEOS shared library. GEOS is a run time
-# requirement. Additionally, the speedups and vectorized C extension
-# modules need GEOS headers and libraries to be built. Shapely versions
-# >=1.3 require GEOS >= 3.3.
-#
-# For the first use case (see 1, above), we aim to treat GEOS as if it
-# were a Python requirement listed in ``install_requires``. That is, in
-# an environment with Shapely 1.2.x and GEOS 3.2, the command ``pip
-# install shapely >=1.3 --no-use-wheel`` (whether wheels are explicitly
-# opted against or are not published for the platform) should fail with
-# a warning and advice to upgrade GEOS to >=3.3.
-#
-# In case 1, the environment's GEOS version is determined by executing
-# the geos-config script. If the GEOS version returned by that script is
-# incompatible with the Shapely source distribution or no geos-config
-# script can be found, this setup script will fail.
-#
-# For the second use case (see 2, distribution building, above), we
-# allow the requirements to be loosened. If this script finds that the
-# environment variable NO_GEOS_CHECK is set, geos-config will not be
-# executed and no attempt will be made to enforce requirements as in the
-# second case.
-#
-# For both cases, a geos-config not in the environment's $PATH may be
-# used by setting the environment variable GEOS_CONFIG to the path to
-# a geos-config script.
-#
-# NB: within this setup scripts, software versions are evaluated according
-# to https://www.python.org/dev/peps/pep-0440/.
+from __future__ import absolute_import, division, print_function
 
-import errno
-import glob
-import itertools as it
-import logging
 import os
-import platform
-import re
-import shutil
-import subprocess
 import sys
+
 from setuptools import setup
-from setuptools.extension import Extension
-from setuptools.command.build_ext import build_ext as distutils_build_ext
-from distutils.errors import CCompilerError, DistutilsExecError, \
-    DistutilsPlatformError
+from setuptools.command.build_py import build_py as _build_py
+from setuptools.command.sdist import sdist as _sdist
 
-from _vendor.packaging.version import Version
+from flent.build_info import VERSION
+from glob import glob
 
-# Get geos_version from GEOS dynamic library, which depends on
-# GEOS_LIBRARY_PATH and/or GEOS_CONFIG environment variables
-from shapely._buildcfg import geos_version_string, geos_version, \
-        geos_config, get_geos_config
+version_string = VERSION
 
-logging.basicConfig()
-log = logging.getLogger(__file__)
-
-# python -W all setup.py ...
-if 'all' in sys.warnoptions:
-    log.level = logging.DEBUG
+if sys.version_info[:3] < (3, 5, 0):
+    sys.stderr.write("Sorry, Flent requires v3.5 or later of Python.\n")
+    sys.exit(1)
 
 
-class GEOSConfig(object):
-    """Interface to config options from the `geos-config` utility
+def rewrite_build_info(module_file):
+    with open(module_file, 'w') as module_fp:
+        module_fp.write('# -*- coding: UTF-8 -*-\n\n')
+        module_fp.write("import os\n")
+        module_fp.write("VERSION='%s'\n" % (version_string))
+        module_fp.write("DATA_DIR=os.path.dirname(__file__)\n")
+
+
+class build_py(_build_py):
+    """build_py command
+
+    This specific build_py command will modify module
+    'flent.build_config' so that it contains information on
+    installation prefixes afterwards.
     """
 
-    def __init__(self, cmd):
-        self.cmd = cmd
+    def build_module(self, module, module_file, package):
+        orig_content = None
+        if module == 'build_info' and package == 'flent':
+            with open(module_file, 'rb') as module_fp:
+                orig_content = module_fp.read()
 
-    def get(self, option):
-        try:
-            stdout, stderr = subprocess.Popen(
-                [self.cmd, option],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        except OSError as ex:
-            # e.g., [Errno 2] No such file or directory
-            raise OSError("Could not find geos-config script")
-        if stderr and not stdout:
-            raise ValueError(stderr.strip())
-        result = stdout.decode('ascii').strip()
-        log.debug('%s %s: %r', self.cmd, option, result)
-        return result
+            rewrite_build_info(module_file)
 
-    def version(self):
-        match = re.match(r'(\d+)\.(\d+)\.(\d+)', self.get('--version').strip())
-        return tuple(map(int, match.groups()))
+        _build_py.build_module(self, module, module_file, package)
 
-# Get the version from the shapely module.
-shapely_version = None
-with open('shapely/__init__.py', 'r') as fp:
-    for line in fp:
-        if line.startswith("__version__"):
-            shapely_version = Version(
-                line.split("=")[1].strip().strip("\"'"))
-            break
+        if orig_content is not None:
+            with open(module_file, 'wb') as module_fp:
+                module_fp.write(orig_content)
 
-if not shapely_version:
-    raise ValueError("Could not determine Shapely's version")
 
-# Allow GEOS_CONFIG to be bypassed in favor of CFLAGS and LDFLAGS
-# vars set by build environment.
-if os.environ.get('NO_GEOS_CONFIG'):
-    geos_config = None
-else:
-    geos_config = GEOSConfig(os.environ.get('GEOS_CONFIG', 'geos-config'))
+class sdist(_sdist):
 
-# Fail installation if the GEOS shared library does not meet the minimum
-# version. We ship it with Shapely for Windows, so no need to check on
-# that platform.
-geos_version = None
-if geos_config and not os.environ.get('NO_GEOS_CHECK') or sys.platform == 'win32':
-    try:
-        log.info(
-            "Shapely >= 1.3 requires GEOS >= 3.3. "
-            "Checking for GEOS version...")
-        geos_version = geos_config.version()
-        log.info("Found GEOS version: %s", geos_version)
-        if (set(sys.argv).intersection(['install', 'build', 'build_ext']) and
-                shapely_version >= Version("1.3") and geos_version < (3, 3)):
-            log.critical(
-                "Shapely >= 1.3 requires GEOS >= 3.3. "
-                "Install GEOS 3.3+ and reinstall Shapely.")
-            sys.exit(1)
-    except OSError as exc:
-        log.warning(
-            "Failed to determine system's GEOS version: %s. "
-            "Installation continuing. GEOS version will be "
-            "checked on import of shapely.", exc)
-
-with open('VERSION.txt', 'w') as fp:
-    fp.write(str(shapely_version))
-
-with open('README.rst', 'r') as fp:
-    readme = fp.read()
-
-with open('CREDITS.txt', 'r', encoding='utf-8') as fp:
-    credits = fp.read()
-
-with open('CHANGES.txt', 'r') as fp:
-    changes = fp.read()
-
-long_description = readme + '\n\n' + credits + '\n\n' + changes
-
-extra_reqs = {
-    'test': ['pytest', 'pytest-cov'],
-    'vectorized': ['numpy']}
-extra_reqs['all'] = list(it.chain.from_iterable(extra_reqs.values()))
-
-# Make a dict of setup arguments. Some items will be updated as
-# the script progresses.
-setup_args = dict(
-    name                = 'Shapely',
-    version             = str(shapely_version),
-    description         = 'Geometric objects, predicates, and operations',
-    license             = 'BSD',
-    keywords            = 'geometry topology gis',
-    author              = 'Sean Gillies',
-    author_email        = 'sean.gillies@gmail.com',
-    maintainer          = 'Sean Gillies',
-    maintainer_email    = 'sean.gillies@gmail.com',
-    url                 = 'https://github.com/Toblerity/Shapely',
-    long_description    = long_description,
-    packages            = [
-        'shapely',
-        'shapely.geometry',
-        'shapely.algorithms',
-        'shapely.examples',
-        'shapely.speedups',
-        'shapely.vectorized',
-    ],
-    classifiers         = [
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Scientific/Engineering :: GIS',
-    ],
-    cmdclass           = {},
-    python_requires    = '>=3.5',
-    extras_require     = extra_reqs,
-    package_data={
-        'shapely': ['shapely/_geos.pxi']},
-    include_package_data=True
-)
-
-# Add DLLs for Windows.
-if sys.platform == 'win32':
-    try:
-        os.mkdir('shapely/DLLs')
-    except OSError as ex:
-        if ex.errno != errno.EEXIST:
-            raise
-    if '(AMD64)' in sys.version:
-        for dll in glob.glob('DLLs_AMD64_VC9/*.dll'):
-            shutil.copy(dll, 'shapely/DLLs')
-    else:
-        for dll in glob.glob('DLLs_x86_VC9/*.dll'):
-            shutil.copy(dll, 'shapely/DLLs')
-    setup_args['package_data']['shapely'].append('shapely/DLLs/*.dll')
-
-# Prepare build opts and args for the speedups extension module.
-include_dirs = []
-library_dirs = []
-libraries = []
-extra_link_args = []
-
-# If NO_GEOS_CONFIG is set in the environment, geos-config will not
-# be called and CFLAGS and LDFLAGS environment variables must be set
-# instead like
-#
-# CFLAGS="-I/usr/local/include" LDFLAGS="-L/usr/local/lib -lgeos_c"
-#
-# Or, equivalently:
-#
-# CFLAGS="$(geos-config --cflags)" LDFLAGS="$(geos-config --clibs)"
-
-if geos_version and geos_config:
-    # Collect other options from GEOS configuration.
-    for item in geos_config.get('--cflags').split():
-        if item.startswith("-I"):
-            include_dirs.extend(item[2:].split(":"))
-    for item in geos_config.get('--clibs').split():
-        if item.startswith("-L"):
-            library_dirs.extend(item[2:].split(":"))
-        elif item.startswith("-l"):
-            libraries.append(item[2:])
+    def make_release_tree(self, base_dir, files):
+        if 'flent/build_info.py' in files and not self.dry_run:
+            files = [f for f in files if f != 'flent/build_info.py']
+            _sdist.make_release_tree(self, base_dir, files)
+            rewrite_build_info(os.path.join(base_dir, 'flent/build_info.py'))
         else:
-            # e.g. -framework GEOS
-            extra_link_args.append(item)
+            _sdist.make_release_tree(self, base_dir, files)
 
 
-# Optional compilation of speedups
-# setuptools stuff from Bob Ippolito's simplejson project
-if sys.platform == 'win32':
-    # distutils.msvc9compiler can raise an IOError when failing to
-    # find the compiler
-    ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError,
-                  IOError)
-else:
-    ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
+data_files = [('share/doc/flent',
+               ['BUGS',
+                'README.rst',
+                'CHANGES.md',
+                'flent-paper.batch'] + glob("*.example")),
+              ('share/man/man1',
+               ['man/flent.1']),
+              ('share/doc/flent/misc',
+               glob("misc/*")),
+              ('share/mime/packages',
+               ['flent-mime.xml']),
+              ('share/applications',
+               ['flent.desktop']),
+              ('share/metainfo',
+               ['flent.appdata.xml'])]
 
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Environment :: MacOS X',
+    'Environment :: X11 Applications',
+    'Environment :: X11 Applications :: Qt',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: System Administrators',
+    'Intended Audience :: Telecommunications Industry',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: POSIX',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Internet',
+    'Topic :: System :: Benchmark',
+    'Topic :: System :: Networking',
+    'Topic :: Utilities',
+]
 
-class BuildFailed(Exception):
-    pass
+with open("README.rst") as fp:
+    long_description = "\n" + fp.read()
 
-
-def construct_build_ext(build_ext):
-    class WrappedBuildExt(build_ext):
-        # This class allows C extension building to fail.
-
-        def run(self):
-            try:
-                build_ext.run(self)
-            except DistutilsPlatformError as x:
-                raise BuildFailed(x)
-
-        def build_extension(self, ext):
-            try:
-                build_ext.build_extension(self, ext)
-            except ext_errors as x:
-                raise BuildFailed(x)
-
-    return WrappedBuildExt
-
-if (platform.python_implementation() == 'PyPy'):
-    ext_modules = []
-    libraries = []
-
-
-pyx_file = "shapely/speedups/_speedups.pyx"
-c_file = "shapely/speedups/_speedups.c"
-
-force_cython = False
-# Always regenerate for sdist or absent c file
-if 'sdist' in sys.argv or not os.path.exists(c_file):
-    force_cython = True
-# Also regenerate if pyx_file is outdated.
-elif os.path.exists(c_file):
-    if os.path.getmtime(pyx_file) > os.path.getmtime(c_file):
-        force_cython = True
-
-ext_modules = [
-    Extension("shapely.speedups._speedups", ["shapely/speedups/_speedups.c"],
-        include_dirs=include_dirs, library_dirs=library_dirs,
-        libraries=libraries, extra_link_args=extra_link_args)]
-
-cmd_classes = setup_args.setdefault('cmdclass', {})
-
-try:
-    import numpy
-    from Cython.Distutils import build_ext as cython_build_ext
-    from distutils.extension import Extension as DistutilsExtension
-
-    if 'build_ext' in setup_args['cmdclass']:
-        raise ValueError('We need to put the Cython build_ext in '
-                         'cmd_classes, but it is already defined.')
-    setup_args['cmdclass']['build_ext'] = cython_build_ext
-
-    include_dirs.append(numpy.get_include())
-    libraries.append(numpy.get_include())
-
-    ext_modules.append(DistutilsExtension(
-        "shapely.vectorized._vectorized",
-        sources=["shapely/vectorized/_vectorized.pyx"],
-        include_dirs=include_dirs,
-        library_dirs=library_dirs,
-        libraries=libraries,
-        extra_link_args=extra_link_args,))
-
-except ImportError:
-    log.info("Numpy or Cython not available, shapely.vectorized submodule "
-             "not being built.")
-    force_cython = False
-
-try:
-    if force_cython:
-        log.info("Updating C extension with Cython.")
-        subprocess.check_call(["cython", "shapely/speedups/_speedups.pyx"])
-except (subprocess.CalledProcessError, OSError):
-    log.warning("Could not (re)create C extension with Cython.")
-    if force_cython:
-        raise
-
-if not os.path.exists(c_file):
-    log.warning("speedup extension not found")
-
-try:
-    # try building with speedups
-    existing_build_ext = setup_args['cmdclass'].\
-        get('build_ext', distutils_build_ext)
-    setup_args['cmdclass']['build_ext'] = \
-        construct_build_ext(existing_build_ext)
-    setup(ext_modules=ext_modules, **setup_args)
-except BuildFailed as ex:
-    BUILD_EXT_WARNING = "The C extension could not be compiled, " \
-                        "speedups are not enabled."
-    log.warning(ex)
-    log.warning(BUILD_EXT_WARNING)
-    log.warning("Failure information, if any, is above.")
-    log.warning("I'm retrying the build without the C extension now.")
-
-    # Remove any previously defined build_ext command class.
-    if 'build_ext' in setup_args['cmdclass']:
-        del setup_args['cmdclass']['build_ext']
-
-    if 'build_ext' in cmd_classes:
-        del cmd_classes['build_ext']
-
-    setup(**setup_args)
-
-    log.warning(BUILD_EXT_WARNING)
-    log.info("Plain-Python installation succeeded.")
+setup(name="flent",
+      version=version_string,
+      description="The FLExible Network Tester",
+      long_description=long_description,
+      include_package_data=True,
+      author="Toke Høiland-Jørgensen <toke@toke.dk>",
+      author_email="toke@toke.dk",
+      url="http://flent.org",
+      license="GNU GPLv3",
+      classifiers=classifiers,
+      packages=["flent"],
+      entry_points={'console_scripts': ['flent = flent:run_flent'],
+                    'gui_scripts': ['flent-gui = flent:run_flent_gui']},
+      zip_safe=False,
+      data_files=data_files,
+      cmdclass={'build_py': build_py, 'sdist': sdist},
+      extras_require={
+          'GUI': ['QtPy', 'PyQt5'],
+          'Plots': ['matplotlib>=1.5'],
+      },
+      )
