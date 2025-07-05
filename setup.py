fd7ea36c869890c6b421d3ac20e7ab581d33fac7
wu <wu@.com>
2025-07-05 17:18:53 +0800

    thomasballinger/curtsies

--- setup.py
+++ setup.py
@@ -1,53 +1,49 @@
-"""Setup file for mysensors package."""
-from pathlib import Path
+from setuptools import setup
+import ast
+import os
+import io
 
-from setuptools import setup, find_packages
 
-PROJECT_DIR = Path(__file__).parent.resolve()
-VERSION = (PROJECT_DIR / "mysensors" / "VERSION").read_text().strip()
+def version():
+    """Return version string."""
+    with open(os.path.join("curtsies", "__init__.py")) as input_file:
+        for line in input_file:
+            if line.startswith("__version__"):
+                return ast.parse(line).body[0].value.s
 
-README_FILE = PROJECT_DIR / "README.md"
-LONG_DESCR = README_FILE.read_text(encoding="utf-8")
 
-REQUIRES = [
-    "click",
-    "crcmod>=1.7",
-    "getmac",
-    "IntelHex>=2.2.1",
-    "pyserial>=3.4",
-    "pyserial-asyncio>=0.4",
-    "voluptuous>=0.11.1",
-]
-EXTRAS = {"mqtt-client": ["paho-mqtt"]}
+def long_description():
+    with open("readme.md", encoding="utf-8") as f:
+        return f.read()
 
 
 setup(
-    name="pymysensors",
-    version=VERSION,
-    description="Python API for talking to a MySensors gateway",
-    long_description=LONG_DESCR,
+    name="curtsies",
+    version=version(),
+    description="Curses-like terminal wrapper, with colored strings!",
+    long_description=long_description(),
     long_description_content_type="text/markdown",
-    url="https://github.com/theolind/pymysensors",
-    author="Theodor Lindquist",
-    author_email="theodor.lindquist@gmail.com",
-    license="MIT License",
-    install_requires=REQUIRES,
-    extras_require=EXTRAS,
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    python_requires=">=3.5.3",
-    entry_points={"console_scripts": ["pymysensors = mysensors.cli:cli"]},
-    keywords=["sensor", "actuator", "IoT", "DYI"],
-    zip_safe=True,
-    classifiers=[
-        "Intended Audience :: Developers",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Home Automation",
+    url="https://github.com/bpython/curtsies",
+    author="Thomas Ballinger",
+    author_email="thomasballinger@gmail.com",
+    license="MIT",
+    packages=["curtsies"],
+    install_requires=[
+        "blessings>=1.5",
+        "wcwidth>=0.1.4",
     ],
+    tests_require=[
+        "pyte",
+        "pytest",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    zip_safe=False,
 )
