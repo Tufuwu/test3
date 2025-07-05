c79268d4493e41e648465935f2999027c7c47517
wu <wu@.com>
2025-07-05 14:03:26 +0800

    strongswan/strongtnc

--- runtests.py
+++ runtests.py
@@ -1,22 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os
 import sys
 import pytest
 import coverage
+from django.test import TransactionTestCase
+
+TransactionTestCase.databases = {'default', 'meta'}
 
 if __name__ == '__main__':
     # Environment variables
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strongMan.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings_tests')
 
     measure_coverage = True
     if '--no-cov' in sys.argv:
         measure_coverage = False
         sys.argv.remove('--no-cov')
-
-    def_args = ['strongMan/apps', 'strongMan/helper_apps', 'strongMan/tests/tests']
-    if '--no-vici' in sys.argv:
-        def_args.append('--ignore=strongMan/tests/tests/vici/')
-        sys.argv.remove('--no-vici')
 
     # Start coverage tracking
     if measure_coverage:
@@ -27,7 +25,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         code = pytest.main(sys.argv)
     else:
-        code = pytest.main(def_args)
+        code = pytest.main(['tests', 'apps'])
 
     # Show coverage report
     if measure_coverage:
