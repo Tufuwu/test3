5cce58fb3766d6682aa5e6b3b727eb4872bb9150
wu <wu@.com>
2025-07-06 11:33:04 +0800

    vstinner/python-ptrace

--- setup.py
+++ setup.py
@@ -1,48 +1,78 @@
-from setuptools import setup
-from tools.configuration import Configuration
+#!/usr/bin/env python
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+# Prepare a release:
+#
+#  - git pull --rebase  # check that there is no incoming changesets
+#  - check version in ptrace/version.py and doc/conf.py
+#  - set release date in doc/changelog.rst
+#  - check that "python3 setup.py sdist" contains all files tracked by
+#    the SCM (Git): update MANIFEST.in if needed
+#  - git commit -a -m "prepare release VERSION"
+#  - Remove untracked files/dirs: git clean -fdx
+#  - run tests, type: tox --parallel auto
+#  - git push
+#  - check GitHub Actions status:
+#    https://github.com/vstinner/python-ptrace/actions
+#
+# Release a new version:
+#
+#  - git tag VERSION
+#  - Remove untracked files/dirs: git clean -fdx
+#  - python3 setup.py sdist bdist_wheel
+#  - git push --tags
+#  - twine upload dist/*
+#
+# After the release:
+#
+#  - increment version in  ptrace/version.py and doc/conf.py
+#  - git commit -a -m "post-release"
+#  - git push
 
-with open('requirements.txt', "r") as req_file:
-    requirements = req_file.read().splitlines()
+from imp import load_source
+from os import path
+try:
+    # setuptools supports bdist_wheel
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
-# such name for package is used here because 'repostat' is already occupied by https://pypi.org/project/repostat/
-setup(name='repostat-app',
-      version=Configuration.get_release_data_info()['develop_version'],
-      description='Desktop git repository analyser and report creator.',
-      keywords='git analysis statistics vcs python visualization',
-      url='https://github.com/vifactor/repostat',
-      author='Viktor Kopp',
-      author_email='vifactor@gmail.com',
-      license='GPLv3',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Environment :: Console",
-          "Intended Audience :: Developers",
-          "Intended Audience :: Science/Research",
-          "Intended Audience :: Education",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-          "Topic :: Software Development :: Version Control",
-          "Topic :: Utilities",
-          "Operating System :: OS Independent",
-          "Operating System :: POSIX",
-          "Operating System :: MacOS :: MacOS X",
-          "Operating System :: Microsoft :: Windows",
-      ],
-      python_requires='>3.5',
-      packages=['analysis', 'tools', 'report'],
-      package_data={'report': ['templates/*.html',
-                               'templates/*.js',
-                               'assets/images/*.gif',
-                               'assets/*.js',
-                               'assets/*.css'],
-                    'tools': ['release_data.json']},
-      install_requires=requirements,
-      entry_points={"console_scripts": ["repostat = analysis.repostat:main"]},
-      include_package_data=True,
-      zip_safe=False)
+
+MODULES = ["ptrace", "ptrace.binding", "ptrace.syscall", "ptrace.syscall.linux", "ptrace.debugger"]
+
+SCRIPTS = ("strace.py", "gdb.py")
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Natural Language :: English',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+]
+
+with open('README.rst') as fp:
+    LONG_DESCRIPTION = fp.read()
+
+ptrace = load_source("version", path.join("ptrace", "version.py"))
+PACKAGES = {}
+for name in MODULES:
+    PACKAGES[name] = name.replace(".", "/")
+
+install_options = {
+    "name": ptrace.PACKAGE,
+    "version": ptrace.__version__,
+    "url": ptrace.WEBSITE,
+    "download_url": ptrace.WEBSITE,
+    "author": "Victor Stinner",
+    "description": "python binding of ptrace",
+    "long_description": LONG_DESCRIPTION,
+    "classifiers": CLASSIFIERS,
+    "license": ptrace.LICENSE,
+    "packages": list(PACKAGES.keys()),
+    "package_dir": PACKAGES,
+    "scripts": SCRIPTS,
+}
+
+setup(**install_options)
