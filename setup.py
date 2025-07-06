bd6e2d47ae68deb1fbd55852f2513f9502caecd7
wu <wu@.com>
2025-07-06 17:56:38 +0800

    danirus/django-comments-xtd

--- setup.py
+++ setup.py
@@ -1,43 +1,58 @@
-from importlib.machinery import SourceFileLoader
+import sys
 
-import setuptools
-
-version = SourceFileLoader('version', 'clabe/version.py').load_module()
-
-
-test_requires = [
-    'pytest<5.2',
-    'coverage<5',
-    'pytest-cov',
-    'black==22.3.0',
-    'isort[pipfile]',
-    'flake8',
-    'mypy',
-]
-
-with open('README.md', 'r') as f:
-    long_description = f.read()
+from setuptools import setup, find_packages
+from setuptools.command.test import test
 
 
-setuptools.setup(
-    name='clabe',
-    version=version.__version__,
-    author='Cuenca',
-    author_email='dev@cuenca.com',
-    description='Validate and generate the control digit of a CLABE in Mexico',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/cuenca-mx/clabe',
-    packages=setuptools.find_packages(),
+def run_tests(*args):
+    from django_comments_xtd.tests import run_tests
+    errors = run_tests()
+    if errors:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+
+
+test.run_tests = run_tests
+
+
+setup(
+    name="django-comments-xtd",
+    version="2.8.2",
+    packages=find_packages(),
     include_package_data=True,
-    package_data=dict(mati=['py.typed']),
-    install_requires=['pydantic>=1.4,<2.0'],
-    setup_requires=['pytest-runner'],
-    tests_require=test_requires,
-    extras_require=dict(test=test_requires),
+    license="MIT",
+    description=("Django Comments Framework extension app with thread "
+                 "support, follow up notifications and email "
+                 "confirmations."),
+    long_description=("A reusable Django app that extends django-contrib-"
+                      "comments Framework with thread support, following up "
+                      "notifications and comments that only hits the "
+                      "database after users confirm them by email."),
+    author="Daniel Rus Morales",
+    author_email="mbox@danir.us",
+    maintainer="Daniel Rus Morales",
+    maintainer_email="mbox@danir.us",
+    url="http://pypi.python.org/pypi/django-comments-xtd",
+    install_requires=[
+        'Django>=2.2',
+        'django-contrib-comments>=1.9',
+        'djangorestframework>=3.9',
+        'docutils',
+        'six',
+    ],
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+        'Natural Language :: English',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
     ],
+    test_suite="dummy",
+    zip_safe=True
 )
