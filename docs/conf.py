c4180f9d13a722209c62121f652ffd205be8345b
wu <wu@.com>
2025-07-06 17:35:35 +0800

    cnvogelg/amitools

--- docs/conf.py
+++ docs/conf.py
@@ -1,66 +1,30 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# -*- coding: utf-8 -*-
+# sphinx config
 
-# -- Path setup --------------------------------------------------------------
+project = "amitools"
+copyright = "2020, Christian Vogelgsang"
+author = "Christian Vogelgsang"
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("_ext"))
-
-
-# -- Project information -----------------------------------------------------
-
-project = "django-statici18n"
-copyright = "2012-2020, Sébastien Fievet"
-author = "Sébastien Fievet"
-
-# The full version, including alpha/beta/rc tags
-release = "2.1.1"
-
+version = "0.6"
+release = "0.6.0"
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ["djangodocs", "sphinx.ext.intersphinx"]
-
-intersphinx_mapping = {
-    "python": ("http://docs.python.org/3.8", None),
-    "django": (
-        "https://docs.djangoproject.com/en/2.2/",
-        "https://docs.djangoproject.com/en/2.2/_objects",
-    ),
-}
-
-# Add any paths that contain templates here, relative to this directory.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+]
 templates_path = ["_templates"]
-
-# The master toctree document.
+source_suffix = ".rst"
 master_doc = "index"
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
+language = "en"
+exclude_patterns = []
+pygments_style = None
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "alabaster"
+# html_static_path = ['_static']
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# -- Extension configuration -------------------------------------------------
+intersphinx_mapping = {"https://docs.python.org/": None}
+todo_include_todos = True
