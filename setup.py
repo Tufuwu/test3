de5f20f18d941933dcb18025b9b9ff9a07d74e44
wu <wu@.com>
2025-07-06 10:25:17 +0800

    ungarj/mapchete

--- setup.py
+++ setup.py
@@ -1,114 +1,94 @@
-#!/usr/bin/env python3
-# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+"""Install Mapchete."""
 
-import json
-import subprocess
-import sys
-from pathlib import Path
-from shutil import rmtree, which
-from setuptools import Command, find_packages, setup
-from setuptools.command.build_py import build_py
-from ulauncher import config, VERSION
+import os
+from setuptools import find_packages, setup
 
-icons = {
-    "app": "data/icons/system/default/ulauncher.svg",
-    "indicator": "data/icons/system/default/ulauncher-indicator.svg",
-    "indicator-dark": "data/icons/system/dark/ulauncher-indicator.svg",
-    "indicator-light": "data/icons/system/light/ulauncher-indicator.svg",
-}
+# don't install dependencies when building win readthedocs
+on_rtd = os.environ.get("READTHEDOCS") == "True"
 
+# get version number
+# from https://github.com/mapbox/rasterio/blob/master/setup.py#L55
+with open("mapchete/__init__.py") as f:
+    for line in f:
+        if line.find("__version__") >= 0:
+            version = line.split("=")[1].strip().strip('"').strip("'")
+            break
 
-def data_files_from_path(target_path, source_path):
-    # Creates a list of valid entries for data_files weird custom format
-    # Recurses over the real_path and adds it's content to package_path
-    entries = []
-    for p in Path.cwd().glob(source_path + "/**/*"):
-        if p.is_file():
-            relative_file = p.relative_to(Path(source_path).absolute())
-            entries.append((f"{target_path}/{relative_file.parent}", [f"{source_path}/{relative_file}"]))
-    return entries
+# use README.rst for project long_description
+with open("README.rst") as f:
+    readme = f.read()
 
 
-class build_preferences(Command):
-    description = "Build Ulauncher preferences (Vue.js app)"
-    user_options = [
-        ("force", None, "Rebuild even if source has no modifications since last build"),
-    ]
+def _parse_requirements(file):
+    return sorted(set(
+        line.partition("#")[0].strip()
+        for line in open(os.path.join(os.path.dirname(__file__), file))
+    ) - set(""))
 
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        src = Path("preferences-src")
-        dst = Path("data/preferences")
-
-        if "--force" in sys.argv and dst.is_dir():
-            # Need to do this in particular to avoid packaging node_modules if the user has
-            # been switching between building Ulauncher v5 and v6
-            rmtree(dst)
-
-        if not src.is_dir():
-            raise Exception(f"{src.resolve()} directory missing.")
-
-        sourceModified = max(map(lambda p: p.stat().st_mtime, Path.cwd().glob("preferences-src/**/*")))
-
-        if dst.is_dir() and dst.stat().st_mtime > sourceModified:
-            print("Detected no changes to Preferences since last build.")
-            return
-
-        yarnbin = which("yarn") or which("yarnpkg")
-        subprocess.run(["sh", "-c", f"cd preferences-src; {yarnbin}; {yarnbin} build"], check=True)
-
-
-class build_wrapper(build_py, Command):
-    user_options = [
-        ("with-preferences", None, "Also build preferences (when building from git tree)"),
-    ]
-
-    def run(self):
-        # Build Preferences before python package build
-        if "--with-preferences" in sys.argv:
-            build_preferences.run(self)
-
-        build_py.run(self)
-        print("Overwriting the namespace package with fixed values")
-        Path(self.build_lib + "/ulauncher/__init__.py").write_text(
-            "\n".join(
-                [
-                    "import gi",
-                    f"gi.require_versions({json.dumps(dict(config['gi_versions']))})",
-                    f"ASSETS = '{sys.prefix}/share/ulauncher'",
-                    f"VERSION = '{VERSION}'",
-                ]
-            )
-        )
-
+# dependencies for extra features
+req_contours = ["matplotlib"]
+req_s3 = ["boto3"]
+req_serve = ["flask"]
+req_vrt = ["lxml"]
+req_complete = req_contours + req_s3 + req_serve + req_vrt
 
 setup(
-    packages=find_packages(exclude=["tests", "conftest.py"]),
-    # These will be placed in /usr
-    data_files=[
-        ("share/applications", ["io.ulauncher.Ulauncher.desktop"]),
-        ("lib/systemd/user", ["ulauncher.service"]),
-        ("share/doc/ulauncher", ["README.md"]),
-        ("share/licenses/ulauncher", ["LICENSE"]),
-        # Install icons in themes, so different icons can be used for different depending on theme
-        # It's only needed for the app indicator icon
-        ("share/icons/hicolor/48x48/apps", [icons["app"], icons["indicator"]]),
-        ("share/icons/hicolor/scalable/apps", [icons["app"], icons["indicator"]]),
-        # for Fedora + GNOME
-        ("share/icons/gnome/scalable/apps", [icons["app"], icons["indicator"]]),
-        # for Elementary
-        ("share/icons/elementary/scalable/apps", [icons["indicator-light"]]),
-        # for Ubuntu
-        ("share/icons/breeze/apps/48", [icons["indicator-dark"]]),
-        ("share/icons/ubuntu-mono-dark/scalable/apps", [icons["indicator"]]),
-        ("share/icons/ubuntu-mono-light/scalable/apps", [icons["indicator-dark"]]),
-        # Recursively add data as share/ulauncher
-        *data_files_from_path("share/ulauncher", "data"),
+    name="mapchete",
+    version=version,
+    description="Tile-based geodata processing using rasterio & Fiona",
+    long_description=readme,
+    author="Joachim Ungar",
+    author_email="joachim.ungar@gmail.com",
+    url="https://github.com/ungarj/mapchete",
+    license="MIT",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "mapchete=mapchete.cli.main:main"
+        ],
+        "mapchete.cli.commands": [
+            "convert=mapchete.cli.default.convert:convert",
+            "create=mapchete.cli.default.create:create",
+            "execute=mapchete.cli.default.execute:execute",
+            "formats=mapchete.cli.default.formats:formats",
+            "index=mapchete.cli.default.index:index",
+            "processes=mapchete.cli.default.processes:processes",
+            "serve=mapchete.cli.default.serve:serve",
+        ],
+        "mapchete.formats.drivers": [
+            "geojson=mapchete.formats.default.geojson",
+            "gtiff=mapchete.formats.default.gtiff",
+            "mapchete_input=mapchete.formats.default.mapchete_input",
+            "png_hillshade=mapchete.formats.default.png_hillshade",
+            "png=mapchete.formats.default.png",
+            "raster_file=mapchete.formats.default.raster_file",
+            "vector_file=mapchete.formats.default.vector_file",
+            "tile_directory=mapchete.formats.default.tile_directory"
+        ],
+        "mapchete.processes": [
+            "example_process=mapchete.processes.examples.example_process",
+            "contours=mapchete.processes.contours",
+            "convert=mapchete.processes.convert",
+            "hillshade=mapchete.processes.hillshade"
+        ]
+    },
+    package_dir={"static": "static"},
+    package_data={"mapchete.static": ["*"]},
+    install_requires=_parse_requirements("requirements.txt") if not on_rtd else [],
+    extras_require={
+        "complete": req_complete,
+        "contours": req_contours,
+        "s3": req_s3,
+        "serve": req_serve,
+        "vrt": req_vrt,
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: GIS",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
-    cmdclass={"build_py": build_wrapper, "build_prefs": build_preferences},
 )
