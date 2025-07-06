3377d574d8c1a105f65b45a2ff5dee28ce9b1efe
wu <wu@.com>
2025-07-06 18:07:54 +0800

    dbrgn/result

--- setup.py
+++ setup.py
@@ -1,83 +1,28 @@
-"""
-Links
-`````
-* `documentation
-  <http://pythonhosted.org/pyramid_storage/>`_
-* `development version
-  <https://github.com/danjac/pyramid_storage>`_
+from setuptools import setup
 
-"""
+readme = open('README.rst').read()
 
-from setuptools import setup, Command
-
-
-class PyTest(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import sys
-        import subprocess
-        errno = subprocess.call([sys.executable, 'runtests.py'])
-        raise SystemExit(errno)
-
-
-docs_extras = [
-    'Sphinx',
-    'docutils',
-    'repoze.sphinx.autointerface',
-]
-
-tests_require = [
-    'pytest',
-    'mock',
-]
-
-
-setup(
-    name='pyramid_storage',
-    cmdclass={'test': PyTest},
-    version='0.4.0.dev0',
-    license='BSD',
-    author='Dan Jacob',
-    author_email='danjac354@gmail.com',
-    description='File storage package for Pyramid',
-    long_description=__doc__,
-    url='https://github.com/danjac/pyramid_storage/',
-    packages=[
-        'pyramid_storage',
-    ],
-    zip_safe=False,
-    platforms='any',
-    install_requires=[
-        'pyramid',
-    ],
-    tests_require=tests_require,
-    extras_require={
-        'docs': docs_extras,
-        's3': ['boto'],
-        'gcloud': ['google-cloud-storage'],
-    },
-    test_suite='pyramid_storage',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Topic :: Communications :: Email',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Framework :: Pyramid',
-    ]
-)
+setup(name='result',
+      version='0.5.0',
+      description='A rust-like result type for Python',
+      author='Danilo Bargen',
+      author_email='mail@dbrgn.ch',
+      url='https://github.com/dbrgn/result',
+      packages=['result'],
+      package_data={'result': ['py.typed']},
+      zip_safe=True,
+      include_package_data=True,
+      license='MIT',
+      keywords='rust result enum',
+      long_description=readme,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3 :: Only',
+      ],
+      install_requires=['typing; python_version < "3.5"'])
