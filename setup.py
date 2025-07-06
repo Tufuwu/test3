b22c743876fee25349a00f0d798b562500c335e2
wu <wu@.com>
2025-07-06 16:16:50 +0800

    zac-hd/hypothesis-jsonschema

--- setup.py
+++ setup.py
@@ -1,55 +1,52 @@
-"""
-Python toolkit to work with BUFR messages.
-"""
 import os
 
-from setuptools import setup
+import setuptools
 
 
-def get_version():
-    with open(os.path.join(os.path.dirname(__file__), 'pybufrkit', '__init__.py')) as ins:
-        for line in ins.readlines():
-            if line.startswith('__version__'):
-                return line.split('=')[1].strip()[1:-1]
+def local_file(name: str) -> str:
+    """Interpret filename as relative to this file."""
+    return os.path.relpath(os.path.join(os.path.dirname(__file__), name))
 
 
-def get_requirements():
-    requirements = ['bitstring>=3.1.3', 'six']
-    return requirements
+SOURCE = local_file("src")
+README = local_file("README.md")
+
+with open(local_file("src/hypothesis_jsonschema/__init__.py")) as o:
+    for line in o:
+        if line.startswith("__version__"):
+            _, __version__, _ = line.split('"')
 
 
-setup(
-    name='pybufrkit',
-    version=get_version(),
-    platforms=['any'],
-    packages=['pybufrkit'],
-    package_dir={'pybufrkit': 'pybufrkit'},
-    include_package_data=True,
-    setup_requires=["pytest-runner"],
-    install_requires=get_requirements(),
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': ['pybufrkit = pybufrkit:main'],
-    },
-
-    author='Yang Wang',
-    author_email='ywangd@gmail.com',
-    description='Python toolkit to work with BUFR files',
-    long_description=__doc__,
-    license='MIT',
+setuptools.setup(
+    name="hypothesis-jsonschema",
+    version=__version__,
+    author="Zac Hatfield-Dodds",
+    author_email="zac@hypothesis.works",
+    packages=setuptools.find_packages(SOURCE),
+    package_dir={"": SOURCE},
+    package_data={"": ["py.typed"]},
+    url="https://github.com/Zac-HD/hypothesis-jsonschema",
+    project_urls={"Funding": "https://github.com/sponsors/Zac-HD"},
+    license="MPL 2.0",
+    description="Generate test data from JSON schemata with Hypothesis",
+    zip_safe=False,
+    install_requires=["hypothesis>=5.3.0", "jsonschema>=3.0.2"],
+    python_requires=">=3.6",
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
+        "Development Status :: 4 - Beta",
+        "Framework :: Hypothesis",
         "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Topic :: Utilities",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Education :: Testing",
+        "Topic :: Software Development :: Testing",
+        "Typing :: Typed",
     ],
-    keywords=['BUFR', 'WMO'],
-    url='https://github.com/ywangd/pybufrkit',
+    long_description=open(README).read(),
+    long_description_content_type="text/markdown",
+    keywords="python testing fuzzing property-based-testing json-schema",
 )
