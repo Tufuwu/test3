c4d2d2b2447fe84e18e869180890424be3e17529
wu <wu@.com>
2025-07-06 11:25:58 +0800

    vertica/verticapy

--- setup.py
+++ setup.py
@@ -1,51 +1,48 @@
+# (c) Copyright [2018-2020] Micro Focus or one of its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-import sys
+import collections
+from setuptools import setup, find_packages
 
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+ReqOpts = collections.namedtuple('ReqOpts', ['skip_requirements_regex', 'default_vcs'])
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
-
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read().replace(".. :changelog:", "")
-import djangorestframework_camel_case
-
+opts = ReqOpts(None, 'git')
 setup(
-    name="djangorestframework-camel-case",
-    version=djangorestframework_camel_case.__version__,
-    description="Camel case JSON support for Django REST framework.",
-    long_description=readme + "\n\n" + history,
-    long_description_content_type="text/x-rst",
-    author="Vitaly Babiy",
-    author_email="vbabiy86@gmail.com",
-    url="https://github.com/vbabiy/djangorestframework-camel-case",
-    packages=["djangorestframework_camel_case"],
-    package_dir={"djangorestframework_camel_case": "djangorestframework_camel_case"},
-    include_package_data=True,
-    python_requires=">=3.5",
-    install_requires=[],
-    license="BSD",
-    zip_safe=False,
-    keywords="djangorestframework_camel_case",
+    name='VerticaPy',
+    version='0.4.0',
+    description='A Python library that exposes sci-kit like functionality to conduct data science projects on data stored in Vertica.',
+    author='Badr Ouali',
+    author_email='badr.ouali@vertica.com',
+    url='https://github.com/vertica/VerticaPy',
+    keywords="machine-learning database vertica",
+    packages=find_packages(),
+    license="Apache License 2.0",
+    install_requires=[
+    ],
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
+        "Development Status :: 2 - Beta",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    test_suite="tests",
+        "Topic :: Data Science",
+        "Topic :: Machine Learning",
+        "Topic :: Database",
+        "Topic :: Database :: Database Engines/Servers",
+        "Operating System :: OS Independent"
+    ]
 )
