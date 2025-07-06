1ac68abb71049d882e2fa6fbffc8c01671054a9c
wu <wu@.com>
2025-07-06 09:46:20 +0800

    tw4l/brunnhilde

--- setup.py
+++ setup.py
@@ -1,52 +1,35 @@
-import os
-from importlib.machinery import SourceFileLoader
-from pathlib import Path
-
 from setuptools import setup
-from setuptools.command.install import install
-
-# avoid loading the package before requirements are installed:
-version = SourceFileLoader('version', 'pydf/version.py').load_module()
-
-description = 'PDF generation in python using wkhtmltopdf suitable for heroku'
-THIS_DIR = Path(__file__).resolve().parent
-readme_path = THIS_DIR / 'README.rst'
-if readme_path.exists():
-    long_description = readme_path.read_text()
-else:
-    long_description = description
-
-
-class OverrideInstall(install):
-    def run(self):
-        install.run(self)
-        for filepath in self.get_outputs():
-            if filepath.endswith('pydf/bin/wkhtmltopdf'):
-                os.chmod(filepath, 0o775)
-
 
 setup(
-    name='python-pdf',
-    cmdclass={'install': OverrideInstall},
-    version=version.VERSION,
-    description=description,
-    long_description=long_description,
-    author='Samuel Colvin',
-    license='MIT',
-    author_email='s@muelcolvin.com',
-    url='https://github.com/tutorcruncher/pydf',
-    packages=['pydf'],
-    platforms='any',
-    package_data={'pydf': ['bin/*']},
+    name="brunnhilde",
+    version="1.9.3",
+    url="https://github.com/tw4l/brunnhilde",
+    author="Tessa Walsh",
+    author_email="tessa@bitarchivist.net",
+    py_modules=["brunnhilde"],
+    scripts=["brunnhilde.py"],
+    description="A Siegfried-based digital archives reporting tool for directories and disk images",
+    keywords="archives reporting characterization identification diskimages",
+    platforms=["POSIX", "Windows"],
+    install_requires=["requests"],
+    test_suite="test",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Internet :: WWW/HTTP',
-        ],
-    zip_safe=False
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Archiving",
+        "Topic :: System :: Filesystems",
+        "Topic :: Utilities",
+    ],
 )
