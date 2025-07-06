e1bedd34a55f4033eb081b3487ac9ced0fdb7b7d
wu <wu@.com>
2025-07-06 17:04:15 +0800

    zhuyifei1999/guppy3

--- setup.py
+++ setup.py
@@ -1,111 +1,75 @@
-import codecs
+import os.path
+import re
 import sys
+from setuptools import setup, Extension
+from distutils.command.install import INSTALL_SCHEMES
 
-from setuptools import find_packages
-from setuptools import setup
-
-version = "7.0.0a1.dev0"
-
-
-def read(filename):
-    try:
-        with codecs.open(filename, encoding="utf-8") as f:
-            return f.read()
-    except NameError:
-        with open(filename, encoding="utf-8") as f:
-            return f.read()
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 
-long_description = "\n\n".join(
-    [read("README.rst"), read("CREDITS.rst"), read("CHANGES.rst")]
-)
+setsc = Extension("guppy.sets.setsc", [
+                      "src/sets/sets.c",
+                      "src/sets/bitset.c",
+                      "src/sets/nodeset.c"
+                  ])
 
-if sys.version_info < (3,):
-    long_description = long_description.encode("utf-8")
+heapyc = Extension("guppy.heapy.heapyc", [
+                       'src/heapy/heapyc.c',
+                       'src/heapy/stdtypes.c'
+                   ])
 
 
-setup(
-    name="zest.releaser",
-    version=version,
-    description="Software releasing made easy and repeatable",
-    long_description=long_description,
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    keywords=["releasing", "packaging", "pypi"],
-    author="Reinout van Rees",
-    author_email="reinout@vanrees.org",
-    url="https://zestreleaser.readthedocs.io",
-    license="GPL",
-    packages=find_packages(exclude=["ez_setup"]),
-    namespace_packages=["zest"],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=[
-        "setuptools",
-        "colorama",
-        "requests",
-        "twine >= 1.6.0",
-    ],
-    extras_require={
-        "recommended": [
-            "chardet",
-            "check-manifest",
-            "pyroma",
-            "readme_renderer",
-            "wheel",
-        ],
-        "test": [
-            "z3c.testsetup >= 0.8.4",
-            "zope.testing",
-            "zope.testrunner",
-            "wheel",
-        ],
-    },
-    entry_points={
-        "console_scripts": [
-            "release = zest.releaser.release:main",
-            "prerelease = zest.releaser.prerelease:main",
-            "postrelease = zest.releaser.postrelease:main",
-            "fullrelease = zest.releaser.fullrelease:main",
-            "longtest = zest.releaser.longtest:main",
-            "lasttagdiff = zest.releaser.lasttagdiff:main",
-            "lasttaglog = zest.releaser.lasttaglog:main",
-            "addchangelogentry = zest.releaser.addchangelogentry:main",
-            "bumpversion = zest.releaser.bumpversion:main",
-        ],
-        # The datachecks are implemented as entry points to be able to check
-        # our entry point implementation.
-        "zest.releaser.prereleaser.middle": [
-            "datacheck = zest.releaser.prerelease:datacheck",
-        ],
-        "zest.releaser.releaser.middle": [
-            "datacheck = zest.releaser.release:datacheck",
-        ],
-        "zest.releaser.postreleaser.middle": [
-            "datacheck = zest.releaser.postrelease:datacheck",
-        ],
-        "zest.releaser.addchangelogentry.middle": [
-            "datacheck = zest.releaser.addchangelogentry:datacheck",
-        ],
-        "zest.releaser.bumpversion.middle": [
-            "datacheck = zest.releaser.bumpversion:datacheck",
-        ],
-        # Documentation generation
-        "zest.releaser.prereleaser.before": [
-            "preparedocs = "
-            + "zest.releaser.preparedocs:prepare_entrypoint_documentation",
-        ],
-    },
-)
+def doit():
+    if sys.version_info.major < 3:
+        print('''\
+setup.py: Error: This guppy package only supports Python 3.
+You can find the original Python 2 version, Guppy-PE, here:
+http://guppy-pe.sourceforge.net/''', file=sys.stderr)
+        sys.exit(1)
+    if sys.implementation.name != 'cpython':
+        print('''\
+setup.py: Warning: This guppy package only supports CPython.
+Compilation failure expected, but continuting anyways...''', file=sys.stderr)
+
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+        long_description = f.read()
+
+    with open('guppy/_version.py', 'r') as versionfile:
+        version = re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]$',
+                            versionfile.read(), re.M)
+        version = version.group(1)
+
+    setup(name="guppy3",
+          version=version,
+          description="Guppy 3 -- Guppy-PE ported to Python 3",
+          long_description=long_description,
+          long_description_content_type='text/markdown',
+          author="YiFei Zhu",
+          author_email="zhuyifei1999@gmail.com",
+          url="https://github.com/zhuyifei1999/guppy3/",
+          license='MIT',
+          packages=[
+              "guppy",
+              "guppy.etc",
+              "guppy.gsl",
+              "guppy.heapy",
+              "guppy.heapy.test",
+              "guppy.sets",
+          ],
+          ext_modules=[setsc, heapyc],
+          python_requires='>=3.6',
+          classifiers=[
+              "Programming Language :: Python :: 3",
+              "Programming Language :: Python :: Implementation :: CPython",
+              "Programming Language :: C",
+              "License :: OSI Approved :: MIT License",
+              "Operating System :: OS Independent",
+              "Development Status :: 4 - Beta",
+              "Topic :: Software Development :: Debuggers",
+              "Environment :: Console",
+              "Intended Audience :: Developers",
+          ])
+
+
+doit()
