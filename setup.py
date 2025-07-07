24070fa5bb78a6de1f3651cd7f6534e0123f858c
wu <wu@.com>
2025-07-07 10:31:48 +0800

    polyconseil/django-cid

--- setup.py
+++ setup.py
@@ -1,28 +1,49 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+def clean_history(history):
+    # PyPI does not allow the `raw` directive. We'll laboriously
+    # replace it. Hang tight, it's going to be ugly.
+    history = history.replace('|backward-incompatible|', '**backward incompatible:** ')
+    lines = []
+    for line in history.split('\n'):
+        if line.startswith('.. role:: raw-html'):
+            break
+        lines.append(line)
+    return '\n'.join(lines)
+
 
 readme = open('README.rst').read()
+changelog = clean_history(open('HISTORY.rst').read())
 
-setup(name='result',
-      version='0.5.0',
-      description='A rust-like result type for Python',
-      author='Danilo Bargen',
-      author_email='mail@dbrgn.ch',
-      url='https://github.com/dbrgn/result',
-      packages=['result'],
-      package_data={'result': ['py.typed']},
-      zip_safe=True,
-      include_package_data=True,
-      license='MIT',
-      keywords='rust result enum',
-      long_description=readme,
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3 :: Only',
-      ],
-      install_requires=['typing; python_version < "3.5"'])
+setup(
+    name='django-cid',
+    version='2.2.dev0',
+    description="""Correlation IDs in Django for debugging requests""",
+    long_description=readme + '\n\n' + changelog,
+    author='Snowball One',
+    author_email='opensource+django-cid@polyconseil.fr',
+    maintainer="Polyconseil",
+    maintainer_email="opensource+django-cid@polyconseil.fr",
+    url='https://github.com/Polyconseil/django-cid',
+    packages=find_packages(exclude=('sandbox*', 'tests*')),
+    include_package_data=True,
+    install_requires=[
+        'django>=2.2',
+    ],
+    license="BSD",
+    zip_safe=False,
+    keywords='django logging correlation id debugging',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
