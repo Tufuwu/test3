ecd0216e4954c04fde2721e7a4c5df0114b87f01
wu <wu@.com>
2025-07-14 10:42:47 +0800

    coleifer/scout

--- setup.py
+++ setup.py
@@ -1,38 +1,43 @@
 import os
+import warnings
+
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
-    README = readme.read()
+cur_dir = os.path.dirname(__file__)
+readme_file = os.path.join(cur_dir, 'README.md')
+with open(readme_file) as fh:
+    readme = fh.read()
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+try:
+    from scout import __version__ as scout_version
+except ImportError:
+    scout_version = '0.0.0'
+    warnings.warn('Unable to determine scout library version!')
 
 setup(
-    name='django-db-logger',
-    version='0.1.11',
-    packages=['django_db_logger', 'django_db_logger.migrations'],
-    include_package_data=True,
-    license='MIT License',
-    description='Django logging in database',
-    long_description=README,
-    url='https://github.com/CiCiUi/django-db-logger',
-    author='zhangshine',
-    author_email='zhangshine0125@gmail.com',
-    install_requires=['django>=1.9', 'six'],
+    name='scout',
+    version=scout_version,
+    url='http://github.com/coleifer/scout/',
+    license='MIT',
+    author='Charles Leifer',
+    author_email='coleifer@gmail.com',
+    description='scout - a lightweight search server powered by SQLite',
+    packages=['scout'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'flask',
+        'peewee>=3.0.0'],
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    ],
+        'Programming Language :: Python'],
+    py_modules=['scout_client'],
+    test_suite='scout.tests',
+    entry_points="""
+        [console_scripts]
+        scout=scout.server:main
+    """,
 )
