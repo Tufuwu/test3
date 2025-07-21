0a6c94dc546934f6bb02aee1f442f33e294b0dab
wu <wu@.com>
2025-07-21 12:25:33 +0800

    kipe/pycron

--- setup.py
+++ setup.py
@@ -1,63 +1,27 @@
-import os
-
+#!/usr/bin/env python
 from setuptools import setup
-from e3fp import version
-
-ON_RTD = os.environ.get("READTHEDOCS") == "True"
-
-
-requirements = [
-    "scipy>=0.18.0",
-    "numpy>=1.11.3",
-    "mmh3>=2.3.1",
-    "sdaxen_python_utilities>=0.1.4",
-]
-if ON_RTD:  # ReadTheDocs can't handle C libraries
-    requirements = requirements[-1:] + ["mock"]
-
-test_requirements = ["pytest", "mock"]
-
-classifiers = [
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2.7",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-    "Programming Language :: Python :: 3.8",
-    "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-    "Operating System :: OS Independent",
-    "Development Status :: 4 - Beta",
-    "Intended Audience :: Science/Research",
-    "Intended Audience :: Developers",
-    "Topic :: Scientific/Engineering :: Chemistry",
-    "Topic :: Software Development :: Libraries :: Python Modules",
-]
-
-
-def get_readme():
-    with open("README.rst") as f:
-        return f.read()
-
 
 setup(
-    name="e3fp",
+    name='pycron',
+    version='3.0.0',
+    description='Simple cron-like parser, which determines if current datetime matches conditions.',
+    author='Kimmo Huoman',
+    author_email='kipenroskaposti@gmail.com',
+    license='MIT',
+    keywords='cron parser',
+    url='https://github.com/kipe/pycron',
     packages=[
-        "e3fp",
-        "e3fp.config",
-        "e3fp.conformer",
-        "e3fp.fingerprint",
-        "e3fp.test",
+        'pycron',
     ],
-    version=version,
-    description="Molecular 3D fingerprinting",
-    long_description=get_readme(),
-    keywords="e3fp 3d molecule fingerprint conformer",
-    author="Seth Axen",
-    author_email="seth.axen@gmail.com",
-    license="LGPLv3",
-    url="https://github.com/keiserlab/e3fp",
-    classifiers=classifiers,
-    download_url="https://github.com/keiserlab/e3fp/tarball/" + version,
-    install_requires=requirements,
-    include_package_data=True,
-    tests_require=test_requirements,
+    python_requires='>=3.5',
+    tests_require=[
+        "arrow>=0.12.0",
+        "coverage>=4.4.2",
+        "coveralls>=1.2.0",
+        "Delorean>=0.6.0",
+        "nose>=1.0",
+        "pendulum>=1.3.2",
+        "pytz>=2017.3",
+        "udatetime>=0.0.14"
+    ]
 )
