5b5a6d416642ddbc208d7d6b9d526437f73ae3a5
wu <wu@.com>
2025-07-27 19:55:51 +0800

    vifactor/repostat

--- setup.py
+++ setup.py
@@ -1,51 +1,48 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from setuptools import setup
+from tools.configuration import Configuration
 
-import os
-import sys
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
+with open('requirements.txt', "r") as req_file:
+    requirements = req_file.read().splitlines()
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist upload")
-    sys.exit()
-
-readme = open("README.rst").read()
-history = open("HISTORY.rst").read().replace(".. :changelog:", "")
-import djangorestframework_camel_case
-
-setup(
-    name="djangorestframework-camel-case",
-    version=djangorestframework_camel_case.__version__,
-    description="Camel case JSON support for Django REST framework.",
-    long_description=readme + "\n\n" + history,
-    long_description_content_type="text/x-rst",
-    author="Vitaly Babiy",
-    author_email="vbabiy86@gmail.com",
-    url="https://github.com/vbabiy/djangorestframework-camel-case",
-    packages=["djangorestframework_camel_case"],
-    package_dir={"djangorestframework_camel_case": "djangorestframework_camel_case"},
-    include_package_data=True,
-    python_requires=">=3.5",
-    install_requires=[],
-    license="BSD",
-    zip_safe=False,
-    keywords="djangorestframework_camel_case",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    test_suite="tests",
-)
+# such name for package is used here because 'repostat' is already occupied by https://pypi.org/project/repostat/
+setup(name='repostat-app',
+      version=Configuration.get_release_data_info()['develop_version'],
+      description='Desktop git repository analyser and report creator.',
+      keywords='git analysis statistics vcs python visualization',
+      url='https://github.com/vifactor/repostat',
+      author='Viktor Kopp',
+      author_email='vifactor@gmail.com',
+      license='GPLv3',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      classifiers=[
+          "Development Status :: 5 - Production/Stable",
+          "Environment :: Console",
+          "Intended Audience :: Developers",
+          "Intended Audience :: Science/Research",
+          "Intended Audience :: Education",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+          "Topic :: Software Development :: Version Control",
+          "Topic :: Utilities",
+          "Operating System :: OS Independent",
+          "Operating System :: POSIX",
+          "Operating System :: MacOS :: MacOS X",
+          "Operating System :: Microsoft :: Windows",
+      ],
+      python_requires='>3.5',
+      packages=['analysis', 'tools', 'report'],
+      package_data={'report': ['templates/*.html',
+                               'templates/*.js',
+                               'assets/images/*.gif',
+                               'assets/*.js',
+                               'assets/*.css'],
+                    'tools': ['release_data.json']},
+      install_requires=requirements,
+      entry_points={"console_scripts": ["repostat = analysis.repostat:main"]},
+      include_package_data=True,
+      zip_safe=False)
