f383a6ea113dcbb09f412bb5e4a43a47bbf6fc8c
wu <wu@.com>
2025-07-29 23:05:27 +0800

    wimglenn/pytest-structlog

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
-# ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+from setuptools import setup
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-d = generate_distutils_setup(
-    packages=['rospy_message_converter'],
-    package_dir={'': 'src'},
+setup(
+    name="pytest-structlog",
+    version="0.5",
+    url="https://github.com/wimglenn/pytest-structlog",
+    description="Structured logging assertions",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    author="Wim Glenn",
+    author_email="hey@wimglenn.com",
+    license="MIT",
+    install_requires=["pytest", "structlog"],
+    py_modules=["pytest_structlog"],
+    entry_points={"pytest11": ["pytest-structlog=pytest_structlog"]},
+    classifiers=[
+        "Framework :: Pytest",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    options={"bdist_wheel": {"universal": "1"}},
 )
-
-setup(**d)
