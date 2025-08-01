76a523dc29f173e46f8a7ad83f7e3e701897f24c
wu <wu@.com>
2025-08-01 17:05:41 +0800

    camptocamp/marabunta

--- setup.py
+++ setup.py
@@ -1,48 +1,72 @@
-from setuptools import find_packages, setup
+# -*- coding: utf-8 -*-
+import sys
 
+from setuptools import setup, find_packages
+
+if (sys.version_info[:3] < (3, 0)):
+    with open('README.rst') as f:
+        readme = f.read()
+else:
+    with open('README.rst', encoding='utf-8') as f:
+        readme = f.read()
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+test_deps = [
+    "pytest",
+    "mock",
+]
+
+extras = {
+    'test': test_deps,
+}
 
 setup(
-    name="bundlewrap",
-    version="4.3.0",
-    description="Config management with Python",
-    long_description=(
-        "By allowing for easy and low-overhead config management, BundleWrap fills the gap between complex deployments using Chef or Puppet and old school system administration over SSH.\n"
-        "While most other config management systems rely on a client-server architecture, BundleWrap works off a repository cloned to your local machine. It then automates the process of SSHing into your servers and making sure everything is configured the way it's supposed to be. You won't have to install anything on managed servers."
-    ),
-    author="Torsten Rehn",
-    author_email="torsten@rehn.email",
-    license="GPLv3",
-    url="http://bundlewrap.org",
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            "bw=bundlewrap.cmdline:main",
-        ],
-    },
-    keywords=["configuration", "config", "management"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: System :: Installation/Setup",
-        "Topic :: System :: Systems Administration",
-    ],
+    name='marabunta',
+    use_scm_version=True,
+    description='Migration tool for Odoo',
+    long_description=readme + '\n\n' + history,
+    author='Camptocamp (Guewen Baconnier)',
+    author_email='guewen.baconnier@camptocamp.com',
+    url='https://github.com/camptocamp/marabunta',
+    license='AGPLv3+',
+    packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        "cryptography",
-        "Jinja2",
-        "Mako",
-        "passlib",
-        "pyyaml",
-        "requests >= 1.0.0",
-        "tomlkit",
+        "psycopg2",
+        "ruamel.yaml>=0.15.1",
+        "pexpect",
+        "werkzeug",
+        "future",
     ],
-    zip_safe=False,
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    tests_require=test_deps,
+    extras_require=extras,
+    include_package_data=True,
+    package_data={
+        'marabunta': ['html/*.html'],
+    },
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: '
+        'GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ),
+    entry_points={
+        'console_scripts': ['marabunta = marabunta.core:main']
+    },
 )
