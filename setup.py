afe2d9ff2bb4f2997e0716bd6d4253a81337b216
wu <wu@.com>
2025-08-02 22:34:29 +0800

    django-guardian/django-guardian

--- setup.py
+++ setup.py
@@ -1,56 +1,55 @@
-#!/usr/bin/env python
-from pathlib import Path
-from setuptools import find_packages, setup
+import os
+from setuptools import setup
+from extras import RunFlakesCommand
 
 
-REQUIREMENTS = [
-    'django>=2.2',
-    'django-classy-tags>=1',
-]
+version = '2.3.0'
 
-
-CLASSIFIERS = [
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Web Environment',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Framework :: Django',
-    'Framework :: Django :: 2.2',
-    'Framework :: Django :: 3.1',
-    'Framework :: Django :: 3.2',
-    'Topic :: Internet :: WWW/HTTP',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    'Topic :: Software Development',
-    'Topic :: Software Development :: Libraries',
-]
-
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+readme_file = os.path.join(os.path.dirname(__file__), 'README.rst')
+with open(readme_file) as f:
+    long_description = f.read()
 
 setup(
-    name='django-sekizai',
-    version='2.0.0',
-    author='Jonas Obrist',
-    author_email='ojiidotch@gmail.com',
-    maintainer='Django CMS Association and contributors',
-    maintainer_email='info@django-cms.org',
-    url='https://github.com/django-cms/django-sekizai',
-    license='BSD-3-Clause',
-    description='Django Sekizai',
+    name='django-guardian',
+    version=version,
+    python_requires='>=3.5',
+    url='http://github.com/django-guardian/django-guardian',
+    author='Lukasz Balcerzak',
+    author_email='lukaszbalcerzak@gmail.com',
+    download_url='https://github.com/django-guardian/django-guardian/tags',
+    description="Implementation of per object permissions for Django.",
     long_description=long_description,
-    long_description_content_type='text/x-rst',
-    packages=find_packages(),
-    python_requires='>=3.7',
-    include_package_data=True,
     zip_safe=False,
-    install_requires=REQUIREMENTS,
-    classifiers=CLASSIFIERS,
-    test_suite='tests.settings.run',
+    packages=[
+        'guardian', 'guardian.conf', 'guardian.management',
+        'guardian.migrations', 'guardian.templatetags', 'guardian.testapp',
+        'guardian.management.commands', 'guardian.testapp.migrations',
+        'guardian.testapp.tests'
+    ],
+    include_package_data=True,
+    license='BSD',
+    install_requires=["Django>=2.2"],
+    tests_require=['mock', 'django-environ', 'pytest', 'pytest-django'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Environment :: Web Environment',
+                 'Framework :: Django',
+                 'Framework :: Django :: 2.2',
+                 'Framework :: Django :: 3.0',
+                 'Framework :: Django :: 3.1',
+                 'Framework :: Django :: 3.2',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Topic :: Security',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 ],
+    test_suite='tests.main',
+    cmdclass={'flakes': RunFlakesCommand},
 )
