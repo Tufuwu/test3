21578364875322ecfe45fb498d503ecfbe70b5e0
wu <wu@.com>
2025-08-03 15:21:15 +0800

    packages=["stregsystem", "stregreport", "media", "kiosk", "treo"],

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ setup(
     name='Stregsystemet',
     version='0.1',
     author='FIT',
+    packages=["stregsystem", "stregreport", "media", "kiosk", "treo"],
     license='THE LIMFJORDS-PORTER-WARE LICENSE',
 )
