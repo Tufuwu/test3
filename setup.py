9844ad7a1fa8aa93422be4a3ef297a4810904d25
wu <wu@.com>
2025-08-03 15:24:14 +0800

    from setuptools import setup, find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
-
+from setuptools import setup, find_packages
 setup(
     name='Stregsystemet',
     version='0.1',
     author='FIT',
-    packages=["stregsystem", "stregreport", "media", "kiosk", "treo"],
+    packages=find_packages(include=["stregsystem*", "kiosk*", "stregreport*", "treo*"]),
     license='THE LIMFJORDS-PORTER-WARE LICENSE',
 )
