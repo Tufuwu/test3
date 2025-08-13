aab326f1a2961dd2c68ab453888e4d0acd3de5f4
wu <wu@.com>
2025-08-13 19:05:09 +0800

    tommyod/efficient-apriori

--- setup.py
+++ setup.py
@@ -1,45 +1,100 @@
-import sys
-import versioneer
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
 from os import path
+from efficient_apriori import __version__
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
+VERSION = __version__
 
-# Only install pytest and runner when test command is run
-# This makes work easier for offline installs or low bandwidth machines
-needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
-pytest_runner = ["pytest-runner"] if needs_pytest else []
-test_requirements = ["mock", "pycodestyle", "pytest", "requests-mock>=1.0,<2.0"]
+here = path.abspath(path.dirname(__file__))
+
+
+def read(fname):
+    return open(path.join(here, fname)).read()
+
 
 setup(
-    name="tableauserverclient",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author="Tableau",
-    author_email="github@tableau.com",
-    url="https://github.com/tableau/server-client-python",
-    packages=[
-        "tableauserverclient",
-        "tableauserverclient.models",
-        "tableauserverclient.server",
-        "tableauserverclient.server.endpoint",
-    ],
-    license="MIT",
-    description="A Python module for working with the Tableau Server REST API.",
-    long_description=long_description,
+    name="efficient_apriori",
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version=VERSION,
+    description=" An efficient Python implementation of the Apriori \
+    algorithm.",
+    long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    test_suite="test",
-    setup_requires=pytest_runner,
-    install_requires=[
-        "requests>=2.11,<3.0",
+    # The project's main homepage.
+    url="https://github.com/tommyod/Efficient-Apriori",
+    # Author details
+    author="tommyod",
+    author_email="tommy.odland@gmail.com",
+    # Choose your license
+    license="MIT",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 5 - Production/Stable",
+        # Indicate who your project is intended for
+        # 'Intended Audience :: End Users/Desktop',
+        # 'Intended Audience :: Healthcare Industry',
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    tests_require=test_requirements,
-    extras_require={"test": test_requirements},
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(".", exclude=["contrib", "docs", "tests"]),
+    package_dir={"efficient_apriori": "efficient_apriori"},
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    # extras_require={
+    #     'dev': ['check-manifest'],
+    #     'test': ['coverage'],
+    # },
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
+    # include_package_data=True,
+    package_data={"": ["templates/*", "*.tex", "*.html"]},
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    # data_files=[('my_data', ['data/data_file'])],
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    # entry_points={
+    #    'console_scripts': [
+    #         'sample=sample:main',
+    #    ],
+    # }
 )
