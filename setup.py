836eae9ff410604a199450e7f5c88802e5fc0c30
wu <wu@.com>
2025-08-13 15:07:36 +0800

    elmotec/circlify

--- setup.py
+++ setup.py
@@ -1,72 +1,45 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# License: LGPLv3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
+# encoding: utf-8
 
-import ast
-import re
-import sys
+"""Packaging script."""
+
 import os
 
-from setuptools import find_packages, setup
-from setuptools.command.test import test
+from setuptools import setup
 
-# extract the version without importing the module
-VERSION = open('src/css_parser/version.py', 'rb').read().decode('utf-8')
-VERSION = '.'.join(map(str, ast.literal_eval(re.search(r'^version\s+=\s+(.+)', VERSION, flags=re.M).group(1))))
-long_description = '\n' + open('README.md', 'rb').read().decode('utf-8') + '\n'  # + read('CHANGELOG.txt')
-
-
-class Test(test):
-
-    user_options = [
-        ('which-test=', 'w', "Specify which test to run as either"
-            " the test method name (without the leading test_)"
-            " or a module name with a trailing period"),
-    ]
-
-    def initialize_options(self):
-        self.which_test = None
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        import importlib
-        orig = sys.path[:]
-        try:
-            sys.path.insert(0, os.getcwd())
-            m = importlib.import_module('run_tests')
-            which_test = (self.which_test,) if self.which_test else ()
-            m.run_tests(which_test)
-        finally:
-            sys.path = orig
-
+here = os.path.abspath(os.path.dirname(__file__))
+readme = open(os.path.join(here, "README.rst")).read()
 
 setup(
-    name='css-parser',
-    version=VERSION,
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    description='A CSS Cascading Style Sheets library for Python',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    cmdclass={'test': Test},
-    author='Various People',
-    author_email='redacted@anonymous.net',
-    url='https://github.com/ebook-utils/css-parser',
-    license='LGPL 3.0 or later',
-    keywords='CSS, Cascading Style Sheets, CSSParser, DOM Level 2 Stylesheets, DOM Level 2 CSS',
+    name="circlify",
+    description="Circle packing algorithm for Python",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    version="0.14.0",
+    author="Elmotec",
+    author_email="elmotec@gmx.com",
+    license="MIT",
+    keywords="circle packing enclosure hierarchy graph display visualization",
+    url="http://github.com/elmotec/circlify",
+    py_modules=["circlify"],
+    test_suite="tests",
+    setup_requires=[],
+    tests_require=[],
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Text Processing :: Markup :: HTML'
-    ]
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+        "Topic :: Utilities",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Intended Audience :: Developers",
+    ],
 )
