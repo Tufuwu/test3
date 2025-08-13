bcb22bf982a0af21c2a855fdef30ffdbceebca05
wu <wu@.com>
2025-08-13 17:17:26 +0800

    modscripps/mixsea

--- Makefile
+++ Makefile
@@ -1,42 +1,92 @@
-export W := $(shell pwd)
+.PHONY: clean clean-test clean-pyc clean-build docs help
+.DEFAULT_GOAL := help
 
-# create a full source package
-sdist: build
+define BROWSER_PYSCRIPT
+import os, webbrowser, sys
+
+from urllib.request import pathname2url
+
+webbrowser.open("file://" + pathname2url(os.path.abspath(sys.argv[1])))
+endef
+export BROWSER_PYSCRIPT
+
+define PRINT_HELP_PYSCRIPT
+import re, sys
+
+for line in sys.stdin:
+	match = re.match(r'^([a-zA-Z_-]+):.*?## (.*)$$', line)
+	if match:
+		target, help = match.groups()
+		print("%-20s %s" % (target, help))
+endef
+export PRINT_HELP_PYSCRIPT
+
+BROWSER := python -c "$$BROWSER_PYSCRIPT"
+
+help:
+	@python -c "$$PRINT_HELP_PYSCRIPT" < $(MAKEFILE_LIST)
+
+clean: clean-build clean-pyc clean-test ## remove all build, test, coverage and Python artifacts
+
+clean-build: ## remove build artifacts
+	rm -fr build/
+	rm -fr dist/
+	rm -fr .eggs/
+	find . -name '*.egg-info' -exec rm -fr {} +
+	find . -name '*.egg' -exec rm -f {} +
+
+clean-pyc: ## remove Python file artifacts
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+
+clean-test: ## remove test and coverage artifacts
+	rm -fr .tox/
+	rm -f .coverage
+	rm -fr htmlcov/
+	rm -fr .pytest_cache
+
+style: ## style code using isort & black, then check style using flake8
+	isort mixsea/*.py
+	isort mixsea/tests/*.py
+	black mixsea
+	flake8 mixsea 
+
+style-check: ## check code style using isort & black, then check style using flake8
+	isort -c mixsea/*.py
+	isort -c mixsea/tests/*.py
+	black --check mixsea
+	flake8 mixsea 
+
+test: ## run tests quickly with the default Python
+	pytest
+
+test-all: ## run tests on every Python version with tox
+	tox
+
+coverage: ## check code coverage quickly with the default Python
+	coverage run --source mixsea -m pytest
+	coverage report -m
+	coverage html
+	$(BROWSER) htmlcov/index.html
+
+docs: ## generate Sphinx HTML documentation, including API docs
+	rm -rf docs/generated/
+	$(MAKE) -C docs clean
+	$(MAKE) -C docs html
+	$(BROWSER) docs/_build/html/index.html
+
+servedocs: docs ## compile the docs watching for changes
+	watchmedo shell-command -p '*.rst' -c '$(MAKE) -C docs html' -R -D .
+
+release: dist ## package and upload a release
+	twine upload dist/*
+
+dist: clean ## builds source and wheel package
 	python setup.py sdist
-	-# mv dist/pyang-*.tar.gz dist/pyang_src-*.tar.gz
+	python setup.py bdist_wheel
+	ls -l dist
 
-# create a minimal package
-dist: build
-	python setup.py sdist
-
-.PHONY:	test tags clean doc build lint pylint
-build: doc pyang/xpath_parsetab.py
-
-doc:
-	(cd doc; $(MAKE))
-
-pyang/xpath_parsetab.py: pyang/xpath_parser.py
-	python -m pyang.xpath_parser
-
-test: lint
-	(cd test; $(MAKE) test)
-
-lint:
-	flake8 .
-
-pylint:
-	touch bin/__init__.py  # makes pylint tell pyang script apart from pyang package
-	pylint bin/pyang bin/json2xml bin/yang2html pyang $(shell find test -name '*.py') || true
-	rm -f bin/__init__.py
-
-clean:
-	rm -f pyang/parser.out pyang/xpath_parsetab.py
-	(cd test && $(MAKE) clean)
-	(cd doc &&  $(MAKE) clean)
-	python setup.py clean --all
-	rm -rf build dist MANIFEST
-	find . -name "*.pyc" -delete
-	find . -name "__pycache__" -delete
-
-tags:
-	find . -name "*.py" | etags -
+install: clean ## install the package to the active Python's site-packages
+	python setup.py install
--- setup.py
+++ setup.py
@@ -1,104 +1,47 @@
-from setuptools import setup
-from setuptools import Distribution
-from os.path import join
-import pyang
-import glob
-import os
-import re
-import sys
-import tempfile
+from setuptools import find_packages, setup
 
-modules_iana = glob.glob(os.path.join("modules", "iana", "*.yang"))
-modules_ietf = glob.glob(os.path.join("modules", "ietf", "*.yang"))
-xslt = glob.glob(os.path.join("xslt", "*.xsl"))
-schema = glob.glob(os.path.join("schema", "*.rng"))
-images = glob.glob(os.path.join("tools", "images", "*"))
-man1 = glob.glob(os.path.join("man", "man1", "*.1"))
+with open("README.rst") as readme_file:
+    readme = readme_file.read()
 
-
-class PyangDist(Distribution):
-    """The purpose of this subclass of Distribution is to extend the
-    install procedure with preprocessing of shell scripts and man
-    pages so that they reflect the actual installation prefix, which
-    may be changed through the --prefix option.
-    """
-
-    def preprocess_files(self, prefix):
-        """Change the installation prefix where necessary."""
-        if prefix is None:
-            return
-        files = ("bin/yang2dsdl", "man/man1/yang2dsdl.1", "pyang/plugins/jsonxsl.py")
-        regex = re.compile("^(.*)/usr/local(.*)$")
-        for f in files:
-            inf = open(f)
-            cnt = inf.readlines()
-            inf.close()
-            ouf = open(f, "w")
-            for line in cnt:
-                mo = regex.search(line)
-                if mo is None:
-                    ouf.write(line)
-                else:
-                    ouf.write(mo.group(1) + prefix + mo.group(2) + "\n")
-            ouf.close()
-
-    def run_commands(self):
-        opts = self.command_options
-        if "install" in opts:
-            self.preprocess_files(opts["install"].get("prefix", ("", None))[1])
-        Distribution.run_commands(self)
-
-
-# If the installation is on windows, place pyang.bat file in Scripts directory
-script_files = []
-if os.name == "nt":
-    pyang_bat_file = "{}/{}.bat".format(tempfile.gettempdir(), "pyang")
-    with open(pyang_bat_file, "w") as script:
-        script.write("@echo off\npython %~dp0pyang %*\n")
-    script_files = [
-        "bin/pyang",
-        "bin/yang2html",
-        "bin/yang2dsdl",
-        "bin/json2xml",
-        pyang_bat_file,
-    ]
-else:
-    script_files = ["bin/pyang", "bin/yang2html", "bin/yang2dsdl", "bin/json2xml"]
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
 
 setup(
-    name="pyang",
-    version=pyang.__version__,
-    author="Martin Bjorklund",
-    author_email="mbj@tail-f.com",
-    description="A YANG (RFC 6020/7950) validator and converter",
-    long_description="An extensible  YANG (RFC 6020/7950) validator."
-    + " Provides a framework for plugins that can convert YANG modules"
-    + "to other formats.",
-    url="https://github.com/mbj4668/pyang",
-    install_requires=["lxml"],
-    license="BSD",
+    # Project information
+    name="mixsea",
+    version="0.1.0",
+    author="mixsea Developers",
+    author_email="",
+    url="https://github.com/modscripps/mixsea",
+    license="MIT License",
+    # Description
+    description="Ocean mixing parameterizations",
+    long_description=f"{readme}\n\n{history}",
+    long_description_content_type="text/x-rst",
+    # Requirements
+    python_requires=">=3.6",
+    install_requires=["numpy", "gsw", "scipy"],
+    extras_require={
+        "test": ["pytest"],  # install these with: pip install mixsea[test]
+    },
+    # Packaging
+    packages=find_packages(include=["mixsea", "mixsea.*"], exclude=["*.tests"]),
+    package_data={"mixsea": ["tests/data/*.csv"]},
+    include_package_data=True,
+    zip_safe=False,
+    platforms=["any"],  # or more specific, e.g. "win32", "cygwin", "osx"
+    # Metadata
+    project_urls={"Documentation": "https://mixsea.readthedocs.io"},
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Physics",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3",
-    ],
-    keywords="YANG validator",
-    distclass=PyangDist,
-    scripts=script_files,
-    packages=["pyang", "pyang.plugins", "pyang.translators", "pyang.transforms"],
-    data_files=[
-        ("share/man/man1", man1),
-        ("share/yang/modules/iana", modules_iana),
-        ("share/yang/modules/ietf", modules_ietf),
-        ("share/yang/xslt", xslt),
-        ("share/yang/images", images),
-        ("share/yang/schema", schema),
-        ("etc/bash_completion.d", ["etc/bash_completion.d/pyang"]),
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
-
-# Remove Bat file
-if os.name == "nt":
-    os.remove(pyang_bat_file)
