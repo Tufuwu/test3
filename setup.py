6b6a0cab535636f208c834f16cfc834d18ba4d15
wu <wu@.com>
2025-08-13 16:24:17 +0800

    lkorigin/debspawn

--- setup.py
+++ setup.py
@@ -1,48 +1,112 @@
 #!/usr/bin/env python3
-from io import open
-from os.path import abspath, dirname, join
 
+import os
+import sys
+import platform
+import shutil
+
+from debspawn import __appname__, __version__
 from setuptools import setup
+from setuptools.command.install_scripts import install_scripts as install_scripts_orig
+from subprocess import check_call
+from docs.assemble_man import generate_docbook_pages
 
-PROJECT_ROOT = abspath(dirname(__file__))
-with open(join(PROJECT_ROOT, 'README.rst'), encoding='utf-8') as f:
-    readme = f.read()
 
-version = (
-    [ln for ln in open(join(PROJECT_ROOT, 'zeroconf', '__init__.py')) if '__version__' in ln][0]
-    .split('=')[-1]
-    .strip()
-    .strip('\'"')
-)
+class install_scripts(install_scripts_orig):
+
+    def _create_manpage(self, xml_src, out_dir):
+        man_name = os.path.splitext(os.path.basename(xml_src))[0]
+        out_fname = os.path.join(out_dir, man_name)
+
+        print('Generating manual page {}'.format(man_name))
+        check_call(['xsltproc',
+                    '--nonet',
+                    '--stringparam', 'man.output.quietly', '1',
+                    '--stringparam', 'funcsynopsis.style', 'ansi',
+                    '--stringparam', 'man.th.extra1.suppress', '1',
+                    '-o', out_fname,
+                    'http://docbook.sourceforge.net/release/xsl/current/manpages/docbook.xsl',
+                    xml_src])
+        return out_fname
+
+    def run(self):
+        if platform.system() == 'Windows':
+            super().run()
+            return
+
+        if not self.skip_build:
+            self.run_command('build_scripts')
+        self.outfiles = []
+
+        # check for xsltproc, we need it to build manual pages
+        if not shutil.which('xsltproc'):
+            print('The "xsltproc" binary was not found. Please install it to continue!')
+            sys.exit(1)
+
+        if self.dry_run:
+            return
+
+        if '--single-version-externally-managed' not in sys.argv:
+            print()
+            print('Attempting to install Debspawn as binary distribution may not yield a working installation.', file=sys.stderr)
+            print('We require a file to be installed in a system location, and manual pages are in an external location as well.', file=sys.stderr)
+            print(('Currently, no workarounds for this issue have been implemented in Debspawn itself, so please run setup.py with '
+                   '`--single-version-externally-managed`.'), file=sys.stderr)
+            print('If you are using pip, try `sudo pip3 install --no-binary debspawn .`', file=sys.stderr)
+            sys.exit(1)
+
+        self.mkpath(self.install_dir)
+
+        # We want the files to be installed without a suffix on Unix
+        for infile in self.get_inputs():
+            infile = os.path.basename(infile)
+            in_built = os.path.join(self.build_dir, infile)
+            in_stripped = infile[:-3] if infile.endswith('.py') else infile
+            outfile = os.path.join(self.install_dir, in_stripped)
+            # NOTE: Mode is preserved by default
+            self.copy_file(in_built, outfile)
+            self.outfiles.append(outfile)
+
+        # handle generation of manual pages
+        man_dir = os.path.normpath(os.path.join(self.install_dir, '..', 'share', 'man', 'man1'))
+        self.mkpath(man_dir)
+        pages = generate_docbook_pages(self.build_dir)
+        for page in pages:
+            self.outfiles.append(self._create_manpage(page, man_dir))
+
+
+cmdclass = {
+    'install_scripts': install_scripts,
+}
+
+packages = [
+    'debspawn',
+    'debspawn.utils',
+]
+
+package_data = {'': ['debspawn/dsrun']}
+
+scripts = ['debspawn.py']
+
+install_requires = ['toml']
 
 setup(
-    name='zeroconf',
-    version=version,
-    description='Pure Python Multicast DNS Service Discovery Library ' '(Bonjour/Avahi compatible)',
-    long_description=readme,
-    author='Paul Scott-Murphy, William McBrine, Jakub Stasiak',
-    url='https://github.com/jstasiak/python-zeroconf',
-    package_data={"zeroconf": ["py.typed"]},
-    packages=["zeroconf"],
-    platforms=['unix', 'linux', 'osx'],
-    license='LGPL',
+    name=__appname__,
+    version=__version__,
+    author="Matthias Klumpp",
+    author_email="matthias@tenstral.net",
+    description='Debian package builder and build helper using systemd-nspawn',
+    license="LGPL-3.0+",
+    url="https://lkorigin.github.io/",
+
+    python_requires='>=3.5',
+    platforms=['any'],
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
-        'Operating System :: POSIX',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS :: MacOS X',
-        'Topic :: Software Development :: Libraries',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ],
-    keywords=['Bonjour', 'Avahi', 'Zeroconf', 'Multicast DNS', 'Service Discovery', 'mDNS'],
-    install_requires=['ifaddr>=0.1.7'],
+    include_package_data=True,
+
+    packages=packages,
+    cmdclass=cmdclass,
+    package_data=package_data,
+    scripts=scripts,
+    install_requires=install_requires
 )
