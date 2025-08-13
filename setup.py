3b0a254fffcfe44c79b181f4216f5d7c55b98fe7
wu <wu@.com>
2025-08-13 19:33:20 +0800

    isort setup.py

--- setup.py
+++ setup.py
@@ -15,12 +15,14 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-from setuptools import distutils
 import setuptools.command.build
+from setuptools import distutils
+
 try:
     import setuptools.modified as dep_util
 except ModuleNotFoundError:
     from distutils import dep_util
+
 import os
 
 from photocollage import APP_NAME, APP_VERSION
