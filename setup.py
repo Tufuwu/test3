b8d624822eb4c9318d00ed915e5f6393b050f644
wu <wu@.com>
2025-08-13 17:27:17 +0800

    opencivicdata/python-opencivicdata

--- setup.py
+++ setup.py
@@ -1,35 +1,35 @@
-from setuptools import setup
+#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-with open("README.md") as f:
-    readme = f.read()
+install_requires = ["Django>=2.2", "psycopg2-binary"]
 
-with open("aioitertools/__init__.py") as f:
-    for line in f:
-        if line.startswith("__version__"):
-            version = line.split('"')[1]
+extras_require = {
+    "dev": ["pytest>=3.6", "pytest-cov", "pytest-django", "coveralls", "flake8"]
+}
+
 
 setup(
-    name="aioitertools",
-    description="asyncio version of the standard multiprocessing module",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    version=version,
-    author="John Reese",
-    author_email="john@noswap.com",
-    url="https://github.com/jreese/aioitertools",
+    name="opencivicdata",
+    version="3.2.0",
+    author="James Turk",
+    author_email="james@openstates.org",
+    license="BSD",
+    description="python opencivicdata library",
+    long_description="",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    platforms=["any"],
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Framework :: AsyncIO",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    license="MIT",
-    packages=["aioitertools", "aioitertools.tests"],
-    package_data={"aioitertools": ["py.typed"]},
-    python_requires=">=3.6",
-    setup_requires=["setuptools>=38.6.0"],
-    install_requires=[],
 )
