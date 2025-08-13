e08fde9b000fc14763fc39e9bb9c79f02a26b271
wu <wu@.com>
2025-08-13 16:07:44 +0800

    horlogeskynet/archey4

--- setup.py
+++ setup.py
@@ -1,82 +1,66 @@
+#!/usr/bin/env python3
+
 """
-Mizani
-
-Mizani is a scales package for graphics. It is based on Hadley
-Wickham's *Scales* package.
+This is the Archey 4's `setup.py` file, allowing us to distribute it as a package...
+... with cool meta-data.
 """
 
-from setuptools import setup, find_packages
+import os
 
-import versioneer
+from setuptools import find_packages, setup
 
-
-__author__ = 'Hassan Kibirige'
-__email__ = 'has2k1@gmail.com'
-__description__ = "Scales for Python"
-__license__ = 'BSD (3-clause)'
-__url__ = 'https://github.com/has2k1/mizani'
-__classifiers__ = [
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: BSD License',
-    'Programming Language :: Python :: 3',
-    'Topic :: Scientific/Engineering :: Visualization',
-]
+from archey._version import __version__
 
 
-def check_dependencies():
-    """
-    Check for system level dependencies
-    """
-    pass
-
-
-def get_required_packages():
-    """
-    Return required packages
-
-    Plus any version tests and warnings
-    """
-    install_requires = ['numpy',
-                        'pandas >= 1.1.0',
-                        'matplotlib >= 3.1.1',
-                        'palettable']
-    return install_requires
-
-
-def get_package_data():
-    """
-    Return package data
-
-    For example:
-
-        {'': ['*.txt', '*.rst'],
-         'hello': ['*.msg']}
-
-    means:
-        - If any package contains *.txt or *.rst files,
-          include them
-        - And include any *.msg files found in
-          the 'hello' package, too:
-    """
-    return {}
-
-
-if __name__ == '__main__':
-
-    check_dependencies()
-
-    setup(name='mizani',
-          maintainer=__author__,
-          maintainer_email=__email__,
-          description=__description__,
-          long_description=__doc__,
-          license=__license__,
-          version=versioneer.get_version(),
-          cmdclass=versioneer.get_cmdclass(),
-          url=__url__,
-          python_requires='>=3.6',
-          install_requires=get_required_packages(),
-          packages=find_packages(),
-          package_data=get_package_data(),
-          classifiers=__classifiers__
-          )
+setup(
+    name='archey4',
+    version=__version__.lstrip('v'),
+    description='Archey is a simple system information tool written in Python',
+    keywords='archey python3 linux system-information monitoring screenshot',
+    url='https://github.com/HorlogeSkynet/archey4',
+    author='Samuel Forestier',  # Not alone
+    author_email='dev+archey@samuel.domains',
+    license='GPLv3',
+    packages=find_packages(exclude=['archey.test*']),
+    test_suite='archey.test',
+    python_requires='>=3.5',
+    install_requires=[
+        'distro',
+        'netifaces'
+    ],
+    entry_points={
+        'console_scripts': [
+            'archey = archey.__main__:main'
+        ]
+    },
+    long_description="""\
+Archey4 is a **maintained** fork of the original Archey Linux system tool.
+The original Archey program had been written by Melik Manukyan in 2009, and quickly abandoned in 2011.
+At first, it only supported Arch Linux distribution, further support had been added afterwards.
+Many forks popped in the wild due to inactivity, but this one attends since 2017 to succeed where the others failed:
+Remain *maintained*, *community-driven* and *highly-compatible* with yesterday's and today's systems.\
+""",
+    long_description_content_type='text/x-rst',
+    data_files=[
+        # By filtering on `os.path.exists`, install should succeed even when
+        #   the compressed manual page is not available (iterable would be empty).
+        ('share/man/man1', filter(os.path.exists, ['dist/archey.1.gz'])),
+        ('share/doc/archey4', ['README.md', 'COPYRIGHT.md'])
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: System'
+    ]
+)
