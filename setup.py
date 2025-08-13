69613b171a1b833397e67c99bc60d1bf50d3448f
wu <wu@.com>
2025-08-13 14:41:25 +0800

    django-tastypie/django-tastypie

--- setup.py
+++ setup.py
@@ -1,61 +1,57 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
 
-__version__ = "2.1.1"
+
+from tastypie import __version__
+
 
 setup(
-    # package name in pypi
-    name="django-oscar-api",
-    # extract version from module.
+    name='django-tastypie',
     version=__version__,
-    description="REST API module for django-oscar",
-    long_description=open("README.rst").read(),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: Unix",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
+    description='A flexible & capable API layer for Django.',
+    author='Daniel Lindsley',
+    author_email='daniel@toastdriven.com',
+    url='https://github.com/django-tastypie/django-tastypie',
+    long_description=open('README.rst', 'r').read(),
+    packages=[
+        'tastypie',
+        'tastypie.utils',
+        'tastypie.management',
+        'tastypie.management.commands',
+        'tastypie.migrations',
+        'tastypie.contrib',
+        'tastypie.contrib.gis',
+        'tastypie.contrib.contenttypes',
     ],
-    python_requires=">=3.6",
-    keywords="",
-    author="Lars van de Kerkhof, Martijn Jacobs",
-    author_email="lars@permanentmarkers.nl, martijn@devopsconsulting.nl",
-    url="https://github.com/django-oscar/django-oscar-api",
-    license="BSD",
-    packages=find_packages(
-        exclude=[
-            "*tests.unit",
-            "*tests.serializers*",
-            "*tests.doctests*",
-            "*fixtures",
-            "*fixtures*",
-            "*sandbox*",
-        ]
-    ),
-    # include non python files
-    include_package_data=True,
-    zip_safe=False,
-    # specify dependencies
-    install_requires=[
-        "setuptools",
-        "django-oscar>=3.0",
-        "Django>=2.2.13",  # CVE-2020-9402
-        "djangorestframework>=3.9",  # first version to support Django 2.2
-    ],
-    # mark test target to require extras.
-    extras_require={
-        "dev": ["coverage", "mock", "twine", "wheel", "easy_thumbnails"],
-        "lint": ["flake8", "flake8-black", "flake8-bugbear", "black>=19.10b0"],
+    package_data={
+        'tastypie': ['templates/tastypie/*'],
     },
+    zip_safe=False,
+    requires=[
+        'python_mimeparse(>=0.1.4, !=1.5)',
+        'dateutil(>=1.5, !=2.0)',
+    ],
+    install_requires=[
+        'python-mimeparse >= 0.1.4, != 1.5',
+        'python-dateutil >= 1.5, != 2.0',
+    ],
+    tests_require=['mock', 'PyYAML', 'lxml', 'defusedxml'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
 )
