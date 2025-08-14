fddba6bc977348538fd01d1bb4489ad239c0049f
wu <wu@.com>
2025-08-14 11:45:09 +0800

    csingley/ofxtools

--- Makefile
+++ Makefile
@@ -1,39 +1,33 @@
-.PHONY: docs build test coverage build_rpm clean
-
-ifndef VTENV_OPTS
-VTENV_OPTS = -p python2.7 --no-site-packages
-endif
-
-VENV?=virtualenv
-
-bin/python:
-	$(VENV) $(VTENV_OPTS) .
-	bin/python setup.py develop
-
-test: bin/python
-	bin/pip install tox
-	bin/tox
-
-docs:
-	bin/pip install -r doc-requirements.txt --use-mirrors
-	SPHINXBUILD=../bin/sphinx-build $(MAKE) -C docs html $^
-
-coverage: bin/coverage
-	rm -f `pwd`/.coverage
-	rm -rf `pwd`/html
-	- COVERAGE_PROCESS_START=`pwd`/.coveragerc COVERAGE_FILE=`pwd`/.coverage PYTHONPATH=`pwd` bin/nosetests -s circus/tests
-	bin/coverage combine
-	bin/coverage html
-
-bin/coverage: bin/python
-	bin/pip install -r test-requirements.txt --use-mirrors
-	bin/pip install nose coverage
-
-build_rpm:
-	bin/python setup.py bdist_rpm --requires "python26 python-setuptools pyzmq python26-psutil"
+test:
+	coverage erase
+	black --check .
+	mypy ofxtools
+	mypy tests
+	python `which nosetests` -dsv --nologcapture --with-coverage --cover-package ofxtools tests/*.py
 
 clean:
-	rm -rf bin .tox include/ lib/ man/ circus.egg-info/ build/
-	find . -name "*.pyc" | xargs rm -f
-	find . -name "*.un~" | xargs rm -f
-	find . -name "__pycache__" | xargs rm -rf
+	# find -name '.*\.pyc' -exec rm {} \;
+	find . -name "*.py[co]" -o -name __pycache__ -exec rm -rf {} +;
+	find -name '.*~' -exec rm {} \;
+	rm -rf reg-settings.py
+	rm -rf MANIFEST dist build *.egg-info
+	rm coverage.xml
+
+install:
+	make clean
+	make uninstall
+	pip install .
+
+uninstall:
+	pip uninstall -y ofxtools
+
+lint:
+	pylint ofxtools/*.py
+
+lint-tests:
+	pylint tests/*.py
+
+html:
+	sphinx-build -b html docs docs/_build
+
+.PHONY:	test clean lint lint-tests install uninstall html
--- setup.py
+++ setup.py
@@ -1,61 +1,67 @@
-import sys
+"""
+Release process:
+    1. Update ofxtools.__version__.__version__
+    2. Change download_url below
+    3. Commit changes & push
+    4.  Test: `python setup.py sdist`
+    5.  Test: `twine upload --repository-url https://test.pypi.org/legacy/ dist/*`
+    6.  Test result: Check https://test.pypi.org/project/ofxtools
+    7.  `git tag` the release
+    8.  `git push --tags`
+    9.  Verify that new tag shows at https://github.com/csingley/ofxtools/releases
+    10. `twine upload dist/*`
+    11. `make clean`
+    12. Change download_url back to master; commit & push
+"""
+# stdlib imports
+import os.path
 from setuptools import setup, find_packages
-from circus import __version__
 
-if not hasattr(sys, 'version_info') or sys.version_info < (3, 5, 0, 'final'):
-    raise SystemExit("Circus requires Python 3.5 or higher.")
+__here__ = os.path.dirname(os.path.realpath(__file__))
 
+ABOUT = {}
+with open(os.path.join(__here__, "ofxtools", "__version__.py"), "r") as f:
+    exec(f.read(), ABOUT)
 
-install_requires = ['psutil', 'pyzmq>=17.0', 'tornado>=5.0.2']
-
-try:
-    import argparse     # NOQA
-except ImportError:
-    install_requires.append('argparse')
-
-with open("README.md") as f:
+with open(os.path.join(__here__, "README.rst"), "r") as f:
     README = f.read()
 
+URL_BASE = "{}/tarball".format(ABOUT["__url__"])
 
-setup(name='circus',
-      version=__version__,
-      packages=find_packages(exclude=["docs", "examples"]),
-      description=("Circus is a program that will let you run and watch "
-                   " multiple processes and sockets."),
-      long_description=README,
-      author="Mozilla Foundation & contributors",
-      author_email="services-dev@lists.mozila.org",
-      include_package_data=True,
-      zip_safe=False,
-      classifiers=[
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
-          "License :: OSI Approved :: Apache Software License"
-      ],
-      install_requires=install_requires,
-      extras_require={
-        'test': [
-            'nose',
-            'nose-cov',
-            'coverage',
-            'mock',
-            'circus-web',
-            'gevent',
-            'papa',
-            'PyYAML',
-            'pyzmq>=17.0',
-            'flake8==2.1.0',
-        ],
-      },
-      test_suite='circus.tests',
-      entry_points="""
-      [console_scripts]
-      circusd = circus.circusd:main
-      circusd-stats = circus.stats:main
-      circusctl = circus.circusctl:main
-      circus-top = circus.stats.client:main
-      circus-plugin = circus.plugins:main
-      """)
+setup(
+    name=ABOUT["__title__"],
+    version=ABOUT["__version__"],
+    description=ABOUT["__description__"],
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    author=ABOUT["__author__"],
+    author_email=ABOUT["__author_email__"],
+    url=ABOUT["__url__"],
+    packages=find_packages(),
+    package_data={"ofxtools": ["README.rst", "py.typed", "config/*"]},
+    python_requires=">=3.8",
+    license=ABOUT["__license__"],
+    # Note: change 'master' to the tag name when releasing a new verion
+    download_url="{}/master".format(URL_BASE),
+    # download_url="{}/{}".format(URL_BASE, ABOUT["__version__"]),
+    entry_points={"console_scripts": ["ofxget=ofxtools.scripts.ofxget:main"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Topic :: Office/Business",
+        "Topic :: Office/Business :: Financial",
+        "Topic :: Office/Business :: Financial :: Accounting",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords=["ofx", "Open Financial Exchange"],
+)
