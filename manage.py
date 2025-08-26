b429764001c0e8b8dab410e260b45a58886b0064
wu <wu@.com>
2025-08-26 15:38:32 +0800

    jschneier/django-storages

--- manage.py
+++ manage.py
@@ -1,1 +1,11 @@
-# This file only exists to make pytest-django work.
+#!/usr/bin/env python
+
+# XXX we need manage.py until pytest-django is fixed
+# https://github.com/pytest-dev/pytest-django/issues/639
+
+import sys
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
