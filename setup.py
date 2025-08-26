d71687d0527f1278e773ff366ec4044f1d9369ad
wu <wu@.com>
2025-08-26 13:58:19 +0800

    vaibhavsingh97/random-word

--- setup.py
+++ setup.py
@@ -1,72 +1,40 @@
-#!/usr/bin/env python
-import os
-from pathlib import Path
+import random_word
+import setuptools
 
-from setuptools import setup, find_packages, Command
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-ROOTDIR = Path(__file__).parent
-
-__version__ = None  # Overwritten by executing version.py.
-with open(ROOTDIR / "puncover/version.py") as f:
-    exec(f.read())
-
-
-with open(ROOTDIR / "requirements-test.txt") as f:
-    tests_require = list(filter(lambda x: not x.strip().startswith('-r'), f.readlines()))
-
-with open(ROOTDIR / "requirements.txt") as f:
-    requires = f.readlines()
-
-
-class CleanCommand(Command):
-    """Custom clean command to tidy up the project root."""
-
-    # http://stackoverflow.com/a/3780822/196350
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        os.system("rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info")
-
-
-setup(
-    name="puncover",
-    version=__version__,
-    description="Analyses C/C++ build output for code size, static variables, and stack usage.",
-    long_description=open("README.rst").read(),
-    long_description_content_type="text/x-rst",
-    url="https://github.com/hbehrens/puncover",
-    download_url="https://github.com/hbehrens/puncover/tarball/%s" % __version__,
-    author="Heiko Behrens",
+setuptools.setup(
+    name=random_word.__name__,
+    version=random_word.__version__,
+    author=random_word.__author__,
+    author_email="hi@vaibhavsingh97.com",
+    description="This is a simple python package to generate random english words",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="MIT",
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "License :: OSI Approved :: MIT License",
+    keywords="package random words word of the day random word generator",
+    url="https://github.com/vaibhavsingh97/random-word",
+    packages=setuptools.find_packages(),
+    classifiers=(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-    packages=find_packages(exclude=["tests", "tests.*"]),
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    test_suite="nose.collector",
+    tests_require=["nose"],
+    install_requires=["requests", "nose"],
     include_package_data=True,
     zip_safe=False,
-    entry_points={"console_scripts": ["puncover = puncover.puncover:main"]},
-    install_requires=requires,
-    tests_require=tests_require,
-    cmdclass={
-        "clean": CleanCommand,
-    },
-    # TODO: https://github.com/HBehrens/puncover/issues/36
-    #  Fix Python 3.5
-    python_requires=">=3.6",
 )
