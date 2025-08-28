6bbdab9c7e53c3a02470f40daa6ae27020fbba84
wu <wu@.com>
2025-08-28 19:04:38 +0800

    123

--- setup.py
+++ setup.py
@@ -1,58 +1,51 @@
 #!/usr/bin/env python
-"""
-Standard build script.
-"""
+# -*- coding: utf-8 -*-
 
-__docformat__ = 'restructuredtext'
+import os
+import sys
 
 
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup
 except ImportError:
-    print('You must have setuptools installed to use setup.py. Exiting...')
-    raise SystemExit(1)
+    from distutils.core import setup
 
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist upload")
+    sys.exit()
 
-install_dependencies = (
-    'requests',
-    'six'
-)
-test_requirements = (
-    'mock',
-    'pyhamcrest',
-    'pylama',
-    'pytest',
-    'requests_mock',
-    'urllib3<2.0'
-)
+readme = open("README.rst").read()
+history = open("HISTORY.rst").read().replace(".. :changelog:", "")
+import djangorestframework_camel_case
+
 setup(
-    name="python-owasp-zap-v2.4",
-    version="0.0.19",
-    description="OWASP ZAP 2.10 API client",
-    long_description="OWASP Zed Attack Proxy 2.10 API Python client (the 2.4 package name has been kept to make it easier to upgrade)",
-    author="ZAP development team",
-    author_email='',
-    url="https://www.zaproxy.org/",
-    download_url="https://github.com/zaproxy/zap-api-python/releases/tag/0.0.19",
-    platforms=['any'],
-    license="ASL2.0",
-    package_dir={
-        '': 'src',
-    },
-    packages=find_packages('src'),
+    name="djangorestframework-camel-case",
+    version=djangorestframework_camel_case.__version__,
+    description="Camel case JSON support for Django REST framework.",
+    long_description=readme + "\n\n" + history,
+    long_description_content_type="text/x-rst",
+    author="Vitaly Babiy",
+    author_email="vbabiy86@gmail.com",
+    url="https://github.com/vbabiy/djangorestframework-camel-case",
+    packages=["djangorestframework_camel_case"],
+    package_dir={"djangorestframework_camel_case": "djangorestframework_camel_case"},
+    include_package_data=True,
+    python_requires=">=3.5",
+    install_requires=[],
+    license="BSD",
+    zip_safe=False,
+    keywords="djangorestframework_camel_case",
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Development Status :: 5 - Production/Stable',
-        'Topic :: Security',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    install_requires=install_dependencies,
-    tests_require=test_requirements,
-    extras_require={'tests': test_requirements}
+    test_suite="tests",
 )
