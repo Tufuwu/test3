3bb06e1af9e1249830b3f74df78b32512c66fda9
wu <wu@.com>
2025-09-03 11:05:34 +0800

    coverage<6 coveralls

--- setup.py
+++ setup.py
@@ -1,108 +1,51 @@
 #!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-"""
+# -*- coding: utf-8 -*-
 
-    Tokenizer for Icelandic text
+from setuptools import setup
 
-    Copyright (C) 2020 Miðeind ehf.
-    Original author: Vilhjálmur Þorsteinsson
+import feedgen.version
 
-    This software is licensed under the MIT License:
+packages = ['feedgen', 'feedgen/ext']
 
-        Permission is hereby granted, free of charge, to any person
-        obtaining a copy of this software and associated documentation
-        files (the "Software"), to deal in the Software without restriction,
-        including without limitation the rights to use, copy, modify, merge,
-        publish, distribute, sublicense, and/or sell copies of the Software,
-        and to permit persons to whom the Software is furnished to do so,
-        subject to the following conditions:
-
-        The above copyright notice and this permission notice shall be
-        included in all copies or substantial portions of the Software.
-
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-"""
-
-from __future__ import absolute_import
-
-import io
-import re
-from glob import glob
-from os.path import basename, dirname, join, splitext
-from setuptools import find_packages, setup  # type: ignore
-
-
-def read(*names, **kwargs):
-    try:
-        return io.open(
-            join(dirname(__file__), *names),
-            encoding=kwargs.get("encoding", "utf8")
-        ).read()
-    except (IOError, OSError):
-        return ""
-
-
-setup(
-    name="tokenizer",
-    version="2.4.0",  # Also update src/tokenizer/__init__.py
-    license="MIT",
-    description="A tokenizer for Icelandic text",
-    long_description=u"{0}\n{1}".format(
-        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S)
-            .sub("", read("README.rst")
-        ),
-        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
-    ),
-    author="Miðeind ehf.",
-    author_email="mideind@mideind.is",
-    url="https://github.com/mideind/Tokenizer",
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
-    package_data={"tokenizer": ["py.typed", "tokenizer.pyi"]},
-    include_package_data=True,
-    zip_safe=False,
-    classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: Unix",
-        "Operating System :: POSIX",
-        "Operating System :: Microsoft :: Windows",
-        "Natural Language :: Icelandic",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Utilities",
-        "Topic :: Text Processing :: Linguistic",
-    ],
-    keywords=["nlp", "tokenizer", "icelandic"],
-    # Install the typing module if it isn't already in the
-    # Python standard library (i.e. in versions prior to 3.5)
-    install_requires=[
-        "typing;python_version<'3.5'"
-    ],
-    # Set up a tokenize command (tokenize.exe on Windows),
-    # which calls main() in src/tokenizer/main.py
-    entry_points={
-        'console_scripts': [
-            'tokenize=tokenizer.main:main',
+setup(name='feedgen',
+      packages=packages,
+      version=feedgen.version.version_full_str,
+      description='Feed Generator (ATOM, RSS, Podcasts)',
+      author='Lars Kiesow',
+      author_email='lkiesow@uos.de',
+      url='https://lkiesow.github.io/python-feedgen',
+      keywords=['feed', 'ATOM', 'RSS', 'podcast'],
+      license='FreeBSD and LGPLv3+',
+      install_requires=['lxml', 'python-dateutil'],
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 ' +
+        'or later (LGPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Communications',
+        'Topic :: Internet',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: XML'
         ],
-    },
-)
+      test_suite="tests",
+      long_description='''\
+Feedgenerator
+=============
+
+This module can be used to generate web feeds in both ATOM and RSS format. It
+has support for extensions. Included is for example an extension to produce
+Podcasts.
+
+It is licensed under the terms of both, the FreeBSD license and the LGPLv3+.
+Choose the one which is more convenient for you. For more details have a look
+at license.bsd and license.lgpl.
+''')
