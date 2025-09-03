dbdba885c92c78302758ae88ac8f8a456489931e
wu <wu@.com>
2025-09-03 10:59:51 +0800

    name: Python package
    on:
      push:
        branches:
          - main
      pull_request:
        branches:
          - main
    
    jobs:
      build:
        runs-on: ubuntu-22.04
    
        strategy:
          matrix:
            python-version: [3.8, pypy-3.8]
    
        steps:
        - uses: actions/checkout@v2
        - name: Set up Python ${{ matrix.python-version }}
          uses: actions/setup-python@v2
          with:
            python-version: ${{ matrix.python-version }}
        - name: Install dependencies
          run: |
            python setup.py develop
        - name: Test with pytest
          run: |
            python -m pytest
        - name: Notify Slack
          if: failure()
          uses: 8398a7/action-slack@v3
          with:
            status: ${{ job.status }}
            fields: repo,message,commit,author,action,eventName,ref,workflow,job,took
          env:
            SLACK_WEBHOOK_URL: ${{ secrets.SLACK_WEBHOOK_URL }}

--- setup.py
+++ setup.py
@@ -1,66 +1,108 @@
 #!/usr/bin/env python
-#-----------------------------------------------------------------------------
-# Copyright (c) 2013-2020, NeXpy Development Team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING, distributed with this software.
-#-----------------------------------------------------------------------------
+# -*- encoding: utf-8 -*-
+"""
 
-from setuptools import setup, find_packages, Extension
+    Tokenizer for Icelandic text
 
-import os, sys
-import versioneer
+    Copyright (C) 2020 Miðeind ehf.
+    Original author: Vilhjálmur Þorsteinsson
 
-# pull in some definitions from the package's __init__.py file
-sys.path.insert(0, os.path.join('src', ))
-import nexpy
-import nexpy.requires
+    This software is licensed under the MIT License:
 
-verbose=1
+        Permission is hereby granted, free of charge, to any person
+        obtaining a copy of this software and associated documentation
+        files (the "Software"), to deal in the Software without restriction,
+        including without limitation the rights to use, copy, modify, merge,
+        publish, distribute, sublicense, and/or sell copies of the Software,
+        and to permit persons to whom the Software is furnished to do so,
+        subject to the following conditions:
 
-setup (name =  nexpy.__package_name__,        # NeXpy
-       version=versioneer.get_version(),
-       cmdclass=versioneer.get_cmdclass(),
-       license = nexpy.__license__,
-       description = nexpy.__description__,
-       long_description = nexpy.__long_description__,
-       author=nexpy.__author_name__,
-       author_email=nexpy.__author_email__,
-       url=nexpy.__url__,
-       download_url=nexpy.__download_url__,
-       platforms='any',
-       python_requires='>=3.7',
-       install_requires = nexpy.requires.pkg_requirements,
-       extras_require = nexpy.requires.extra_requirements,
-       package_dir = {'': 'src'},
-       packages = find_packages('src'),
-       include_package_data = True,
-       package_data = {
-                       'nexpy.gui': ['resources/icon/*.svg',
-                                     'resources/icon/*.png',
-                                     'resources/*.png',
-                                    ],
-                       'nexpy.definitions': ['base_classes/*.xml'],
-                       'nexpy': [
-                           'examples/*.*',
-                           'examples/*/*.*',
-                           'examples/*/*/*.*',
-                       ],
-                   },
-       entry_points={
-            # create & install scripts in <python>/bin
-            'gui_scripts': ['nexpy = nexpy.nexpygui:main',],
-       },
-       classifiers= ['Development Status :: 4 - Beta',
-                     'Intended Audience :: Developers',
-                     'Intended Audience :: Science/Research',
-                     'License :: OSI Approved :: BSD License',
-                     'Programming Language :: Python',
-                     'Programming Language :: Python :: 3',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8',
-                     'Programming Language :: Python :: 3.9',
-                     'Topic :: Scientific/Engineering',
-                     'Topic :: Scientific/Engineering :: Visualization'],
-      )
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+from __future__ import absolute_import
+
+import io
+import re
+from glob import glob
+from os.path import basename, dirname, join, splitext
+from setuptools import find_packages, setup  # type: ignore
+
+
+def read(*names, **kwargs):
+    try:
+        return io.open(
+            join(dirname(__file__), *names),
+            encoding=kwargs.get("encoding", "utf8")
+        ).read()
+    except (IOError, OSError):
+        return ""
+
+
+setup(
+    name="tokenizer",
+    version="2.4.0",  # Also update src/tokenizer/__init__.py
+    license="MIT",
+    description="A tokenizer for Icelandic text",
+    long_description=u"{0}\n{1}".format(
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S)
+            .sub("", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
+    ),
+    author="Miðeind ehf.",
+    author_email="mideind@mideind.is",
+    url="https://github.com/mideind/Tokenizer",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    package_data={"tokenizer": ["py.typed", "tokenizer.pyi"]},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Natural Language :: Icelandic",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    keywords=["nlp", "tokenizer", "icelandic"],
+    # Install the typing module if it isn't already in the
+    # Python standard library (i.e. in versions prior to 3.5)
+    install_requires=[
+        "typing;python_version<'3.5'"
+    ],
+    # Set up a tokenize command (tokenize.exe on Windows),
+    # which calls main() in src/tokenizer/main.py
+    entry_points={
+        'console_scripts': [
+            'tokenize=tokenizer.main:main',
+        ],
+    },
+)
