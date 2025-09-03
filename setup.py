9f5f7ce7daf2517bbb4774a670221c85d7513f4b
wu <wu@.com>
2025-09-03 14:12:48 +0800

    ciciui/django-db-logger

--- setup.py
+++ setup.py
@@ -1,90 +1,38 @@
-#!/usr/bin/env python
-import codecs
 import os
-import re
-import sys
-from os import path
+from setuptools import setup
 
-from setuptools import find_packages, setup
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
 
-# When creating the sdist, make sure the django.mo file also exists:
-if "sdist" in sys.argv or "develop" in sys.argv:
-    os.chdir("fluent_blogs")
-    try:
-        from django.core import management
-
-        management.call_command("compilemessages", stdout=sys.stderr, verbosity=1)
-    except ImportError:
-        if "sdist" in sys.argv:
-            raise
-    finally:
-        os.chdir("..")
-
-
-def read(*parts):
-    file_path = path.join(path.dirname(__file__), *parts)
-    return codecs.open(file_path, encoding="utf-8").read()
-
-
-def find_version(*parts):
-    version_file = read(*parts)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return str(version_match.group(1))
-    raise RuntimeError("Unable to find version string.")
-
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="django-fluent-blogs",
-    version=find_version("fluent_blogs", "__init__.py"),
-    license="Apache 2.0",
-    install_requires=[
-        "django-fluent-contents>=2.0.7",
-        "django-fluent-utils>=2.0.1",
-        "django-categories-i18n>=1.1.1",
-        "django-parler>=2.0.1",
-        "django-slug-preview>=1.0.4",
-        "django-tag-parser>=3.2",
-    ],
-    requires=[
-        "Django (>=2.2)",
-    ],
-    extras_require={
-        "tests": [
-            "django-fluent-pages>=2.0.7",
-            "django-wysiwyg>=0.7.1",
-        ],
-        "blogpage": ["django-fluent-pages>=2.0.7"],
-        "taggit": ["taggit", "taggit-autosuggest"],
-    },
-    description="A blog engine with flexible block contents (based on django-fluent-contents).",
-    long_description=read("README.rst"),
-    author="Diederik van der Boor",
-    author_email="opensource@edoburu.nl",
-    url="https://github.com/edoburu/django-fluent-blogs",
-    download_url="https://github.com/edoburu/django-fluent-blogs/zipball/master",
-    packages=find_packages(),
+    name='django-db-logger',
+    version='0.1.11',
+    packages=['django_db_logger', 'django_db_logger.migrations'],
     include_package_data=True,
-    test_suite="runtests",
-    zip_safe=False,
+    license='MIT License',
+    description='Django logging in database',
+    long_description=README,
+    url='https://github.com/CiCiUi/django-db-logger',
+    author='zhangshine',
+    author_email='zhangshine0125@gmail.com',
+    install_requires=['django>=1.9', 'six'],
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Framework :: Django",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Framework :: Django",
-        "Framework :: Django :: 2.2",
-        "Framework :: Django :: 3.1",
-        "Framework :: Django :: 3.2",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 )
